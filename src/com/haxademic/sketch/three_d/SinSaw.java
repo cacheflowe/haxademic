0fb459209f147eecbca2e21b1e329a2915b74c5c
cacheflowe <cacheflowe@cacheflowe.com>
2014-01-31 14:28:19 -0700

    Much better sketch

--- src/com/haxademic/sketch/three_d/SinSaw.java
+++ src/com/haxademic/sketch/three_d/SinSaw.java
@@ -6,6 +6,7 @@ import processing.core.PGraphics;
 import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.draw.util.DrawUtil;
+import com.haxademic.core.draw.util.OpenGLUtil;
 import com.haxademic.core.math.MathUtil;
 
 public class SinSaw
@@ -17,13 +18,15 @@ extends PAppletHax
 		super.setup();
 		_texture = P.p.createGraphics( 520, 120, P.P3D );
 		_texture.background(255);
-
+		p.smooth(OpenGLUtil.SMOOTH_HIGH);
+		_texture.smooth(OpenGLUtil.SMOOTH_HIGH);
 	}
 	
 	protected void overridePropsFile() {
 		_appConfig.setProperty( "rendering", "false" );
 		_appConfig.setProperty( "width", "520" );
 		_appConfig.setProperty( "height", "120" );
+		_appConfig.setProperty( "fps", "60" );
 	}
 		
 	public void drawApp() {
@@ -31,7 +34,9 @@ extends PAppletHax
 		p.fill( 255 );
 		p.noStroke();
 		p.rectMode( PConstants.CENTER );
+		p.ellipseMode( PConstants.CENTER );
 		_texture.rectMode( PConstants.CENTER );
+		_texture.ellipseMode( PConstants.CENTER );
 
 		DrawUtil.setColorForPImage(p);
 		p.image(_texture, 0, 0);
@@ -39,28 +44,35 @@ extends PAppletHax
 		_texture.beginDraw();
 		_texture.fill( 255, 255, 255, 255 );
 		_texture.noStroke();
+		
+		_texture.fill( 0, 7f );
+		_texture.rect(p.width/2f, p.height/2f, p.width, p.height);
 
-		float incrementer = p.frameCount / 10f;
+		float incrementer = p.frameCount / 20f;
+		float halfH = p.height/2f;
 		
 		// draw sin
-		fill(255, 0, 0);
+		fill(255, 255, 0);
+		_texture.fill(255, 255, 0);
 		float sin = P.sin(incrementer);
-		rect(250, 50 + 20 * sin, 20, 20);
-		_texture.rect(250, 50 + 20 * sin, 20, 20);
+		ellipse(250, halfH + 20 * sin, 20, 20);
+		_texture.ellipse(250, halfH + 20 * sin, 20, 20);
 		
 		// draw saw
-		fill(0, 255, 0);
+		fill(255, 0, 255);
+		_texture.fill(255, 0, 255);
 		float saw = MathUtil.saw(incrementer);
-		rect(350, 50 + 20 * saw, 20, 20);
-		_texture.rect(350, 50 + 20 * saw, 20, 20);
+		ellipse(350, halfH + 20 * saw, 20, 20);
+		_texture.ellipse(350, halfH + 20 * saw, 20, 20);
 		
 		// draw saw tan
-		fill(0, 0, 255);
-		float sawtan = MathUtil.sawTan(incrementer);
-		rect(450, 50 + 20 * sawtan, 20, 20);
-		_texture.rect(450, 50 + 20 * sawtan, 20, 20);
+		fill(0, 255, 255);
+		_texture.fill(0, 255, 255);
+		float sawtan = MathUtil.sawTan(incrementer * 2f);
+		ellipse(450, halfH + 20 * sawtan, 20, 20);
+		_texture.ellipse(450, halfH + 20 * sawtan, 20, 20);
 		
 		_texture.endDraw();
-		_texture.copy(_texture, 0, 0, _texture.width, _texture.height, -2, 0, _texture.width, _texture.height);
+		_texture.copy(_texture, 0, 0, _texture.width, _texture.height, -1, 0, _texture.width, _texture.height);
 	}
 }
