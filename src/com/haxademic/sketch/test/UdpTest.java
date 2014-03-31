a163fbfdc4090443d07f0fafe1178778d04be266
cacheflowe <cacheflowe@cacheflowe.com>
2014-03-31 11:54:17 -0600

    Code formatting

--- src/com/haxademic/sketch/test/UdpTest.java
+++ src/com/haxademic/sketch/test/UdpTest.java
@@ -8,36 +8,36 @@ import com.haxademic.core.app.PAppletHax;
 @SuppressWarnings("serial")
 public class UdpTest 
 extends PAppletHax {
-	
+
 	protected UDP udp;
 
-	
+
 	protected void overridePropsFile() {
 		// _appConfig.setProperty( "width", "1200" );
 	}
-	
+
 	public void setup() {
 		super.setup();
-		  udp = new UDP( this, 6000 );
-		  udp.log( true ); 		// <-- printout the connection activity
-		  udp.listen( true );
+		udp = new UDP( this, 6000 );
+		udp.log( true ); 		// <-- printout the connection activity
+		udp.listen( true );
 
 	}
-	
+
 	public void drawApp() {
 	}
 
 	public void keyPressed() {
 		super.keyPressed();
-		
-	    String message  = str( key );	// the message to send
-	    String ip       = "10.0.1.138";	// the remote IP address
-	    int port        = 6100;		// the destination port
-	    
-	    message = message+";\n";
-	    udp.send( message, ip, port );
+
+		String message  = str( key );	// the message to send
+		String ip       = "10.0.1.138";	// the remote IP address
+		int port        = 6100;		// the destination port
+
+		message = message+";\n";
+		udp.send( message, ip, port );
 	}
-	
+
 	/**
 	 * To perform any action on datagram reception, you need to implement this 
 	 * handler in your code. This method will be automatically called by the UDP 
@@ -48,14 +48,14 @@ extends PAppletHax {
 	 */
 	// void receive( byte[] data ) { 			// <-- default handler
 	public void receive( byte[] data, String ip, int port ) {	// <-- extended handler
-	  
-	  
-	  // get the "real" message =
-	  // forget the ";\n" at the end <-- !!! only for a communication with Pd !!!
-	  data = subset(data, 0, data.length-2);
-	  String message = new String( data );
-	  
-	  // print the result
-	  P.println( "receive: \""+message+"\" from "+ip+" on port "+port );
+
+
+		// get the "real" message =
+		// forget the ";\n" at the end <-- !!! only for a communication with Pd !!!
+		data = subset(data, 0, data.length-2);
+		String message = new String( data );
+
+		// print the result
+		P.println( "receive: \""+message+"\" from "+ip+" on port "+port );
 	}
 }
