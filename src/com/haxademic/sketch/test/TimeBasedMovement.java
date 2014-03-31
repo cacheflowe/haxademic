b6969f5a5302961e0deb64f92bc8e5cfee42e298
cacheflowe <cacheflowe@cacheflowe.com>
2014-03-31 11:51:44 -0600

    Tweaks

--- src/com/haxademic/sketch/test/TimeBasedMovement.java
+++ src/com/haxademic/sketch/test/TimeBasedMovement.java
@@ -15,7 +15,7 @@ extends PAppletHax
 	protected void overridePropsFile() {
 		_appConfig.setProperty( "width", "640" );
 		_appConfig.setProperty( "height", "480" );
-		_appConfig.setProperty( "fps", "30" );
+		_appConfig.setProperty( "fps", "60" );
 		// _appConfig.setProperty( "fps", "60" );
 	}
 
@@ -29,13 +29,15 @@ extends PAppletHax
 	public void drawApp() {
 		p.background(0);
 		
+		p.fill(255);
+		float timeX = curX % p.width;
+		// P.println(timeX);
+		p.rect( timeX, 100, 100, 100 );
+
 		timeFactor.update();
-		curX += moveXPerFrame * timeFactor.multiplier();
+		curX += P.round(moveXPerFrame * timeFactor.multiplier());
+//		P.println(curX);
 //		P.println("target_fps: "+timeFactor.targetFps()+" / actual_fps: "+timeFactor.actualFps()+" / timeFactor: "+timeFactor.multiplier());
 		
-		p.fill(255);
-		float timeX = P.round(curX % p.width);
-		P.println(timeX);
-		p.rect( timeX, 100, 100, 100 );
 	}
 }
