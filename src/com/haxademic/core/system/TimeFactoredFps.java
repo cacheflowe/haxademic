47d5f00a0db613b9285b47f14683d06d2fd47f37
cacheflowe <cacheflowe@cacheflowe.com>
2014-03-31 11:51:02 -0600

    Add eased time update

--- src/com/haxademic/core/system/TimeFactoredFps.java
+++ src/com/haxademic/core/system/TimeFactoredFps.java
@@ -1,5 +1,7 @@
 package com.haxademic.core.system;
 
+import com.haxademic.core.math.easing.EasingFloat;
+
 import processing.core.PApplet;
 
 public class TimeFactoredFps {
@@ -7,6 +9,7 @@ public class TimeFactoredFps {
 	protected PApplet p;
 	protected float _targetFps;
 	protected float _timeFactor;
+	protected EasingFloat _timeFactorEased;
 	protected float _actualFps;
 	protected float _lastTime;
 	
@@ -14,6 +17,7 @@ public class TimeFactoredFps {
 		this.p = p;
 		_targetFps = targetFps;
 		_timeFactor = 1;
+		_timeFactorEased = new EasingFloat(1, 20);
 		_actualFps = targetFps;
 		_lastTime = p.millis();
 	}
@@ -30,9 +34,15 @@ public class TimeFactoredFps {
 		return _timeFactor;
 	}
 	
+	public float multiplierEased() {
+		return _timeFactorEased.value();
+	}
+	
 	public void update() {
 		_actualFps = 1000f / ( p.millis() - _lastTime );
 		_timeFactor = _targetFps / _actualFps;
+		_timeFactorEased.setTarget(_timeFactor);
+		_timeFactorEased.update();
 		_lastTime = p.millis();
 	}
 }	
