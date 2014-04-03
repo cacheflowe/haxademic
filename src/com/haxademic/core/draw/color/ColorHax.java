b157f28e01ec71f8b042d96d3882e80a0afe138b
cacheflowe <cacheflowe@cacheflowe.com>
2014-04-03 11:25:40 -0600

    Clean up and add hex string constructor

--- src/com/haxademic/core/draw/color/ColorHax.java
+++ src/com/haxademic/core/draw/color/ColorHax.java
@@ -1,6 +1,6 @@
 package com.haxademic.core.draw.color;
 
-import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.app.P;
 
 public class ColorHax {
 	public float r;
@@ -8,7 +8,6 @@ public class ColorHax {
 	public float b;
 	public float a;
 	
-	// Cell Constructor
 	public ColorHax( float r, float g, float b, float a ) {
 		this.r = r;
 		this.g = g;
@@ -16,20 +15,25 @@ public class ColorHax {
 		this.a = a;
 	} 
 	
-	public final static int alphaFromColorInt( int c ) { return (c >> 24) & 0xFF; }
-
-	public final static int redFromColorInt( int c ) { return (c >> 16) & 0xFF;	}
+	public ColorHax( String hex ) {
+		int color = ColorUtil.colorFromHex(hex);
+		this.r = redFromColorInt(color);
+		this.g = greenFromColorInt(color);
+		this.b = blueFromColorInt(color);
+		this.a = 255;
+	} 
 	
+	public final static int alphaFromColorInt( int c ) { return (c >> 24) & 0xFF; }
+	public final static int redFromColorInt( int c ) { return (c >> 16) & 0xFF;	}
 	public final static int greenFromColorInt( int c ) { return (c >> 8)  & 0xFF; }
-
 	public final static int blueFromColorInt( int c ) { return c & 0xFF; }
 
-
 	public int colorInt() {
-		return PAppletHax.getInstance().color(r, g, b, a);
+		return P.p.color(r, g, b, a);
 	}
 	
 	public int colorIntWithAlpha( float alpha, float offset ) {
-		return PAppletHax.getInstance().color(r + offset, g + offset, b + offset, alpha);
+		return P.p.color(r + offset, g + offset, b + offset, alpha);
 	}
+	
 }
