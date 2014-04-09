0acb6a02d1c44d560186c3511016c96b53305831
cacheflowe <cacheflowe@cacheflowe.com>
2014-04-09 12:49:20 -0600

    Update testing

--- src/com/haxademic/app/haxmapper/HaxMapperTextureTester.java
+++ src/com/haxademic/app/haxmapper/HaxMapperTextureTester.java
@@ -1,9 +1,9 @@
 package com.haxademic.app.haxmapper;
 
 import com.haxademic.app.haxmapper.textures.BaseTexture;
-import com.haxademic.app.haxmapper.textures.TextureEQColumns;
-import com.haxademic.app.haxmapper.textures.TextureShaderTimeStepper;
-import com.haxademic.app.haxmapper.textures.TextureWaveformSimple;
+import com.haxademic.app.haxmapper.textures.TextureColorAudioSlide;
+import com.haxademic.app.haxmapper.textures.TextureScrollingColumns;
+import com.haxademic.app.haxmapper.textures.TextureSphereRotate;
 import com.haxademic.core.app.PAppletHax;
 
 @SuppressWarnings("serial")
@@ -26,10 +26,12 @@ extends PAppletHax {
 		super.setup();	
 		
 //		_texture = new TextureWebCam();
-		_texture = new TextureEQColumns( 200, 200 );
+		_texture = new TextureScrollingColumns( 200, 200 );
 //		_texture2 = new TextureEQGrid( 200, 200 );
-		_texture3 = new TextureWaveformSimple( 400, 400 );
-		_texture2 = new TextureShaderTimeStepper( 300, 300, "bw-kaleido.glsl" );
+//		_texture3 = new TextureWaveformSimple( 400, 400 );
+		_texture3 = new TextureColorAudioSlide( 400, 400 );
+		_texture2 = new TextureSphereRotate( 500, 500 );
+//		_texture2 = new TextureShaderTimeStepper( 300, 300, "bw-kaleido.glsl" );
 //		_texture = new TextureImageTimeStepper( 600, 600 );
 //		_texture = new TextureSphereRotate( 400, 400 );
 	}
@@ -45,7 +47,7 @@ extends PAppletHax {
 		_texture2.update();
 		_texture3.update();
 		p.image( _texture.texture(), 0, 0 );
-		p.image( _texture2.texture(), 0, 200 );
+		p.image( _texture2.texture(), 0, 400 );
 		p.image( _texture3.texture(), p.width - 400, 400 );
 	}
 }
