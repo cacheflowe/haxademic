64467469b743a56485a79ba69366d181c0467d62
cacheflowe <cacheflowe@cacheflowe.com>
2014-04-11 15:07:08 -0600

    Add vertically-growing bars mode

--- src/com/haxademic/app/haxmapper/textures/TextureEQColumns.java
+++ src/com/haxademic/app/haxmapper/textures/TextureEQColumns.java
@@ -8,28 +8,30 @@ extends BaseTexture {
 
 	protected int _numLines = 40;
 	protected boolean _hasStroke = true;
-	
+	protected boolean _barsGrow = false;
+
 	public TextureEQColumns( int width, int height ) {
 		super();
 
 		buildGraphics( width, height );
 	}
-	
+
 	public void newLineMode() {
 		_numLines = MathUtil.randRange(20, 30);
 		_hasStroke = !_hasStroke;
+		_barsGrow = MathUtil.randBoolean(P.p);
 	}
 
 	public void update() {
 		super.update();
-		
+
 		float eqW = _texture.width / _numLines;
-		float spectrumInterval = ( 512f / _numLines );
+		// float spectrumInterval = ( 512f / _numLines );
 		float avergeInterval = ( 32f / _numLines );
-		
+
 		_texture.beginDraw();
 		_texture.clear();
-		
+
 		if( _hasStroke == true ) {
 			_texture.stroke(0);
 			_texture.strokeWeight(1);
@@ -37,12 +39,18 @@ extends BaseTexture {
 			_texture.noStroke();
 		}
 
-		for( int i=0; i < _numLines; i++ ) {
-			 _texture.fill( _colorEase.colorInt(), P.constrain( P.p.audioIn.getEqAvgBand( P.floor(i*avergeInterval) ) * 255, 0, 255 ) );
-//			_texture.fill( _colorEase.colorInt(), P.constrain( P.p.audioIn.getEqBand( P.floor(i*spectrumInterval) ) * 255, 0, 255 ) );
-			_texture.rect(i * eqW, 0, eqW, _texture.height );  //  P.p.audioIn.getEqBand( P.floor(i*spectrumInterval)%512 ) * 50
+		if( _barsGrow == true ) {
+			for( int i=0; i < _numLines; i++ ) {
+				_texture.fill( _colorEase.colorInt() );
+				_texture.rect(i * eqW, 0, eqW, P.p.audioIn.getEqAvgBand( P.floor(i*avergeInterval) ) * _texture.height * 0.8f );  //  P.p.audioIn.getEqBand( P.floor(i*spectrumInterval)%512 ) * 50
+			}
+		} else {
+			for( int i=0; i < _numLines; i++ ) {
+				_texture.fill( _colorEase.colorInt(), P.constrain( P.p.audioIn.getEqAvgBand( P.floor(i*avergeInterval) ) * 255, 0, 255 ) );
+				_texture.rect(i * eqW, 0, eqW, _texture.height );  //  P.p.audioIn.getEqBand( P.floor(i*spectrumInterval)%512 ) * 50
+			}
 		}
-		
+
 		_texture.endDraw();
 	}
 }
