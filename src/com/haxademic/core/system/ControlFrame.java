e869fca117f3590cf69bfeb298c47d9df94890a7
cacheflowe <cacheflowe@cacheflowe.com>
2014-04-14 11:48:46 -0600

    Awesome external window controls from eskimoblood

---
+++ src/com/haxademic/core/system/ControlFrame.java
@@ -0,0 +1,62 @@
+package com.haxademic.core.system;
+
+// from: https://gist.github.com/eskimoblood/10414654
+
+import java.awt.Frame;
+
+import processing.core.PApplet;
+import controlP5.ControlP5;
+
+@SuppressWarnings("serial")
+public class ControlFrame 
+extends PApplet {
+
+	Frame f;
+	int w;
+	int h;
+	int x;
+	int y;
+	Object _parent;
+	String _name;
+
+	public void setup() {
+		size(w, h);
+		frameRate(30);
+	}
+
+	public void draw() {
+		if( frameCount == 2 ) {
+			createControlP5(_parent, _name);
+		}
+		background(0);
+	}
+
+	public ControlFrame(Object parent, int w, int h, int x, int y) {
+		_parent = parent;
+		_name = parent.getClass().getSimpleName();
+		this.w = w;
+		this.h = h;
+		this.x = x;
+		this.y = y;
+
+		createFrame(_name);
+	}
+
+	private void createFrame(String name) {
+		f = new Frame(name);
+		f.add(this);
+
+		this.init();
+
+		f.setTitle(name);
+		f.setSize(w, h);
+		f.setLocation(x, y);
+		f.setResizable(false);
+		f.setVisible(true);
+	}
+
+	private void createControlP5(Object parent, String name) {
+		ControlP5 cp5 = new ControlP5(this);
+		cp5.addControllersFor(name, parent);
+	}
+}
---
+++ src/com/haxademic/sketch/test/ControlFrameTest.java
@@ -0,0 +1,56 @@
+package com.haxademic.sketch.test;
+
+import processing.core.PApplet;
+
+import com.haxademic.core.app.PAppletHax;
+
+import controlP5.ControlElement;
+
+@SuppressWarnings("serial")
+public class ControlFrameTest 
+extends PAppletHax {
+	
+	protected ControlFrameRGB controlFrameLauncher;
+	
+	protected void overridePropsFile() {
+		_appConfig.setProperty( "fps", "30" );
+		_appConfig.setProperty( "width", "500" );
+		_appConfig.setProperty( "height", "500" );
+	}
+
+	public void setup() {
+		super.setup();	
+		controlFrameLauncher = new ControlFrameRGB(this);
+	}
+
+	public void drawApp() {
+		controlFrameLauncher.update();
+	}
+	
+	
+	
+	public class ControlFrameRGB {
+		// from: https://gist.github.com/eskimoblood/10414654
+
+		private final PApplet p;
+
+		@ControlElement(properties = {"min=0", "max=255"}, x = 10, y = 10)
+		private float red = 0;
+
+		@ControlElement(properties = {"min=0", "max=255"}, x = 10, y = 30)
+		private float green = 0;
+
+		@ControlElement(properties = {"min=0", "max=255"}, x = 10, y = 50)
+		private float blue = 0;
+
+		public ControlFrameRGB(PApplet p) {
+			this.p = p;
+			new ControlFrame(this, 200, 200, p.width, 0);
+		}
+
+		public void update() {
+			p.background(red, green, blue);
+		}
+	}
+
+}
