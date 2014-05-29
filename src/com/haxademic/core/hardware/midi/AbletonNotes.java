8efb946debc8f16e03fbfeebeea901366b4eb1c4
cacheflowe <cacheflowe@cacheflowe.com>
2014-05-28 23:08:24 -0600

    Add ableton notes definition

---
+++ src/com/haxademic/core/hardware/midi/AbletonNotes.java
@@ -0,0 +1,20 @@
+package com.haxademic.core.hardware.midi;
+
+public class AbletonNotes {
+	public static int NOTE_01 = 48;
+	public static int NOTE_02 = 49;
+	public static int NOTE_03 = 50;
+	public static int NOTE_04 = 51;
+	public static int NOTE_05 = 52;
+	public static int NOTE_06 = 53;
+	public static int NOTE_07 = 54;
+	public static int NOTE_08 = 55;
+	public static int NOTE_09 = 56;
+	public static int NOTE_10 = 57;
+	public static int NOTE_11 = 58;
+	public static int NOTE_12 = 59;
+	public static int NOTE_13 = 60;
+	public static int NOTE_14 = 61;
+	public static int NOTE_15 = 62;
+	public static int NOTE_16 = 63;
+}
