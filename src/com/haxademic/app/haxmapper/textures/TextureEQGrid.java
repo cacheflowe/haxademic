7705fbd6d3b3215f168012a35e6e622532366bb1
cacheflowe <cacheflowe@cacheflowe.com>
2014-05-29 00:09:28 -0600

    Add another drawing mode

--- src/com/haxademic/app/haxmapper/textures/TextureEQGrid.java
+++ src/com/haxademic/app/haxmapper/textures/TextureEQGrid.java
@@ -1,19 +1,17 @@
 package com.haxademic.app.haxmapper.textures;
 
 import com.haxademic.core.app.P;
+import com.haxademic.core.math.MathUtil;
 
 public class TextureEQGrid 
 extends BaseTexture {
 
-	protected int _numLines;
-	protected boolean _is3D = false;
-	
 	protected float _cols = 32;
 	protected float _rows = 32;
-	protected float _amp = 20;
 	protected float _spectrumInterval = 512f / (_cols * _rows);	// 256 keeps it in the bottom half of the spectrum since the high ends is so overrun
 	protected float _cellW;
 	protected float _cellH;
+	protected boolean _boxesGrow = false;
 
 
 	public TextureEQGrid( int width, int height ) {
@@ -25,6 +23,10 @@ extends BaseTexture {
 		_cellH = P.ceil( _texture.height/_rows );
 	}
 	
+	public void newLineMode() {
+		_boxesGrow = MathUtil.randBoolean(P.p);
+	}
+
 	public void update() {
 		super.update();
 		
@@ -38,10 +40,22 @@ extends BaseTexture {
 		_texture.noStroke();
 		for (int i = 0; i < _cols; i++) {
 			for (int j = 0; j < _rows; j++) {
-				float alphaVal = P.p.audioIn.getEqBand( P.floor(_spectrumInterval * spectrumIndex) );
-				_texture.fill( _colorEase.colorInt(), P.constrain( alphaVal * 255f, 0, 255 ) );
-				_texture.rect( startX + i*_cellW, startY + j*_cellH, _cellW, _cellH );	
-				spectrumIndex++;
+				if( _boxesGrow ) {
+					float scaleVal = P.constrain( 0.1f * P.p.audioIn.getEqBand( P.floor(_spectrumInterval * spectrumIndex) ), 0, 1 );
+					_texture.fill( _colorEase.colorInt() );
+					_texture.rect( 
+						startX + i*_cellW + _cellW * 0.5f - (_cellW * scaleVal * 0.5f), 
+						startY + j*_cellH + _cellH * 0.5f - (_cellH * scaleVal * 0.5f), 
+						_cellW * scaleVal, 
+						_cellH * scaleVal 
+					);	
+					spectrumIndex++;
+				} else {
+					float alphaVal = P.p.audioIn.getEqBand( P.floor(_spectrumInterval * spectrumIndex) );
+					_texture.fill( _colorEase.colorInt(), P.constrain( alphaVal * 255f, 0, 255 ) );
+					_texture.rect( startX + i*_cellW, startY + j*_cellH, _cellW, _cellH );	
+					spectrumIndex++;
+				}
 			}
 		}
 		
