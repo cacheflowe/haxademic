836fb76de5a7f299714fb83e3cb25644339742ca
cacheflowe <cacheflowe@cacheflowe.com>
2015-03-18 22:01:26 -0600

    Add adjustable threshold

--- data/shaders/filters/blackandwhite.glsl
+++ data/shaders/filters/blackandwhite.glsl
@@ -6,6 +6,7 @@ precision mediump int;
 #define PROCESSING_TEXTURE_SHADER
 
 uniform sampler2D texture;
+uniform float cutoff;
 
 varying vec4 vertColor;
 varying vec4 vertTexCoord;
@@ -15,7 +16,7 @@ const vec4 lumcoeff = vec4(0.299, 0.587, 0.114, 0);
 void main() {
   vec4 col = texture2D(texture, vertTexCoord.xy);
   float lum = dot(col, lumcoeff);
-  if (0.5 < lum) {
+  if (lum > cutoff) {
     gl_FragColor = vertColor;
   } else {
     gl_FragColor = vec4(0, 0, 0, 1);
