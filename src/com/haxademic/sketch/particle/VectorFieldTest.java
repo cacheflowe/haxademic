d871201dfb10a13e7e1db82c97965c03810519ee
cacheflowe <cacheflowe@cacheflowe.com>
2015-05-30 22:50:04 -0600

    Update demo

--- src/com/haxademic/sketch/particle/VectorFieldTest.java
+++ src/com/haxademic/sketch/particle/VectorFieldTest.java
@@ -3,27 +3,32 @@ package com.haxademic.sketch.particle;
 import java.util.ArrayList;
 
 import processing.core.PVector;
-import processing.opengl.PShader;
 
 import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.draw.util.DrawUtil;
 import com.haxademic.core.draw.util.OpenGLUtil;
+import com.haxademic.core.image.filters.FXAAFilter;
 import com.haxademic.core.math.easing.EasingFloat;
-import com.haxademic.core.system.FileUtil;
 
 @SuppressWarnings("serial")
 public class VectorFieldTest 
-extends PAppletHax  
+extends PAppletHax
 {
 
 	protected ArrayList<PVector> _vectorField;
 	protected ArrayList<FieldParticle> _particles;
-	protected PShader _fxaa;
 	
+	public static void main(String args[]) {
+		_hasChrome = false;
+		PAppletHax.main(P.concat(args, new String[] { "--hide-stop", "--bgcolor=000000", Thread.currentThread().getStackTrace()[1].getClassName() }));
+	}
+
 	protected void overridePropsFile() {
 		_appConfig.setProperty( "width", "1280" );
 		_appConfig.setProperty( "height", "720" );
+		_appConfig.setProperty( "fills_screen", "true" );
+		_appConfig.setProperty( "force_foreground", "true" );
 //		_appConfig.setProperty( "fps", "30" );
 //		_appConfig.setProperty( "rendering", "true" );
 	}
@@ -31,12 +36,11 @@ extends PAppletHax
 	public void setup() {
 		super.setup();
 		
-		p.smooth(OpenGLUtil.SMOOTH_HIGH);
+		p.smooth(OpenGLUtil.SMOOTH_LOW);
 //		p.smooth();
-		_fxaa = p.loadShader( FileUtil.getHaxademicDataPath() + "shaders/filters/fxaa.glsl" );
 		
 		_vectorField = new ArrayList<PVector>();
-		float spacing = 30f;
+		float spacing = 100f;
 		for( int x = 0; x <= p.width; x += spacing ) {
 			for( int y = 0; y <= p.height; y += spacing ) {
 				_vectorField.add( new PVector(x, y, 0) );
@@ -44,7 +48,7 @@ extends PAppletHax
 		}
 		
 		_particles = new ArrayList<FieldParticle>();
-		for( int i = 0; i < 3000; i++ ) {
+		for( int i = 0; i < 6000; i++ ) {
 			_particles.add( new FieldParticle() );
 		}
 	}
@@ -52,6 +56,9 @@ extends PAppletHax
 	public void drawApp() {
 		if( p.frameCount == 1 ) p.background(0);
 
+//		OpenGLUtil.setBlending(p.g, true);
+//		OpenGLUtil.setBlendMode(p.g, OpenGLUtil.Blend.ADDITIVE);
+		
 		// fade out background
 		DrawUtil.setDrawCorner(p);
 		p.noStroke();
@@ -60,30 +67,30 @@ extends PAppletHax
 		
 		// draw field
 		DrawUtil.setDrawCenter(p);
-		p.fill(30);
+		p.fill(255);
 		for (PVector vector : _vectorField) {
 			float noise = p.noise(
 					vector.x/11f + p.noise(p.frameCount/100f), 
 					vector.y/20f + p.noise(p.frameCount/50f) 
 			);
 			vector.set(vector.x, vector.y, noise * 2f * P.TWO_PI);
-//			p.pushMatrix();
-//			p.translate(vector.x, vector.y);
-//			p.rotate( vector.z );	// use z for rotation!
-//		    p.rect(0, 0, 1, 5);
-//		    p.popMatrix();
+			
+			// draw attractors
+			p.pushMatrix();
+			p.translate(vector.x, vector.y);
+			p.rotate( vector.z );	// use z for rotation!
+		    p.rect(0, 0, 1, 10);
+		    p.popMatrix();
 		}
 		
 		// draw particles
 		DrawUtil.setDrawCenter(p);
 		for( int i = 0; i < _particles.size(); i++ ) {
-			p.fill(i % 55 + 100, i % 100 + 100, i % 150 + 55);
+			p.fill((i % 150 + 55 / 10), i % 155 + 100, i % 100 + 100);
 			_particles.get(i).update( _vectorField );
 		}
 		
-		p.filter(_fxaa);
-		p.filter(_fxaa);
-		p.filter(_fxaa);
+		FXAAFilter.applyTo(p, p.g);
 	}
 	
 	public class FieldParticle {
@@ -93,7 +100,7 @@ extends PAppletHax
 		public float speed;
 		
 		public FieldParticle() {
-			speed = p.random(4,10);
+			speed = p.random(10,20);
 			radians = new EasingFloat(0, p.random(6,20) );
 			position = new PVector( p.random(0, p.width), p.random(0, p.height) );
 		}
@@ -103,7 +110,7 @@ extends PAppletHax
 			int closeVectors = 0;
 			float averageDirection = 0;
 			for (PVector vector : _vectorField) {
-				if( vector.dist( position ) < 40 ) {
+				if( vector.dist( position ) < 100 ) {
 					averageDirection += vector.z;
 					closeVectors++;
 				}
@@ -129,7 +136,7 @@ extends PAppletHax
 			p.pushMatrix();
 			p.translate(position.x, position.y);
 			p.rotate( -radians.value() );
-		    p.rect(0, 0, speed * 0.5f, speed * 1.8f);
+		    p.rect(0, 0, speed * 0.2f, speed * 1.8f);
 		    p.popMatrix();
 		}
 	}
