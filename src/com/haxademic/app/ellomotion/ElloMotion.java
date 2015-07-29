39ddb0cf650ffaf5aff85a2cd8f7e6c4acd6fad8
cacheflowe <cacheflowe@cacheflowe.com>
2015-07-29 10:36:11 -0600

    Finish up the app

--- src/com/haxademic/app/ellomotion/ElloMotion.java
+++ src/com/haxademic/app/ellomotion/ElloMotion.java
@@ -9,7 +9,10 @@ import processing.opengl.PShader;
 
 import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.draw.text.CustomFontText2D;
+import com.haxademic.core.draw.util.DrawUtil;
 import com.haxademic.core.draw.util.OpenGLUtil;
+import com.haxademic.core.hardware.kinect.KinectRegionGrid;
 import com.haxademic.core.image.ImageUtil;
 import com.haxademic.core.system.FileUtil;
 
@@ -20,33 +23,37 @@ extends PAppletHax{
 	protected ArrayList<String> _files;
 	protected PGraphics _movieComposite; 
 	
-	protected PShader _desaturate;
-	protected PShader _vignette;
-	protected PShader _postBrightness;
-	protected PShader _clipBrightness;
-	protected PShader _threshold;
-	protected PShader _blurH;
-	protected PShader _blurV;
-	protected PShader _invert;
-	protected PShader _badTV;
-	protected PShader edge;
-	protected PShader _halftone;
-	protected PShader _mirror;
-	protected PShader _contrast;
-	
-	protected PShader _lightShader;
-
 	protected boolean _isDebug = false;
 	
+	protected KinectRegionGrid _kinectGrid;
+	protected int _lastPlayerTime = 0;
+	
 	protected KinectLayer _kinectLayer;
-	protected PImage _gallery;
+//	protected PImage _gallery;
+	protected ArrayList<PImage> _galleryImages;
+	protected ArrayList<String> _galleryArtists;
+	protected int _galleryIndex;
+	protected CustomFontText2D _galleryLabel;
+	protected int _labelWidth;
 		
-	protected float SCALE_DOWN = 0.5f;
+	protected float SCALE_DOWN = 0.65f;
 	protected float BLOB_DETECT_SCALE = 0.6f;
+	
+	// TODO ===========================
+	//	* Add username in corner - fade in when kinect users are active
+	// ================================
 
 	public static void main(String args[]) {
 		_isFullScreen = false;
-		PApplet.main(new String[] { "--hide-stop", "--bgcolor=000000", ElloMotion.class.getName() });
+		_hasChrome = false;
+		boolean isSecondScreen = false;
+		if( isSecondScreen ) {
+			PApplet.main(new String[] { "--hide-stop", "--bgcolor=000000", "--location=1920,0", "--display=1", ElloMotion.class.getName() });		// requires hiding os x 2nd screen menu bar: http://www.cultofmac.com/255910/hide-the-menu-bar-on-your-secondary-monitor-with-mavericks-os-x-tips/
+		} else {
+			PApplet.main(new String[] { "--hide-stop", "--bgcolor=000000", ElloMotion.class.getName() });
+		}
+
+//		PApplet.main(new String[] { "--hide-stop", "--bgcolor=000000", ElloMotion.class.getName() });
 	}
 
 	protected void overridePropsFile() {
@@ -54,100 +61,99 @@ extends PAppletHax{
 		p.appConfig.setProperty( "height", "720" );
 //		p.appConfig.setProperty( "width", "960" );
 //		p.appConfig.setProperty( "height", "540" );
-//		p.appConfig.setProperty( "fills_screen", "true" );
+		p.appConfig.setProperty( "fills_screen", "false" );
 		p.appConfig.setProperty( "fullscreen", "false" );
 		p.appConfig.setProperty( "rendering", "false" );
 		p.appConfig.setProperty( "hide_cursor", "true" );
 		p.appConfig.setProperty( "force_foreground", "false" );
 
 		p.appConfig.setProperty( "kinect_active", "true" );
-		p.appConfig.setProperty( "kinect_far", "500" );
-		p.appConfig.setProperty( "kinect_far", "1500" );
+		p.appConfig.setProperty( "kinect_close", "500" );
+		p.appConfig.setProperty( "kinect_far", "1300" );
 		p.appConfig.setProperty( "kinect_pixel_skip", "5" );
 		p.appConfig.setProperty( "kinect_mirrored", "true" );		
 	}
 	
 	public void setup() {
 		super.setup();
-		_gallery = p.loadImage(FileUtil.getFile("images/ello-art/ello-echophon.jpg"));
 		buildCanvas();
+		loadGalleryImages();
 		_kinectLayer = new KinectLayer();
+		_kinectGrid = new KinectRegionGrid(1, 1, p.appConfig.getInt("kinect_close", 0), p.appConfig.getInt("kinect_far", 0), 0, 0, 480, 20, 10);
+		buildText();
+		newImageForNewPlayers();
+	}
+	
+	public void loadGalleryImages() {
+		_galleryImages = new ArrayList<PImage>();
+		_galleryArtists = new ArrayList<String>();
+		
+		String imgBase = "images/ello-art/";
+		
+		ArrayList<String> files = FileUtil.getFilesInDirOfType( FileUtil.getHaxademicDataPath() + imgBase, "jpg" );
+		// files.addAll( FileUtil.getFilesInDirOfType( FileUtil.getHaxademicDataPath() + imgBase, "png" ) );
+		FileUtil.shuffleFileList( files );
+		
+		for( int i=0; i < files.size(); i++ ) {
+			_galleryImages.add( P.p.loadImage( FileUtil.getFile( imgBase + files.get(i) ) ) );
+			_galleryArtists.add(files.get(i).replaceAll(".jpg", ""));
+		}
+		_galleryIndex = 0;
 	}
 	
 	public void drawApp() {
 		background(0);
-		_kinectLayer.setGallery(_gallery);
-		_kinectLayer.update();
+		newImageForNewPlayers();
+		_kinectLayer.updateWithImage(_galleryImages.get(_galleryIndex));
 		p.image(_kinectLayer.canvas(), 0, 0, p.width, p.height);
+		drawArtistText();
 //		p.image(_kinectLayer.mask(), 0, 0, p.width, p.height);
 //		postProcessEffects();
 	}
 
+	protected void drawArtistText() {
+		DrawUtil.resetPImageAlpha(p);
+		PImage img = _galleryLabel.getTextPImage();
+		p.fill(0);
+		p.rect(p.width - 20 - _labelWidth, p.height - 20 - img.height, _labelWidth, img.height);
+		p.image(img, p.width - 39 - img.width, p.height - 19 - img.height);
+	}
+	
+	protected void newImageForNewPlayers() {
+		_kinectGrid.update();
+		if(_kinectGrid.getRegion(0).isActive() == true) {
+			_lastPlayerTime = p.frameCount;
+		} else {
+			if(p.frameCount - _lastPlayerTime == 150) {
+				// next image
+				_galleryIndex++;
+				if(_galleryIndex > _galleryImages.size()-1) _galleryIndex = 0;
+				_galleryLabel.updateText("ello.co/"+_galleryArtists.get(_galleryIndex));
+				_labelWidth = _galleryLabel.getTextPImage().width + 40 - _galleryLabel.getLeftmostPixel();
+			}
+		}
+	}
 	
 	// Video player ================ 
+	protected void buildText() {
+		_galleryLabel = new CustomFontText2D( this, FileUtil.getFile("fonts/AtlasTypewriter-Regular-Web.ttf"), 18, p.color(255), CustomFontText2D.ALIGN_RIGHT, 500, 46 );
+	}
+	
 	protected void buildCanvas() {
 		// build movie players & composite
 		_movieComposite = p.createGraphics(P.round(p.width * SCALE_DOWN), P.round(p.height * SCALE_DOWN), P.OPENGL);
-
-		
-		// build shaders
-		_desaturate = loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/saturation.glsl" );
-		_desaturate.set("saturation", 0.6f);
-		_threshold = loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/blackandwhite.glsl" );
-		_threshold.set("cutoff", 0.5f);
-		_blurV = loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/blur-vertical.glsl" );
-		_blurV.set( "v", 3f/p.height );
-		_blurH = loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/blur-horizontal.glsl" );
-		_blurH.set( "h", 3f/p.width );
-		_invert = loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/invert.glsl" );
-
-		_lightShader = loadShader( FileUtil.getHaxademicDataPath()+"shaders/textures/light-leak.glsl" );
-		
-		_badTV = loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/badtv.glsl" );
-		_badTV.set("time", millis() / 1000.0f);
-		_badTV.set("grayscale", 0);
-		_badTV.set("nIntensity", 0.75f);
-		_badTV.set("sIntensity", 0.55f);
-		_badTV.set("sCount", 4096.0f);
-
-
-		edge = loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/edges.glsl" ); 
-		
-		_halftone = loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/dotscreen.glsl" ); 
-		_halftone.set("tSize", 256f, 256f);
-		_halftone.set("center", 0.5f, 0.5f);
-		_halftone.set("angle", 1.57f);
-		_halftone.set("scale", 1f);
-
-		_mirror = loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/mirror.glsl" ); 
-
-		_contrast = loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/contrast.glsl" ); 
-		_contrast.set("contrast", 1.4f);
-		
-		_vignette = loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/vignette.glsl" );
-		_vignette.set("darkness", 0.85f);
-		_vignette.set("spread", 0.25f);
-
-		_postBrightness = p.loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/brightness.glsl" );
-		_postBrightness.set("brightness", 1.0f );
-
-		_clipBrightness = p.loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/brightness.glsl" );
-		_clipBrightness.set("brightness", 2.0f );
 	}
 	
-	protected void postProcessEffects() {
-		p.filter(_vignette);
-		
-		p.filter( _postBrightness );		
-		_badTV.set("time", millis() / 1000.0f);
-//		p.filter(_badTV);
-	}
 	
 	protected void handleInput( boolean isMidi ) {
 		super.handleInput( isMidi );
 		
 		if( p.key == 'd' ){
 			_isDebug = !_isDebug;
+		}
+		if( p.key == 'n' ){
+			_galleryIndex++;
+			if(_galleryIndex > _galleryImages.size()-1) _galleryIndex = 0;
 		}
 	}
 	
@@ -169,8 +175,8 @@ extends PAppletHax{
 		
 		protected void initImageBuffer() {
 			_particleMask = p.createGraphics(_movieComposite.width, _movieComposite.height, P.OPENGL);
-			_galleryImg = p.createGraphics(_movieComposite.width, _movieComposite.height, P.OPENGL);
 			_particleMask.smooth(OpenGLUtil.SMOOTH_LOW);
+			_galleryImg = p.createGraphics(_movieComposite.width, _movieComposite.height, P.OPENGL);
 			_galleryImg.smooth(OpenGLUtil.SMOOTH_LOW);
 		}
 		
@@ -187,27 +193,29 @@ extends PAppletHax{
 			return (PGraphics) _silhouette.debugKinectBuffer();
 		}
 		
-		public void setGallery(PImage img) {
+		public void updateWithImage(PImage img) {
+			// copy current gallery image to canvas
 			_galleryImg.beginDraw();
-			_galleryImg.clear();
-			float[] cropProps = ImageUtil.getOffsetAndSizeToCrop(p.width * SCALE_DOWN, p.height * SCALE_DOWN, img.width, img.height, true);
-			_galleryImg.image(img, cropProps[0], cropProps[1], cropProps[2], cropProps[3]);
+			float[] galleryCrop = ImageUtil.getOffsetAndSizeToCrop(p.width * SCALE_DOWN, p.height * SCALE_DOWN, img.width, img.height, true);
+			_galleryImg.image(img, galleryCrop[0], galleryCrop[1], galleryCrop[2], galleryCrop[3]);
 			_galleryImg.endDraw();
-		}
-		
-		public void update() {
+
+			// kinect silhouette 
 			if(_cropProps == null) {
 				if(_silhouette._canvas.width != 0 && _silhouette._canvas.height != 0) {
-					_cropProps = ImageUtil.getOffsetAndSizeToCrop(p.width * SCALE_DOWN, p.height * SCALE_DOWN, _silhouette._canvas.width, _silhouette._canvas.height, true);
+					_cropProps = ImageUtil.getOffsetAndSizeToCrop(p.width * SCALE_DOWN, p.height * SCALE_DOWN, _silhouette._canvas.width, _silhouette._canvas.height, true).clone();
 				}
 			}
 			_silhouette.update(false);
 			
+			// draw kinect mask to canvas
 			_particleMask.beginDraw();
-			_particleMask.clear();
-			if(_silhouette._canvas != null) _particleMask.image(_silhouette._canvas, _cropProps[0], _cropProps[1], _cropProps[2], _cropProps[3]);
+			if(_silhouette._canvas != null) {
+				_particleMask.image(_silhouette._canvas, _cropProps[0], _cropProps[1], _cropProps[2], _cropProps[3]);
+			}
 			_particleMask.endDraw();
 			
+			// apply mask to gallery image
 			_galleryImg.mask(_particleMask);
 		}
 	}
