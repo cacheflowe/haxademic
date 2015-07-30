6a0c2a5e661d3fa0f6af2ac2d4c3333a0a0d5b99
cacheflowe <cacheflowe@cacheflowe.com>
2015-07-30 00:58:41 -0600

    Add new time stepping modes

--- src/com/haxademic/app/haxmapper/textures/TextureShaderTimeStepper.java
+++ src/com/haxademic/app/haxmapper/textures/TextureShaderTimeStepper.java
@@ -14,6 +14,13 @@ import processing.opengl.PShader;
 public class TextureShaderTimeStepper
 extends BaseTexture {
 
+	public enum ShaderTimeMode {
+		BeatEaseOut,
+		BeatSpeedUp,
+		DirectionSpeedShift,
+		ForwardOsc
+	}
+	
 	protected PGraphics _image;
 	protected PShader _patternShader;
 	protected int _timingFrame = 0;
@@ -23,8 +30,10 @@ extends BaseTexture {
 	protected float _smallTimeStep = 1f;
 	protected float _largeTimeStep = 3f;
 	protected float _nonBeatSpeed = 0.1f;
-	protected boolean _nonBeatTimeMode = false;
-	protected float _reverseThreshold = 100f;
+	protected float _beatSpeedUp = 0.1f;
+	protected float _beatSpeedUpMax = 0.1f;
+	protected ShaderTimeMode _nonBeatTimeMode = ShaderTimeMode.BeatEaseOut;
+	protected float _reverseTimeThreshold = 100f;
 	
 	protected float[] locations;
 	protected float[] colors;
@@ -58,10 +67,18 @@ extends BaseTexture {
 	}
 
 	public void updateDraw() {
-		updateShaders();
+		// updateShaders();
 		// _texture.background(0,255,0);
+		
+		updateTime();
+		
+		_patternShader.set("time", _timeEaser.value() );
+		_patternShader.set("mode", _mode);
 		_texture.filter( _patternShader );
 		SaturationFilter.instance(P.p).applyTo(_texture);
+		
+		_brightEaser.update();
+		BrightnessFilter.instance(P.p).setBrightness(_brightEaser.value());
 		BrightnessFilter.instance(P.p).applyTo(_texture);
 	}
 	
@@ -71,32 +88,46 @@ extends BaseTexture {
 		if( _active == true && wasActive == false ) {
 			_timeEaser.setCurrent( 0.0001f );
 			_timeEaser.setTarget( 0.0001f );
-			_nonBeatTimeMode = MathUtil.randRangeDecimal(0, 1f) > 0.8f;
+			// pick new time mode
+			float newTimeMode = MathUtil.randRange(0, 3);
+			if(newTimeMode == 0) _nonBeatTimeMode = ShaderTimeMode.BeatEaseOut;
+			else if(newTimeMode == 1) _nonBeatTimeMode = ShaderTimeMode.BeatSpeedUp;
+			else if(newTimeMode == 2) _nonBeatTimeMode = ShaderTimeMode.DirectionSpeedShift;
+			else if(newTimeMode == 3) _nonBeatTimeMode = ShaderTimeMode.ForwardOsc;
 		}
 	}
 	
-	protected void updateShaders() {
-		if(_nonBeatTimeMode == true) {
+	protected void updateTime() {
+		if(_nonBeatTimeMode == ShaderTimeMode.DirectionSpeedShift) {
 			_timeEaser.setCurrent( _timeEaser.value() + _nonBeatSpeed );
 			_timeEaser.setTarget( _timeEaser.value() + _nonBeatSpeed );			
-		} else {
+		} else if(_nonBeatTimeMode == ShaderTimeMode.BeatEaseOut) {
 			_timeEaser.update();
+		} else if(_nonBeatTimeMode == ShaderTimeMode.BeatSpeedUp) {
+			_timeEaser.setCurrent( _timeEaser.value() + _beatSpeedUp );
+			_timeEaser.setTarget( _timeEaser.value() + _beatSpeedUp );
+			_beatSpeedUp *= 1.2f;
+//			P.println("_beatSpeedUp", _beatSpeedUp);
+			if(Math.abs(_beatSpeedUp) > _beatSpeedUpMax) _beatSpeedUp = P.constrain(_beatSpeedUp, -_beatSpeedUpMax, _beatSpeedUpMax);
+		} else if(_nonBeatTimeMode == ShaderTimeMode.ForwardOsc) {
+			_timeEaser.setCurrent( _timeEaser.value() + _nonBeatSpeed * 0.75f + _nonBeatSpeed * 0.25f * P.sin(P.p.frameCount/40f) );
+			_timeEaser.setTarget( _timeEaser.value() + _nonBeatSpeed );
 		}
 		
 		// switch time directions
-		if( Math.abs(_timeEaser.value()) > _reverseThreshold ) {
-			_timeEaser.setCurrent( ( _timeEaser.value() > _reverseThreshold ) ? _reverseThreshold : -_reverseThreshold );
+		if( Math.abs(_timeEaser.value()) > _reverseTimeThreshold ) {
+			_timeEaser.setCurrent( ( _timeEaser.value() > _reverseTimeThreshold ) ? _reverseTimeThreshold : -_reverseTimeThreshold );
 			_largeTimeStep = _largeTimeStep * -1f;
 			_smallTimeStep = _smallTimeStep * -1f;
 			_nonBeatSpeed = _nonBeatSpeed * -1f;
+			_beatSpeedUp = _beatSpeedUp * -1f;
 			_timeEaser.setTarget( _timeEaser.value() + _largeTimeStep );
 			_timeEaser.update();
 			_timeEaser.update();
 		}
-
-
-		_patternShader.set("time", _timeEaser.value() );
-		_patternShader.set("mode", _mode);
+	}
+	
+	protected void updateShaders() {
 		_patternShader.set("mouse", (float)P.p.mouseX, P.p.height - (float)P.p.mouseY);
 		for(int i=0; i < locations.length; i++) {
 			locations[i] += 10f * (-0.5f + P.p.noise(i*10f+P.p.frameCount));
@@ -108,19 +139,35 @@ extends BaseTexture {
 		_patternShader.set("colors", colors);
 
 
-		_brightEaser.update();
-		BrightnessFilter.instance(P.p).setBrightness(_brightEaser.value());
 	}
 	
 	public void updateTiming() {
+		// handle 3 modes
+		if(_nonBeatTimeMode == ShaderTimeMode.BeatEaseOut) {
+			if( _timingFrame % 4 == 0 ) {
+				_timeEaser.setTarget( _timeEaser.value() + _largeTimeStep );
+			} else {
+				_timeEaser.setTarget( _timeEaser.value() + _smallTimeStep );
+			}
+		} else if(_nonBeatTimeMode == ShaderTimeMode.DirectionSpeedShift) {
+			_nonBeatSpeed = MathUtil.randRangeDecimal(-_smallTimeStep/10f, _smallTimeStep/10f);
+		} else if(_nonBeatTimeMode == ShaderTimeMode.BeatSpeedUp) {
+			if(_nonBeatTimeMode == ShaderTimeMode.BeatSpeedUp) {
+				if(_beatSpeedUp > 0) _beatSpeedUp = 0.001f;
+				else _beatSpeedUp = -0.001f;
+			}
+		} else if(_nonBeatTimeMode == ShaderTimeMode.ForwardOsc) {
+			if( _timingFrame % 4 == 0 ) {
+				_nonBeatSpeed = MathUtil.randRangeDecimal(-_smallTimeStep/10f, _smallTimeStep/10f);
+			}
+		}
+		
+		// update brightness filter
 		if( _timingFrame % 4 == 0 ) {
 			_brightEaser.setCurrent(1.3f);
-			_timeEaser.setTarget( _timeEaser.value() + _largeTimeStep );
 		} else {
 			_brightEaser.setCurrent(1.0f);
-			_timeEaser.setTarget( _timeEaser.value() + _smallTimeStep );
 		}
-		_nonBeatSpeed = MathUtil.randRangeDecimal(-_smallTimeStep/10f, _smallTimeStep/10f);
 		_brightEaser.setTarget(0.25f);
 		_timingFrame++;
 	}
