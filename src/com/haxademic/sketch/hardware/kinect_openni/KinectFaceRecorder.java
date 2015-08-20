7ec53b9f675ca4444966be849952c752ded5d069
cacheflowe <cacheflowe@cacheflowe.com>
2015-08-20 10:45:33 -0600

    Wrap up demo

--- src/com/haxademic/sketch/hardware/kinect_openni/KinectFaceRecorder.java
+++ src/com/haxademic/sketch/hardware/kinect_openni/KinectFaceRecorder.java
@@ -2,72 +2,87 @@ package com.haxademic.sketch.hardware.kinect_openni;
 
 import java.util.ArrayList;
 
+import com.haxademic.app.haxmapper.HaxMapper;
 import com.haxademic.core.app.P;
-import com.haxademic.core.app.PAppletHax;
-import com.haxademic.core.debug.DebugUtil;
 import com.haxademic.core.draw.color.ColorHaxEasing;
-import com.haxademic.core.draw.util.DrawUtil;
-import com.haxademic.core.draw.util.OpenGLUtil;
 import com.haxademic.core.hardware.dmx.DmxInterface;
 import com.haxademic.core.hardware.kinect.KinectSize;
 import com.haxademic.core.image.MotionBlurPGraphics;
-import com.haxademic.core.image.filters.PixelFilter;
+import com.haxademic.core.image.filters.shaders.BadTVLinesFilter;
+import com.haxademic.core.image.filters.shaders.BrightnessFilter;
+import com.haxademic.core.image.filters.shaders.WobbleFilter;
 import com.haxademic.core.math.MathUtil;
 import com.haxademic.core.system.FileUtil;
 
-import controlP5.ControlP5;
 import processing.core.PGraphics;
 import processing.core.PImage;
 import processing.opengl.PShader;
 
 
-@SuppressWarnings("serial")
-public class KinectFaceRecorder 
-extends PAppletHax {
+public class KinectFaceRecorder {
 
-	public static final float PIXEL_SIZE = 10;
+	public static final float PIXEL_SIZE = 20;
 	public static final int KINECT_TOP = 0;
 	public static final int KINECT_BOTTOM = 480;
-	public static final int KINECT_CLOSE = 100;
+	public static final int KINECT_CLOSE = 0;
 	public static final int KINECT_FAR = 600;
 	
-	protected PixelFilter _pixelFilter;
+	protected final int MAX_SESSIONS = 3;
 	
-	protected int _savedFramePlaybackIndex = 0;
+	protected int _playbackIndexSession = 0;
+	protected int _playbackIndexFrame = 0;
+	protected int _curSessionIndexFrame = 0;
 	protected boolean _isRecording = false;
-	protected ArrayList<PImage> _savedFrames;
-	protected MotionBlurPGraphics _motionBlur;
-	protected PGraphics _pg;
+	protected ArrayList<FaceSession> _faceSessions;
+	protected FaceSession _curFaceSession;
+	protected MotionBlurPGraphics _motionBlurPlayback;
+	protected MotionBlurPGraphics _motionBlurCurrentSession;
+	protected PGraphics _realtimeTexture;
+	public static PGraphics curSessionTexture;
+	public static PGraphics playbackTexture;
 
 	PShader _chromaKeyFilter;
-	protected ControlP5 _cp5;
-	public float thresholdSensitivity;
-	public float smoothing;
-	public float colorToReplaceR;
-	public float colorToReplaceG;
-	public float colorToReplaceB;
+//	protected ControlP5 _cp5;
+//	public float thresholdSensitivity;
+//	public float smoothing;
+//	public float colorToReplaceR;
+//	public float colorToReplaceG;
+//	public float colorToReplaceB;
 	
 	protected DmxInterface _dmxInterface;
 	protected ColorHaxEasing _lightColor1;
 	protected ColorHaxEasing _lightColor2;
+	
+	protected HaxMapper haxMapper;
 
-
-	public void setup() {
-		super.setup();
-		_pixelFilter = new PixelFilter(KinectSize.WIDTH, KinectSize.WIDTH, (int)PIXEL_SIZE);
+	public KinectFaceRecorder(HaxMapper haxMapper) {
+		this.haxMapper = haxMapper;
 		
-		_savedFrames = new ArrayList<PImage>();
-		_savedFramePlaybackIndex = 0;
+		_playbackIndexSession = 0;
+		_playbackIndexFrame = 0;
 
-		_pg = p.createGraphics( p.width, p.height, P.OPENGL );
-		_pg.smooth(OpenGLUtil.SMOOTH_HIGH);
+		// temporary canvas to preprocess frames before storing them. also used to display the realtime texture 
+		playbackTexture = P.p.createGraphics( 320, 240, P.OPENGL );
+		playbackTexture.smooth();
+		
+		curSessionTexture = P.p.createGraphics( 320, 240, P.OPENGL );
+		curSessionTexture.smooth();
 
-		_motionBlur = new MotionBlurPGraphics(20);
+		_realtimeTexture = P.p.createGraphics( 320, 240, P.OPENGL );
+		_realtimeTexture.smooth();
+		
+		_faceSessions = new ArrayList<FaceSession>();
+		
+		_motionBlurPlayback = new MotionBlurPGraphics(14);
+		_motionBlurCurrentSession = new MotionBlurPGraphics(7);
 		
 		setupChromakey();
 		setupDmxLights();
 	}
 		
+	public boolean isRecording() {
+		return _isRecording;
+	}
 		
 	protected void setupDmxLights() {
 		_dmxInterface = new DmxInterface(2);
@@ -76,118 +91,187 @@ extends PAppletHax {
 	}
 	
 	protected void setupChromakey() {
-		_cp5 = new ControlP5(this);
-		int cp5W = 160;
-		int cp5X = 20;
-		int cp5Y = 20;
-		int cp5YSpace = 40;
-		_cp5.addSlider("thresholdSensitivity").setPosition(cp5X,cp5Y).setWidth(cp5W).setRange(0,1f).setValue(0.75f);
-		_cp5.addSlider("smoothing").setPosition(cp5X,cp5Y+=cp5YSpace).setWidth(cp5W).setRange(0,1f).setValue(0.26f);
-		_cp5.addSlider("colorToReplaceR").setPosition(cp5X,cp5Y+=cp5YSpace).setWidth(cp5W).setRange(0,1f).setValue(0.29f);
-		_cp5.addSlider("colorToReplaceG").setPosition(cp5X,cp5Y+=cp5YSpace).setWidth(cp5W).setRange(0,1f).setValue(0.93f);
-		_cp5.addSlider("colorToReplaceB").setPosition(cp5X,cp5Y+=cp5YSpace).setWidth(cp5W).setRange(0,1f).setValue(0.14f);
+//		_cp5 = new ControlP5(this);
+//		int cp5W = 160;
+//		int cp5X = 20;
+//		int cp5Y = 20;
+//		int cp5YSpace = 40;
+//		_cp5.addSlider("thresholdSensitivity").setPosition(cp5X,cp5Y).setWidth(cp5W).setRange(0,1f).setValue(0.75f);
+//		_cp5.addSlider("smoothing").setPosition(cp5X,cp5Y+=cp5YSpace).setWidth(cp5W).setRange(0,1f).setValue(0.26f);
+//		_cp5.addSlider("colorToReplaceR").setPosition(cp5X,cp5Y+=cp5YSpace).setWidth(cp5W).setRange(0,1f).setValue(0.29f);
+//		_cp5.addSlider("colorToReplaceG").setPosition(cp5X,cp5Y+=cp5YSpace).setWidth(cp5W).setRange(0,1f).setValue(0.93f);
+//		_cp5.addSlider("colorToReplaceB").setPosition(cp5X,cp5Y+=cp5YSpace).setWidth(cp5W).setRange(0,1f).setValue(0.14f);
 
-		_chromaKeyFilter = loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/chroma-gpu.glsl" );
-		_chromaKeyFilter.set("thresholdSensitivity", thresholdSensitivity);
-		_chromaKeyFilter.set("smoothing", smoothing);
-		_chromaKeyFilter.set("colorToReplace", colorToReplaceR, colorToReplaceG, colorToReplaceB);
+		_chromaKeyFilter = P.p.loadShader( FileUtil.getHaxademicDataPath()+"shaders/filters/chroma-color.glsl" );
+		_chromaKeyFilter.set("thresholdSensitivity", 0.75f);
+		_chromaKeyFilter.set("smoothing", 0.25f);
+		_chromaKeyFilter.set("colorToReplace", 0.2f, 0.4f, 0.1f);
 	}
 
-	protected void overridePropsFile() {
-		_appConfig.setProperty( "rendering", "false" );
-		_appConfig.setProperty( "kinect_active", "true" );
-		_appConfig.setProperty( "width", "640" );
-		_appConfig.setProperty( "height", "480" );
-	}
-	
-	public void drawApp() {
+	public void update(boolean isRecordingNow, boolean isPlaybackActive) {
+//		_chromaKeyFilter.set("thresholdSensitivity", thresholdSensitivity);
+//		_chromaKeyFilter.set("smoothing", smoothing);
+//		_chromaKeyFilter.set("colorToReplace", 0, 0, colorToReplaceB);
+		
+		
 		runDmxLights();
-		
-		_chromaKeyFilter.set("thresholdSensitivity", thresholdSensitivity);
-		_chromaKeyFilter.set("smoothing", smoothing);
-		_chromaKeyFilter.set("colorToReplace", colorToReplaceR, colorToReplaceG, colorToReplaceB);
+		if(isPlaybackActive == true) playBackSavedFrames();
+		if(isRecordingNow == true) playBackCurSessionFrames();
+		if(isRecordingNow == true) copyCameraToRealtimeTexture();
+		detectAnyFace();
+		if(isRecordingNow == true) saveFramesWhileRecording();
 
-//		DrawUtil.resetGlobalProps( p );
-//		p.shininess(1000f); 
-//		p.lights();
-		p.background(0);
-		
-		// draw filtered web cam
-		DrawUtil.setDrawCorner(p);
-		DrawUtil.setColorForPImage(p);
-		
-//		p.image( p.kinectWrapper.getRgbImage(), 0, 0, 320, 240 );
-		PImage frame = p.kinectWrapper.getRgbImage();
-
-		// loop through kinect data within player's control range
-		p.stroke(255, 127);
-		float pixelDepth;
-		int numPixels = 0;
-		for ( int x = 0; x < KinectSize.WIDTH; x += PIXEL_SIZE ) {
-			for ( int y = KINECT_TOP; y < KINECT_BOTTOM; y += PIXEL_SIZE ) {
-				pixelDepth = p.kinectWrapper.getMillimetersDepthForKinectPixel( x, y );
-				if( pixelDepth != 0 && pixelDepth > KINECT_CLOSE && pixelDepth < KINECT_FAR ) {
-					p.pushMatrix();
-//					p.fill(((pixelDepth - KINECT_CLOSE) / (KINECT_FAR - KINECT_CLOSE)) * 255f);
-					p.fill(frame.get(x, y));
-					p.translate(x, y); // pixelDepth/10f
-					// p.rect(0, 0, PIXEL_SIZE, PIXEL_SIZE);
-					p.popMatrix();
-					
-					numPixels++;
-				}
-			}
-		}
-		
-		// record
-		if(numPixels > 100) {
-			if(_isRecording == false) {
-				_isRecording = true;
-				_lightColor1.setTargetColorInt(newDmxColor());
-				_lightColor2.setTargetColorInt(newDmxColor());
-			}
-		} else {
-			_isRecording = false;
-			_lightColor1.setTargetColorInt(p.color(0));
-			_lightColor2.setTargetColorInt(p.color(0));
-		}
-		if(_isRecording == true && p.frameCount % 20 == 0) {
-			if(_savedFrames.size() < 500) {
-				_savedFrames.add(frame.get());
-				P.println("_savedFrames: ",_savedFrames.size());
-			}
-		}
-		
-		// playback
-		if(_savedFrames.size() > 0) {
-			if(p.frameCount % 3 == 0) _savedFramePlaybackIndex++;
-			if(_savedFramePlaybackIndex > _savedFrames.size() - 1) {
-				_savedFramePlaybackIndex = 0;
-			}
-//			p.image(_savedFrames.get(_savedFramePlaybackIndex), 0, 0);
-			
-			_pg.beginDraw();
-			_pg.clear();
-			_pg.image(_savedFrames.get(_savedFramePlaybackIndex), 0, 0);
-			_pg.endDraw();
-			// _pg.filter(_chromaKeyFilter);
-
-			
-			_motionBlur.updateToCanvas(_pg, p.g, 0.5f);
-		}
-		
+		// debug draw realtime camera
+//		p.image(_realtimeTexture, 640, 0);
 		
 		// hide ControlP5
-		p.translate(0, 9000);
+		// p.translate(0, 9000);
 		
 		// debug memory info
 		// if(p.frameCount % 30 == 0) DebugUtil.showMemoryUsage();
 	}
 	
+	protected void copyCameraToRealtimeTexture() {
+		_realtimeTexture.beginDraw();
+		// _realtimeTexture.copy(p.kinectWrapper.getRgbImage(), 0, 0, 640, 480, 0, 0, _realtimeTexture.width, _realtimeTexture.height);
+		_realtimeTexture.copy(P.p.kinectWrapper.getRgbImage(), 160, 120, 320, 240, 0, 0, _realtimeTexture.width, _realtimeTexture.height);
+		_realtimeTexture.endDraw();
+		_realtimeTexture.filter(_chromaKeyFilter);
+	}
+
+	protected void detectAnyFace() {
+		// loop through kinect data within player's control range
+		float pixelDepth;
+		int numPixels = 0;
+		for ( int x = 0; x < KinectSize.WIDTH; x += PIXEL_SIZE ) {
+			for ( int y = KINECT_TOP; y < KINECT_BOTTOM; y += PIXEL_SIZE ) {
+				pixelDepth = P.p.kinectWrapper.getMillimetersDepthForKinectPixel( x, y );
+				if( pixelDepth != 0 && pixelDepth > KINECT_CLOSE && pixelDepth < KINECT_FAR ) {
+					numPixels++;
+				}
+			}
+		}
+		
+		// detect a person for record mode
+		if(numPixels > 30) {
+			startRecording();
+		} else {
+			stopRecording();
+		}
+	}
+	
+	protected void saveFramesWhileRecording() {
+		// save frames while recording
+		if(_isRecording == true && P.p.frameCount % 20 == 0) {
+			_curFaceSession.addFrame(_realtimeTexture.get());
+		}
+	}
+	
+	protected void startRecording() {
+		if(_isRecording == false) {
+			_isRecording = true;
+			_curFaceSession = new FaceSession();
+			_motionBlurCurrentSession.clearFrames();
+			if(_faceSessions.size() > MAX_SESSIONS && _playbackIndexSession != 0) {
+				_faceSessions.get(0).disposeFrames();
+				_faceSessions.remove(0);
+			}
+			_lightColor1.setTargetColorInt(newDmxColor());
+			_lightColor2.setTargetColorInt(newDmxColor());
+			
+			if(haxMapper != null) haxMapper.startFaceRecording();
+		}
+	}
+	
+	protected void stopRecording() {
+		if(_isRecording == true) {
+			_isRecording = false;
+			_curFaceSession.endRecordSession();
+			_faceSessions.add(_curFaceSession);
+			_curFaceSession = null;
+			_lightColor1.setTargetColorInt(P.p.color(0));
+			_lightColor2.setTargetColorInt(P.p.color(0));
+			
+			if(haxMapper != null) haxMapper.stopFaceRecording();
+		}
+	}
+	
+	protected void playBackCurSessionFrames() {
+		if(_curFaceSession != null) {			
+			// step through sessions & their frames every x frames
+			if(P.p.frameCount % 3 == 0) {
+				_curSessionIndexFrame++;
+				if(_curSessionIndexFrame >= _curFaceSession.frameCount()) {
+					_curSessionIndexFrame = 0;
+				}
+			}
+						
+			// draw current playback frame
+			if(_curFaceSession.frameCount() > 0) {
+				// draw playback to screen
+				curSessionTexture.beginDraw();
+				curSessionTexture.clear();
+				_motionBlurCurrentSession.updateToCanvas(_curFaceSession.getFrame(_curSessionIndexFrame), curSessionTexture, 0.5f);
+				curSessionTexture.endDraw();
+				
+				BrightnessFilter.instance(P.p).setBrightness(1.75f + 0.25f * P.sin(P.p.frameCount/20f));
+				BrightnessFilter.instance(P.p).applyTo(curSessionTexture);
+				WobbleFilter.instance(P.p).setTime(P.p.frameCount/10f);
+				WobbleFilter.instance(P.p).setSpeed(0.8f);
+				WobbleFilter.instance(P.p).setStrength(0.004f + 0.004f * P.sin(P.p.frameCount/25f));
+				WobbleFilter.instance(P.p).setSize(100f);
+				WobbleFilter.instance(P.p).applyTo(curSessionTexture);
+				BadTVLinesFilter.instance(P.p).applyTo(curSessionTexture);
+
+			} 
+		}
+		// p.image(_curSessionTexture, 320, 0);
+	}
+	
+	protected void playBackSavedFrames() {
+		if(_faceSessions.size() > 0) {
+			// _pg.filter(_chromaKeyFilter);
+			if(_playbackIndexSession >= _faceSessions.size()) _playbackIndexSession = 0;
+			FaceSession curSession = _faceSessions.get(_playbackIndexSession);
+
+			// step through sessions & their frames every x frames
+			if(P.p.frameCount % 8 == 0) {
+				_playbackIndexFrame++;
+				if(_playbackIndexFrame >= curSession.frameCount()) {
+					_playbackIndexFrame = 0;
+					_playbackIndexSession++;
+					if(_playbackIndexSession >= _faceSessions.size()) _playbackIndexSession = 0;
+					
+					// clean up old sessions
+					if(_faceSessions.size() > MAX_SESSIONS && _playbackIndexSession == 0) {
+						_faceSessions.get(0).disposeFrames();
+						_faceSessions.remove(0);
+					}
+					
+					// make sure we have a fresh reference to the current session
+					curSession = _faceSessions.get(_playbackIndexSession);
+					_playbackIndexFrame = 0;
+				}
+			}
+			
+			// P.println(_playbackIndexSession, _playbackIndexFrame);
+			
+			// draw current playback frame
+			if(curSession.frameCount() > 0) {
+				// draw playback to screen
+				playbackTexture.beginDraw();
+				playbackTexture.clear();
+				_motionBlurPlayback.updateToCanvas(curSession.getFrame(_playbackIndexFrame), playbackTexture, 0.5f);
+				playbackTexture.endDraw();
+				// p.image(_playbackTexture, 0, 0);
+			}
+		}
+	}
+	
 	protected int newDmxColor() {
-		return p.color(
+		return P.p.color(
 				MathUtil.randRangeDecimal(180, 255), 
-				MathUtil.randRangeDecimal(180, 255), 
+				MathUtil.randRangeDecimal(220, 255), 
 				MathUtil.randRangeDecimal(180, 255)
 		);
 	}
@@ -202,11 +286,36 @@ extends PAppletHax {
 	
 	public class FaceSession {
 		
+		protected int MAX_SAVED_FRAMES = 3;
+		protected int MAX_CUR_FRAMES = 6;
+		
 		protected ArrayList<PImage> _capturedFrames;
 
 		public FaceSession() {
 			_capturedFrames = new ArrayList<PImage>();
 		}
 		
+		public int frameCount() {
+			return _capturedFrames.size();
+		}
+		
+		public PImage getFrame(int index) {
+			if(index >= frameCount()) index = 0; 
+			return _capturedFrames.get(index);
+		}
+		
+		public void addFrame(PImage newFrame) {
+			if(_capturedFrames.size() > MAX_CUR_FRAMES) _capturedFrames.remove(0);
+			_capturedFrames.add(newFrame);
+		}
+		
+		public void endRecordSession() {
+			// keep only a few random frames
+			while(_capturedFrames.size() > MAX_SAVED_FRAMES) _capturedFrames.remove(MathUtil.randRange(0, _capturedFrames.size() - 1));
+		}
+		
+		public void disposeFrames() {
+			_capturedFrames.clear();
+		}
 	}
 }
