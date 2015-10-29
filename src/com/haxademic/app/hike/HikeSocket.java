099e03c4daf3c5c6a7e0757c8126c2d72d20b57d
cacheflowe <cacheflowe@cacheflowe.com>
2015-10-28 18:13:51 -0600

    Add Hike demos

---
+++ src/com/haxademic/app/hike/HikeSocket.java
@@ -0,0 +1,95 @@
+package com.haxademic.app.hike;
+
+import java.util.ArrayList;
+
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.components.IMouseable;
+import com.haxademic.core.components.TextButton;
+import com.haxademic.core.components.TextInput;
+import com.haxademic.core.draw.util.DrawUtil;
+import com.haxademic.core.net.WebSocketRelay;
+
+import processing.data.JSONObject;
+
+
+@SuppressWarnings("serial")
+public class HikeSocket 
+extends PAppletHax {
+	
+	protected ArrayList<IMouseable> _mouseables;
+	
+	protected WebSocketRelay _server;
+	protected int _userFoundTime = -1;
+	
+	protected void overridePropsFile() {
+		_appConfig.setProperty( "width", "640" );
+		_appConfig.setProperty( "height", "640" );
+	}
+	
+	public void setup() {
+		super.setup();
+		
+		// fire up websocket server
+		_server = new WebSocketRelay();
+		_server.start();
+		
+		// build buttons
+		_mouseables = new ArrayList<IMouseable>();
+		_mouseables.add( new TextButton( p, "ACTIVE", "1", 20, 20, 180, 40 ) );
+		_mouseables.add( new TextButton( p, "INACTIVE", "2", 20, 120, 180, 40 ) );
+		_mouseables.add( new TextButton( p, "CAPTURE", "3", 20, 220, 180, 40 ) );
+
+	}
+
+	public void drawApp() {
+		// reset drawing 
+		p.background(0);
+		DrawUtil.resetGlobalProps( p );
+		DrawUtil.setDrawCorner(p);
+		DrawUtil.setColorForPImage(p);
+		p.noStroke();
+		
+		// draw buttons
+		for( int i=0; i < _mouseables.size(); i++ ) {
+			_mouseables.get(i).update( p );
+		}
+	}
+	
+	public void sendSocketMessage(String type, String message) {
+	    JSONObject jsonOut = new JSONObject();
+	    jsonOut.setString("type", type);
+	    jsonOut.setString("text", message);
+	    _server.sendMessage(jsonOut.toString().replaceAll("[\r\n]+", " ").replaceAll("\\s+", " "));
+	}
+	
+	public void keyPressed() {
+		super.keyPressed();
+		if (p.key == ' ') {
+//			_kinectGrid.toggleDebugOverhead();
+		}
+	}
+
+	public void mouseReleased() {
+		for( int i=0; i < _mouseables.size(); i++ ) {
+			if( _mouseables.get(i).checkRelease( p.mouseX, p.mouseY ) ) {
+				if(_mouseables.get(i).id() == "1") sendSocketMessage("user", "active");
+				if(_mouseables.get(i).id() == "2") sendSocketMessage("user", "inactive");
+				if(_mouseables.get(i).id() == "3") sendSocketMessage("capture", "take some pictures!");
+			}
+		}
+	}
+
+	public void mousePressed() {
+		for( int i=0; i < _mouseables.size(); i++ ) {
+			_mouseables.get(i).checkPress( p.mouseX, p.mouseY );
+		}
+	}
+
+	public void mouseMoved() {
+		for( int i=0; i < _mouseables.size(); i++ ) {
+			_mouseables.get(i).checkOver( p.mouseX, p.mouseY );
+		}
+	}
+
+}
+
---
+++ src/com/haxademic/sketch/test/VideoHDPlayer.java
@@ -0,0 +1,84 @@
+package com.haxademic.sketch.test;
+
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.draw.util.DrawUtil;
+import com.haxademic.core.image.ImageUtil;
+import com.haxademic.core.system.FileUtil;
+
+import processing.core.PGraphics;
+import processing.core.PImage;
+import processing.opengl.PShader;
+import processing.video.Movie;
+
+@SuppressWarnings("serial")
+public class VideoHDPlayer
+extends PAppletHax  
+{
+	protected Movie _movie;
+	protected Movie _movieColor;
+	protected float[] _cropProps = null;
+
+	PGraphics _maskImage;
+	PShader _maskShader;
+
+
+	protected void overridePropsFile() {
+		_appConfig.setProperty( "width", "1920" );
+		_appConfig.setProperty( "height", "1080" );
+	}
+
+	public void setup() {
+		super.setup();		
+		_movie = new Movie( p, FileUtil.getFile("video/nike/nike-hike-gray-loop.mov") );
+		_movieColor = new Movie( p, FileUtil.getFile("video/nike/nike-hike-color-loop.mov") );
+
+		_movie.jump(0);
+		_movie.loop();
+		_movie.play();
+
+		_movieColor.jump(0);
+		_movieColor.loop();
+		_movieColor.play();
+
+		_maskImage = createGraphics(p.width, p.height, P2D);
+		_maskImage.noSmooth();
+
+		_maskShader = loadShader(FileUtil.getFile("shaders/filters//mask.glsl"));
+		_maskShader.set("mask", _maskImage);
+
+
+	}
+
+	public void drawApp() {
+		// reset drawing 
+		p.background(0);
+		DrawUtil.resetGlobalProps( p );
+		DrawUtil.setDrawCorner(p);
+		DrawUtil.setColorForPImage(p);
+		p.noStroke();
+
+		// update mask
+		_maskImage.beginDraw();
+		_maskImage.background(0);
+		if (mouseX != 0 && mouseY != 0) {  
+			_maskImage.noStroke();
+			_maskImage.fill(255, 0, 0);
+			_maskImage.ellipse(mouseX, mouseY, 450, 450);
+		}
+		_maskImage.endDraw();
+		_maskShader.set("mask", _maskImage);
+
+
+		// draw movie
+		if(_movie.width > 1 && _movieColor.width > 1) {
+			_cropProps = ImageUtil.getOffsetAndSizeToCrop(p.width, p.height, _movie.width, _movie.height, true);
+			DrawUtil.setPImageAlpha(p, 1);
+			p.image(_movie, _cropProps[0], _cropProps[1], _cropProps[2], _cropProps[3]);
+			p.shader(_maskShader);
+			p.image(_movieColor, _cropProps[0], _cropProps[1], _cropProps[2], _cropProps[3]);
+			p.resetShader();
+		}
+
+	}
+
+}
