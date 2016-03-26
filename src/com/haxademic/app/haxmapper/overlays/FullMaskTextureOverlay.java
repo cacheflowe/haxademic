56b8f95624ed769163df4f7f09dcc674fe18785b
cacheflowe <cacheflowe@cacheflowe.com>
2016-03-25 23:56:47 -0600

    Simpler overlay texture image draw instead of mapped quad

--- src/com/haxademic/app/haxmapper/overlays/FullMaskTextureOverlay.java
+++ src/com/haxademic/app/haxmapper/overlays/FullMaskTextureOverlay.java
@@ -34,19 +34,21 @@ public class FullMaskTextureOverlay {
 
 	public void drawOverlay() {
 		if(_texture == null) return;
-		DrawUtil.setPImageAlpha(pg, 0.6f); 	// light opacity overlay. 
-		pg.beginShape(PConstants.QUAD);
-		pg.texture(_texture);
-		// crop to fill the mapped area with the current texture
+		DrawUtil.setPImageAlpha(pg, 0.65f); 	// light opacity overlay. 
+		DrawUtil.setDrawCorner(pg);
+//		pg.beginShape(PConstants.QUAD);
+//		pg.texture(_texture);
+//		// crop to fill the mapped area with the current texture
 		float left = bounds.x + cropPosOffset[0];
 		float top = bounds.y + cropPosOffset[1];
 		float right = left + cropPosOffset[2];
 		float bottom = top + cropPosOffset[3];
-		pg.vertex(left, top, 0, 		0, 0);
-		pg.vertex(right, top, 0, 		_texture.width, 0);
-		pg.vertex(right, bottom, 0, 	_texture.width, _texture.height);
-		pg.vertex(left, bottom, 0, 		0, _texture.height);
-		pg.endShape();
+//		pg.vertex(left, top, 0, 		0, 0);
+//		pg.vertex(right, top, 0, 		_texture.width, 0);
+//		pg.vertex(right, bottom, 0, 	_texture.width, _texture.height);
+//		pg.vertex(left, bottom, 0, 		0, _texture.height);
+//		pg.endShape();
+		pg.image(_texture, left, top, right-left, bottom-top);
 		DrawUtil.setPImageAlpha(pg, 1f);	// reset alpha for subsequent overlay drawing
 	}
 
