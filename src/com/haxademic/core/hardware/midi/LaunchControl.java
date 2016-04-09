182ef836fa8b8c9505c11e2eedb5c3621f34c2c8
Justin Gitlin <cacheflowe@cacheflowe.local>
2016-04-09 14:15:14 -0600

    Add LaunchControl MIDI mapping

---
+++ src/com/haxademic/core/hardware/midi/LaunchControl.java
@@ -0,0 +1,28 @@
+package com.haxademic.core.hardware.midi;
+
+public class LaunchControl {
+	public static int PAD_01 = 9;
+	public static int PAD_02 = 10;
+	public static int PAD_03 = 11;
+	public static int PAD_04 = 12;
+	public static int PAD_05 = 25;
+	public static int PAD_06 = 26;
+	public static int PAD_07 = 27;
+	public static int PAD_08 = 28;
+	public static int KNOB_01 = 41;
+	public static int KNOB_02 = 42;
+	public static int KNOB_03 = 43;
+	public static int KNOB_04 = 44;
+	public static int KNOB_05 = 45;
+	public static int KNOB_06 = 46;
+	public static int KNOB_07 = 47;
+	public static int KNOB_08 = 48;
+	public static int KNOB_09 = 21;
+	public static int KNOB_10 = 22;
+	public static int KNOB_11 = 23;
+	public static int KNOB_12 = 24;
+	public static int KNOB_13 = 25;
+	public static int KNOB_14 = 26;
+	public static int KNOB_15 = 27;
+	public static int KNOB_16 = 28;
+}
