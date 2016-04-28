a488d89703006a1e1341dca0dac811b4cab9ebd3
cacheflowe <cacheflowe@cacheflowe.com>
2016-04-28 09:54:00 -0600

    Mountains loop render

--- src/com/haxademic/sketch/shader/ShaderVertexTextureDeform2.java
+++ src/com/haxademic/sketch/shader/ShaderVertexTextureDeform2.java
@@ -3,6 +3,15 @@ package com.haxademic.sketch.shader;
 import com.haxademic.core.app.AppSettings;
 import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.draw.util.DrawUtil;
+import com.haxademic.core.image.filters.shaders.BadTVLinesFilter;
+import com.haxademic.core.image.filters.shaders.ColorDistortionFilter;
+import com.haxademic.core.image.filters.shaders.InvertFilter;
+import com.haxademic.core.image.filters.shaders.SphereDistortionFilter;
+import com.haxademic.core.image.filters.shaders.VignetteAltFilter;
+import com.haxademic.core.image.filters.shaders.VignetteFilter;
+import com.haxademic.core.image.filters.shaders.WobbleFilter;
+import com.haxademic.core.math.easing.Penner;
 import com.haxademic.core.system.FileUtil;
 
 import processing.core.PGraphics;
@@ -20,12 +29,12 @@ extends PAppletHax {
 	float angle;
 	PShader textureShader;
 	PShader displacementShader;
-	float _frames = 210;
-	float displaceAmp = 10f; 
+	float _frames = 610;
+	float displaceAmp = 200f; 
 
 
 	protected void overridePropsFile() {
-		p.appConfig.setProperty( AppSettings.WIDTH, 1000 );
+		p.appConfig.setProperty( AppSettings.WIDTH, 1600 );
 		p.appConfig.setProperty( AppSettings.HEIGHT, 1000 );
 		p.appConfig.setProperty( AppSettings.SMOOTHING, AppSettings.SMOOTH_HIGH );
 		p.appConfig.setProperty( AppSettings.RENDERING_MOVIE, false );
@@ -41,11 +50,12 @@ extends PAppletHax {
 	public void setup() {
 		super.setup();	
 
-		texture = createGraphics(p.width, p.height, P.P3D);
-		textureShader = P.p.loadShader( FileUtil.getFile("shaders/textures/basic-diagonal-stripes.glsl")); 
+		texture = createGraphics(p.height * 2, p.height * 2, P.P3D);
+		
+		textureShader = P.p.loadShader( FileUtil.getFile("shaders/textures/bw-clouds.glsl")); 
 		textureShader.set("time", 0 );
 
-		mesh = createSheet(200, texture);
+		mesh = createSheet(450, texture);
 		displacementShader = loadShader(
 			FileUtil.getFile("shaders/vertex/brightness-displace-frag-texture.glsl"), 
 			FileUtil.getFile("shaders/vertex/brightness-displace-sheet-vert.glsl")
@@ -57,28 +67,75 @@ extends PAppletHax {
 	public void drawApp() {
 		background(0);
 		
+//		OpenGLUtil.setWireframe(p.g, true);
+		
 		// rendering
 		float percentComplete = ((float)(p.frameCount%_frames)/_frames);
+		float easedPercent = Penner.easeInOutCubic(percentComplete, 0, 1, 1);
+		float easedPercentHard = Penner.easeInOutQuad(percentComplete, 0, 1, 1);
+
 		angle = P.TWO_PI * percentComplete;
 
 		// set center screen & rotate
-		translate(width/2, height/2);
-		scale(3.65f);
-		rotateX(P.PI * p.mouseY * 0.01f); 
+		translate(width/2, height*0.4f, -width/3);
+		scale(1.4f);
+//		rotateX(P.PI * p.mouseY * 0.01f); 
+		rotateX(1.09f); 
 //		rotateZ(percentComplete * P.TWO_PI); 
-		rotateZ(P.PI/4f); 
+		rotateZ(-P.PI/4f); 
 
 		// update generative texture
-		textureShader.set("time", 10f * P.sin( P.TWO_PI * percentComplete) );
+		textureShader.set("time", 5+ 7f * percentComplete);
 		texture.filter(textureShader);
+		WobbleFilter.instance(p).setTime( P.sin( P.TWO_PI * percentComplete));
+		WobbleFilter.instance(p).setStrength(0.02f * P.sin( P.TWO_PI * percentComplete));
+		WobbleFilter.instance(p).setSpeed(2.01f * P.sin( P.TWO_PI * percentComplete));
+		WobbleFilter.instance(p).applyTo(texture);
+		
+		texture.beginDraw();
+		postProcessForRendering();
+		texture.endDraw();
 
 		// set shader properties & set on processing context
-		displacementShader.set("displaceStrength", displaceAmp + displaceAmp * P.sin(percentComplete * P.TWO_PI));
+//		displacementShader.set("displaceStrength", displaceAmp + displaceAmp * P.sin(percentComplete * P.TWO_PI));
+		displacementShader.set("displaceStrength", displaceAmp);
 		shader(displacementShader);  
 		shape(mesh);
 		
 		// unset shader deformation
 		resetShader();
+		
+//		BadTVLinesFilter.instance(p).setTime(percentComplete * 3f);
+//		BadTVLinesFilter.instance(p).applyTo(p.g);
+		SphereDistortionFilter.instance(p).applyTo(p.g);
+		
+		InvertFilter.instance(p).applyTo(p.g);
+	}
+
+	protected void postProcessForRendering() {
+		// overlay
+		int transitionIn = 220;
+		int transition = 220;
+		DrawUtil.setDrawCorner(texture);
+		int curFrame = (int) (p.frameCount % _frames);
+		if(curFrame <= transitionIn) {
+//			P.println(P.map(curFrame, 1f, transitionIn, 18f, 8f));
+//			VignetteFilter.instance(p).setDarkness(P.map(curFrame, 1f, transitionIn, 18f, 8f));
+//			VignetteFilter.instance(p).setSpread(P.map(curFrame, 1f, transitionIn, 0.05f, 0.4f));
+//			VignetteFilter.instance(p).applyTo(texture);
+			texture.fill(0, P.map(curFrame, 1f, transition, 255f, 0));
+			texture.rect(0,0,texture.width, texture.height);
+		} else if(curFrame >= _frames - transition) {
+//			VignetteFilter.instance(p).setDarkness(P.map(curFrame, _frames - transition, _frames, 2f, 18f));
+//			VignetteFilter.instance(p).setSpread(P.map(curFrame, _frames - transition, _frames, 0.4f, 0.05f));
+//			VignetteFilter.instance(p).applyTo(texture);
+			texture.fill(0, P.map(curFrame, _frames - transition, _frames, 0, 255f));
+			texture.rect(0,0,texture.width, texture.height);
+//		} else {
+		}
+		VignetteFilter.instance(p).setDarkness(2f);
+		VignetteFilter.instance(p).setSpread(0.7f);
+		VignetteFilter.instance(p).applyTo(texture);
 	}
 
 
