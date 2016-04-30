95e77cb9ced30b910ef6b7e261cd7cb01ed9fdcb
cacheflowe <cacheflowe@cacheflowe.com>
2016-04-29 19:44:45 -0600

    Modernized the time lapse generator app

--- src/com/haxademic/app/timelapse/TimeLapse.java
+++ src/com/haxademic/app/timelapse/TimeLapse.java
@@ -1,52 +1,31 @@
 package com.haxademic.app.timelapse;
 
-import java.io.File;
 import java.util.ArrayList;
 
-import processing.core.PApplet;
-import processing.core.PImage;
-
+import com.haxademic.core.app.AppSettings;
+import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.draw.util.DrawUtil;
 import com.haxademic.core.system.FileUtil;
+
+import processing.core.PImage;
 
 public class TimeLapse
 extends PAppletHax {
 	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * Image sequence
-	 */
 	protected ArrayList<String> _images;
-	
-	/**
-	 * Image path
-	 */
 	protected String _imageDir;
-	
-	/**
-	 * Image type
-	 */
 	protected String _imageType;
-	
-	/**
-	 * Image sequence index
-	 */
 	protected int _imageIndex;
+	protected int _framesPerImage;
 	
-	/**
-	 * Frames per image
-	 */
-	protected int _fpi;
-	
-	public void settings() {
-		customPropsFile = FileUtil.getHaxademicDataPath() + "properties/timelapse.properties";
-		super.settings();
+	protected void overridePropsFile() {
+		p.appConfig.setProperty( AppSettings.WIDTH, 1080 );
+		p.appConfig.setProperty( AppSettings.HEIGHT, 1080 );
+		p.appConfig.setProperty( AppSettings.RENDERING_MOVIE, true );
+		p.appConfig.setProperty( "image_type", "jpg" );
+		p.appConfig.setProperty( "image_dir", "/Users/cacheflowe/Documents/workspace/mode_set/client_docs/legwork/nike-all-star/final_snkrs_xpress_game_faces/" );
 	}
 
 	public void setup() {
@@ -56,10 +35,11 @@ extends PAppletHax {
 
 	public void initRender() {
 		_imageDir = p.appConfig.getString( "image_dir", "" );
-		_imageType = p.appConfig.getString( "image_type", ".png" );
-		_images = FileUtil.getFilesInDirOfType( _imageDir, _imageType ); // FileUtil.getHaxademicDataPath() + 
+		_imageType = p.appConfig.getString( "image_type", "png" );
+		_images = FileUtil.getFilesInDirOfType( _imageDir, _imageType ); 
+		P.println(_images.size());
 		_imageIndex = 0;
-		_fpi = p.appConfig.getInt( "frames_per_image", 2 );
+		_framesPerImage = p.appConfig.getInt( "frames_per_image", 1 );
 	}
 		
 	public void drawApp() {
@@ -70,11 +50,11 @@ extends PAppletHax {
 		// load and display current image
 		if( _imageIndex < _images.size() ) {
 			PImage img = p.loadImage( _imageDir + _images.get( _imageIndex ) );
-			p.image( img, 0, 0 );
+			p.image( img, 0, 0, p.width, p.height );
 		}
 		
 		// step to next image
-		if( p.frameCount > 0 && p.frameCount % _fpi == 0 ) _imageIndex++;
+		if( p.frameCount > 0 && p.frameCount % _framesPerImage == 0 ) _imageIndex++;
 		
 		// stop when done
 		if( _imageIndex == _images.size() ) {
