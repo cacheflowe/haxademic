7ca4153851db32049360dab8945503e882b2f77c
Justin Gitlin <cacheflowe@cacheflowe.local>
2016-12-07 12:10:32 -0700

    Add ello gif redner sketches

---
+++ src/com/haxademic/sketch/render/ello/GifRenderEllo002ScaleEased.java
@@ -0,0 +1,75 @@
+package com.haxademic.sketch.render.ello;
+
+import com.haxademic.core.app.AppSettings;
+import com.haxademic.core.app.P;
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.draw.util.DrawUtil;
+import com.haxademic.core.draw.util.OpenGLUtil;
+import com.haxademic.core.image.AnimatedGifEncoder;
+import com.haxademic.core.math.easing.Penner;
+import com.haxademic.core.system.FileUtil;
+
+import processing.core.PConstants;
+import processing.core.PImage;
+import processing.core.PShape;
+
+public class GifRenderEllo002ScaleEased
+extends PAppletHax {
+	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
+	
+	AnimatedGifEncoder encoder;
+	PShape _logo;
+	PShape _logoInverse;
+	PImage _bread;
+	float _frames = 60;
+	
+	protected void overridePropsFile() {
+		p.appConfig.setProperty( AppSettings.WIDTH, "128" );
+		p.appConfig.setProperty( AppSettings.HEIGHT, "128" );
+
+		p.appConfig.setProperty( AppSettings.RENDERING_MOVIE, "false" );
+		p.appConfig.setProperty( AppSettings.RENDERING_GIF, "false" );
+		p.appConfig.setProperty( AppSettings.RENDERING_GIF_FRAMERATE, "40" );
+		p.appConfig.setProperty( AppSettings.RENDERING_GIF_QUALITY, "1" );
+		p.appConfig.setProperty( AppSettings.RENDERING_GIF_START_FRAME, "2" );
+		p.appConfig.setProperty( AppSettings.RENDERING_GIF_STOP_FRAME, ""+Math.round(_frames+1) );
+	}
+	
+	public void setup() {
+		super.setup();
+		_logo = p.loadShape(FileUtil.getHaxademicDataPath()+"svg/ello.svg");
+		_logoInverse = p.loadShape(FileUtil.getHaxademicDataPath()+"svg/ello-inverse.svg");
+		_bread = p.loadImage(FileUtil.getHaxademicDataPath()+"images/bread.png");
+	}
+
+	public void drawApp() {
+		p.background(255);
+//		p.fill(255, 40);
+//		p.rect(0, 0, p.width, p.height);
+		p.noStroke();
+		
+		float frameRadians = PConstants.TWO_PI / _frames;
+		float percentComplete = ((float)(p.frameCount%_frames)/_frames);
+		float easedPercent = Penner.easeInOutQuart(percentComplete, 0, 1, 1);
+
+		float frameOsc = P.sin( PConstants.TWO_PI * percentComplete);
+		float elloSize = (float)((p.width - p.width*0.02) + p.width*0.02 * frameOsc);
+//		float elloSize = (float)(p.width);
+		
+		
+		p.translate(p.width/2, p.height/2);
+//		p.rotate(frameRadians * p.frameCount);
+//		p.rotate(easedPercent * PConstants.TWO_PI);
+		
+		// Ello logo
+//		DrawUtil.setDrawCorner(p);
+//		p.shape(_logo, 0, 0, elloSize, elloSize);
+		
+		// Bread!
+		DrawUtil.setDrawCenter(p);
+		p.image(_bread, 0, 0, elloSize, elloSize);
+	}
+}
+
+
+
---
+++ src/com/haxademic/sketch/render/ello/GifRenderEllo026InsideCube.java
@@ -0,0 +1,55 @@
+package com.haxademic.sketch.render.ello;
+
+import com.haxademic.core.app.AppSettings;
+import com.haxademic.core.app.P;
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.draw.shapes.Shapes;
+import com.haxademic.core.image.filters.shaders.VignetteFilter;
+import com.haxademic.core.math.easing.Penner;
+import com.haxademic.core.system.FileUtil;
+
+import processing.core.PImage;
+
+public class GifRenderEllo026InsideCube 
+extends PAppletHax {
+	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
+
+	protected int _frames = 95;
+	PImage img;
+
+	protected void overridePropsFile() {
+		p.appConfig.setProperty( AppSettings.WIDTH, 1000 );
+		p.appConfig.setProperty( AppSettings.HEIGHT, 1000 );
+		p.appConfig.setProperty( AppSettings.SMOOTHING, AppSettings.SMOOTH_HIGH );
+		p.appConfig.setProperty( AppSettings.RENDERING_MOVIE, true );
+		p.appConfig.setProperty( AppSettings.RENDERING_MOVIE_START_FRAME, 1 );
+		p.appConfig.setProperty( AppSettings.RENDERING_MOVIE_STOP_FRAME, (int)_frames + 1 );
+	}
+
+	public void setup() {
+		super.setup();
+		img = loadImage(FileUtil.getHaxademicDataPath() + "images/ello.png");
+		noStroke();
+	}
+
+	public void drawApp() {
+		background(0);
+		translate(width/2, height/2, 0);
+		
+		float percentComplete = 2f * ((float)(p.frameCount%_frames)/_frames);
+		float easedPercent = Penner.easeInOutQuart(percentComplete % 1, 0, 1, 1);
+		float radsComplete = (easedPercent) * P.TWO_PI;
+
+		if(percentComplete < 1f) {
+			rotateX(P.PI); 
+			rotateY(radsComplete * 0.25f);
+		} else {
+			rotateX(P.PI); 
+			rotateZ(-radsComplete); 
+		}
+
+		Shapes.drawTexturedBox(p.g, 1200 + 0f * P.sin(P.PI + radsComplete), img);
+		
+		VignetteFilter.instance(p).applyTo(p);
+	}
+}
