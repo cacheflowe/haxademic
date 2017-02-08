4f98014bb40426a4801869d7a37f78d1ba605e6c
cacheflowe <cacheflowe@cacheflowe.com>
2017-02-08 10:21:29 -0700

    Add shader filter that leaves just black colors

---
+++ data/shaders/filters/leave-black.glsl
@@ -0,0 +1,23 @@
+// by cacheflowe
+// leaves black but turns other colors transparent
+
+#ifdef GL_ES
+precision mediump float;
+precision mediump int;
+#endif
+
+#define PROCESSING_TEXTURE_SHADER
+
+uniform sampler2D texture;
+varying vec4 vertColor;
+varying vec4 vertTexCoord;
+
+uniform float mix;
+
+void main() {
+    vec4 color = texture2D(texture, vertTexCoord.xy);
+    float grayColor = (color.r + color.g + color.b) / 3.;
+    grayColor = smoothstep(0., 0.2, grayColor); // quick falloff from full black
+    float alpha = 1. - (grayColor * mix);
+    gl_FragColor = vec4(vec3(color), alpha);
+}
