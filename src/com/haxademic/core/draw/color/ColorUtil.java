3cee5e8dd9053ba1b938a357925414f0cf0b2220
cacheflowe <cacheflowe@cacheflowe.com>
2017-02-26 12:02:44 -0700

    Add gradient component generator

--- src/com/haxademic/core/draw/color/ColorUtil.java
+++ src/com/haxademic/core/draw/color/ColorUtil.java
@@ -19,6 +19,14 @@ public class ColorUtil {
 		return percent * 255f;
 	}
 	
+	// concept from: http://dev.thi.ng/gradients/
+	// returns 0-255 w/default params: gradientComponent(0, 255f, 1f, 0, rads);
+	public static float gradientComponent(float dcOffset, float amp, float freq, float phase, float position) {
+		float color = dcOffset + (amp/2f) + (amp/2f) * P.sin((freq * position) + phase);
+		return P.constrain(color, 0f, 255f);
+	}
+
+	
 	public final static int alphaFromColorInt( int c ) { return (c >> 24) & 0xFF; }
 	public final static int redFromColorInt( int c ) { return (c >> 16) & 0xFF;	}
 	public final static int greenFromColorInt( int c ) { return (c >> 8)  & 0xFF; }
