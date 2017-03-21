bc0785940bccbeedbdd24f81336b6bbd6b80d314
cacheflowe <cacheflowe@cacheflowe.com>
2017-03-21 00:46:04 -0600

    Add new bad tv shader

---
+++ src/com/haxademic/core/draw/filters/shaders/BadTVGlitchFilter.java
@@ -0,0 +1,40 @@
+package com.haxademic.core.draw.filters.shaders;
+
+import processing.core.PApplet;
+
+public class BadTVGlitchFilter
+extends BaseFilter {
+
+	public static BadTVGlitchFilter instance;
+	
+	public BadTVGlitchFilter(PApplet p) {
+		super(p, "shaders/filters/badtv2.glsl");
+//		setGrayscale(0);
+//		setIntensityN(0.55f);
+//		setIntensityS(0.45f);
+//		setCountS(4096.0f);
+	}
+	
+	public static BadTVGlitchFilter instance(PApplet p) {
+		if(instance != null) return instance;
+		instance = new BadTVGlitchFilter(p);
+		return instance;
+	}
+	
+//	public void setGrayscale(int grayscale) {
+//		shader.set("grayscale", grayscale);
+//	}
+//	
+//	public void setIntensityN(float nIntensity) {
+//		shader.set("nIntensity", nIntensity);
+//	}
+//	
+//	public void setIntensityS(float sIntensity) {
+//		shader.set("sIntensity", sIntensity);
+//	}
+//	
+//	public void setCountS(float sCount) {
+//		shader.set("sCount", sCount);
+//	}
+	
+}
