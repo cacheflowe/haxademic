b65d68dcb6c62f0949f88a8cf64bb1aea4c8fc07
cacheflowe <cacheflowe@cacheflowe.com>
2017-04-19 10:29:03 -0600

    Draw a shape with inner cutout, for masking

---
+++ src/com/haxademic/sketch/test/InnerCircleTest.java
@@ -0,0 +1,56 @@
+package com.haxademic.sketch.test;
+
+import com.haxademic.core.app.AppSettings;
+import com.haxademic.core.app.P;
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.draw.image.ImageUtil;
+import com.haxademic.core.file.FileUtil;
+
+import processing.core.PGraphics;
+import processing.core.PImage;
+
+public class InnerCircleTest
+extends PAppletHax {
+	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
+
+	PImage img;
+	PGraphics pg;
+
+
+	protected void overridePropsFile() {
+		p.appConfig.setProperty( AppSettings.WIDTH, 640 );
+		p.appConfig.setProperty( AppSettings.HEIGHT, 640 );
+		p.appConfig.setProperty( AppSettings.SHOW_STATS, true );
+	}
+
+	public void setup() {
+		super.setup();
+		img = p.loadImage(FileUtil.getFile("images/bread-large.png"));
+		pg = ImageUtil.imageToGraphics(img);
+	}
+
+	public void drawApp() {
+		background(100,100,255);
+	
+		p.fill(255);
+		p.noStroke();
+		beginShape();
+		// Exterior part of shape, clockwise winding
+		vertex(0, 0);
+		vertex(p.width, 0);
+		vertex(p.width, p.height);
+		vertex(0, p.height);
+		// Interior part of shape, counter-clockwise winding
+		beginContour();
+		float segments = 360f;
+		float segmentRads = P.TWO_PI / segments;
+		float radius = 100f;
+		for(float i = 0; i < segments; i++) {
+			vertex(p.width * 0.5f + radius * P.cos(-i * segmentRads), p.height * 0.5f + radius * P.sin(-i * segmentRads));
+		}
+		endContour();
+		endShape(CLOSE);
+	}
+
+}
+
