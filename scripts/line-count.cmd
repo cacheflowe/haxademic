f252d8deac7e64e88e466048ed7a86fd2f49dfe2
cacheflowe <cacheflowe@cacheflowe.com>
2017-10-10 09:15:16 -0600

    Add Java line count script for windows

---
+++ scripts/line-count.cmd
@@ -0,0 +1,13 @@
+@echo off
+
+:CountLines
+setlocal
+cd ..
+set /a totalNumLines = 0
+for /r %1 %%F in (*.java *.html *.hpp *.cs *.c) do (
+  echo "%%F"
+  for /f %%N in ('find /v /c "" ^<"%%F"') do set /a totalNumLines+=%%N
+)
+
+echo Total LOC (Java) = %totalNumLines%
+timeout 15 > NUL
