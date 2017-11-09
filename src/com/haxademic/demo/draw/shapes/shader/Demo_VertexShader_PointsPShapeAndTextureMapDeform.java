548f924a202661b92d3a9daecfbf06b08a6de736
cacheflowe <cacheflowe@cacheflowe.com>
2017-11-09 07:18:46 -0700

    Clean up demo

--- src/com/haxademic/demo/draw/shapes/shader/Demo_VertexShader_PointsPShapeAndTextureMapDeform.java
+++ src/com/haxademic/demo/draw/shapes/shader/Demo_VertexShader_PointsPShapeAndTextureMapDeform.java
@@ -1,17 +1,14 @@
 package com.haxademic.demo.draw.shapes.shader;
 
 import com.haxademic.app.haxmapper.textures.BaseTexture;
-import com.haxademic.app.haxmapper.textures.TextureEQGrid;
 import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.constants.AppSettings;
-import com.haxademic.core.constants.PBlendModes;
 import com.haxademic.core.constants.PRenderers;
 import com.haxademic.core.draw.context.DrawUtil;
 import com.haxademic.core.draw.image.PerlinTexture;
 import com.haxademic.core.file.DemoAssets;
 import com.haxademic.core.file.FileUtil;
-import com.haxademic.core.math.easing.Penner;
 
 import processing.core.PConstants;
 import processing.core.PGraphics;
@@ -23,15 +20,16 @@ public class Demo_VertexShader_PointsPShapeAndTextureMapDeform
 extends PAppletHax {
 	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 
-	protected PShape shape;
-	protected PImage texture;
 	protected PerlinTexture perlin;
-	protected PShader pointsTexturedShader;
 	protected BaseTexture audioTexture;
 	protected float _frames = 360;
+
+	protected PShape shape;
+	protected PImage texture;
+	protected PShader pointsTexturedShader;
+	protected PGraphics buffer;
 	float w = 1024;
 	float h = 512;
-	protected PGraphics buffer;
 
 	protected void overridePropsFile() {
 		p.appConfig.setProperty(AppSettings.WIDTH, 1024);
@@ -44,25 +42,25 @@ extends PAppletHax {
 
 	public void setup() {
 		super.setup();	
-		buffer = p.createGraphics(p.width, p.height, PRenderers.P3D);
 	}
 	
 	protected void firstFrameSetup() {
-		// load texture
-		perlin = new PerlinTexture(p, (int) w, (int) h);
+		// load & set texture
+		// perlin = new PerlinTexture(p, (int) w, (int) h);
+		// audioTexture = new TextureEQGrid((int) w, (int) h);
+		// texture = audioTexture.texture();
+		// texture = perlin.texture();
+		texture = DemoAssets.textureNebula();
+
+		// build offsecreen buffer (thing don't work the same on the main drawing surface)
+		buffer = p.createGraphics(p.width, p.height, PRenderers.P3D);
+
+		// count vertices for debugView
 		int vertices = P.round( w * h); 
 		p.debugView.setValue("Vertices", vertices);
 		
-		// audio texture
-		audioTexture = new TextureEQGrid((int) w, (int) h);
-		
-		// set texture for app
-//		texture = audioTexture.texture();
-//		texture = perlin.texture();
-		texture = DemoAssets.textureNebula();
-		
-		// replace with a points version
-		shape = P.p.createShape(); // PShape.GEOMETRY
+		// Build points vertices
+		shape = P.p.createShape();
 		shape.beginShape(PConstants.POINTS);
 		shape.stroke(255);
 		shape.strokeWeight(1);
@@ -71,7 +69,7 @@ extends PAppletHax {
 			float x = i % w;
 			float y = P.floor(i / w);
 			if(y % 2 == 1) x = w - x - 1;
-			shape.vertex(x/w, y/h, 0); // x/y coords are used as UV coords, and multplied by `spread` uniform
+			shape.vertex(x/w, y/h, 0); // x/y coords are used as UV coords (0-1), and multplied by `spread` uniform
 		}
 		shape.endShape();
 		shape.setTexture(texture);
@@ -82,6 +80,7 @@ extends PAppletHax {
 			FileUtil.getFile("shaders/point/point-vert.glsl")
 		);
 
+		// clear the screen
 		background(0);
 	}
 
@@ -90,21 +89,20 @@ extends PAppletHax {
 		if(p.frameCount == 1) firstFrameSetup();
 		float percentComplete = ((float)(p.frameCount%_frames)/_frames);
 		
+		// update displacement texture
+		// perlin.update(0.15f, 0.05f, p.frameCount/ 10f, 0);
+		// audioTexture.update();
+		
 		// fade background
 		buffer.beginDraw();
 		DrawUtil.fadeToBlack(buffer, 6);
 		
-		// update displacement texture
-//		perlin.update(0.15f, 0.05f, p.frameCount/ 10f, 0);
-//		audioTexture.update();
-
-		// rotate
+		// move to screen center
 		buffer.translate(p.width/2f, p.height/2f, 0);
 		
 		// draw vertex points. strokeWeight w/disableStyle works here for point size
 		shape.disableStyle();
 		buffer.strokeWeight(1.0f);
-//		pointsDisplacementShader.set("displacementMap2", perlin.texture());
 		pointsTexturedShader.set("displacementMap", texture);
 		pointsTexturedShader.set("pointSize", 1f); // 2.5f + 1.5f * P.sin(P.TWO_PI * percentComplete));
 		pointsTexturedShader.set("width", w);
@@ -115,8 +113,9 @@ extends PAppletHax {
 		buffer.shader(pointsTexturedShader);  
 		buffer.shape(shape);
 		buffer.resetShader();
-		
 		buffer.endDraw();
+		
+		// draw buffer to screen
 		p.image(buffer, 0, 0);
 	}
 		
