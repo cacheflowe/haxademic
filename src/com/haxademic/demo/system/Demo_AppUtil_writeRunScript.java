85050511b5fbd5c8c9d686ec870a2786145fcf7f
Justin Gitlin <cacheflowe@users.noreply.github.com>
2017-11-29 22:21:55 -0500

    Move AppUtil.writeRunScript demo

--- src/com/haxademic/demo/system/Demo_AppUtil_writeRunScript.java
+++ src/com/haxademic/demo/system/Demo_AppUtil_writeRunScript.java
@@ -1,11 +1,10 @@
-package com.haxademic.sketch.system;
+package com.haxademic.demo.system;
 
-import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.constants.AppSettings;
 import com.haxademic.core.system.AppUtil;
 
-public class WriteRunScript
+public class Demo_AppUtil_writeRunScript
 extends PAppletHax {
 	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 	
@@ -18,15 +17,6 @@ extends PAppletHax {
 	public void setup() {
 		super.setup();
 		AppUtil.writeRunScript("scripts/write-test-run.cmd");
-	}
-	
-	public void keyPressed() {
-		super.keyPressed();
-		if(p.key == 'd') DEBUG_MODE = !DEBUG_MODE;
-		if(p.keyCode == 8) P.println("DELETE");
-		if (p.key == P.CODED && p.keyCode == P.RIGHT) P.println("RIGHT ARROW");
-		if (p.key == P.CODED && p.keyCode == P.LEFT) P.println("LEFT ARROW");
-		P.println("key:", p.key, "keyCode:", p.keyCode);
 	}
 
 	public void drawApp() {
