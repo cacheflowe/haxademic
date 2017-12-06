22a2b0247380459863f6777bcefba4812c177b8d
cacheflowe <cacheflowe@cacheflowe.com>
2017-12-05 21:53:01 -0700

    Fic up basic Midi demo

--- src/com/haxademic/demo/hardware/midi/Demo_MidiBus.java
+++ src/com/haxademic/demo/hardware/midi/Demo_MidiBus.java
@@ -1,79 +1,23 @@
 package com.haxademic.demo.hardware.midi;
 
-import java.util.HashMap;
-
 import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.constants.AppSettings;
 
-import themidibus.MidiBus;
-
-public class Demo_MidiBus_REFACTOR
+public class Demo_MidiBus
 extends PAppletHax {
 	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 
-	protected MidiBus midiBus;
-	protected int MIDI_IN_INDEX = 0;
-	protected int MIDI_OUT_INDEX = 4;
-	protected HashMap<String, Number> midiMap = new HashMap<String, Number>(); 
-
 	protected void overridePropsFile() {
-		 p.appConfig.setProperty( "midi_device_in", "Launch Control" );
+		p.appConfig.setProperty(AppSettings.MIDI_DEVICE_IN_INDEX, 0 );
+//		p.appConfig.setProperty(AppSettings.SHOW_DEBUG, true );
 	}
-
-	public void setup() {
-		super.setup();
-		initMidi();
-	}
-
-	protected void initMidi() {
-		MidiBus.list(); // List all available Midi devices on STDOUT. This will show each device's index and name.
-		midiBus = new MidiBus(this, MIDI_IN_INDEX, MIDI_OUT_INDEX);
-//		midiBus = new MidiBus(this);
-	}
-
+	
 	public void drawApp() {
-		background(0);
-		
-		// debug str
-		p.noStroke();
-		p.fill(255);
-		String debugStr = "";
-		for (String key : midiMap.keySet()) {
-			debugStr += key + ": " + midiMap.get(key) + "\n";
-		}
-		p.text(debugStr, 20, 20, p.width - 40, p.height - 40);
-	}
-
-	/**
-	 * PApplet-level listener for MIDIBUS noteOn call
-	 */
-	public void noteOn(int channel, int  pitch, int velocity) {
-		midiMap.put(channel+"-"+pitch, velocity);
-		if( midi != null ) { 
-			if( midi.isMidiButtonOn( pitch ) == false ) {
-				midi.noteOn( channel, pitch, velocity );
-//				try{ 
-//					handleInput( true );
-//				}
-//				catch( ArrayIndexOutOfBoundsException e ){println("noteOn BROKE!");}
-			}
-		}
-	}
-	
-	/**
-	 * PApplet-level listener for MIDIBUS noteOff call
-	 */
-	public void noteOff(int channel, int  pitch, int velocity) {
-		midiMap.put(channel+"-"+pitch, velocity);
-		if( midi != null ) midi.noteOff( channel, pitch, velocity );
-	}
-	
-	/**
-	 * PApplet-level listener for MIDIBUS CC signal
-	 */
-	public void controllerChange(int channel, int number, int value) {
-		midiMap.put(channel+"-"+number, value);
-		if( midi != null ) midi.controllerChange( channel, number, value );
+		p.background(0);
+		// p.midi.isMidiButtonOn(LaunchControl.PAD_01)
+		p.midi.printButtons();
+		p.midi.printCC();
 	}
 	
 	/**
--- src/com/haxademic/sketch/hardware/MidiTest.java
+++ src/com/haxademic/sketch/hardware/MidiTest.java
@@ -1,65 +1,65 @@
-package com.haxademic.sketch.hardware;
-
-import com.haxademic.core.app.P;
-import com.haxademic.core.app.PAppletHax;
-
-public class MidiTest
-extends PAppletHax {
-	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
-
-	protected void overridePropsFile() {
-//		 p.appConfig.setProperty( "osc_active", "true" );
-	}
-
-	public void setup() {
-		super.setup();
-	}
-
-	public void drawApp() {
-		background(0);
-	}
-
-	/**
-	 * PApplet-level listener for MIDIBUS noteOn call
-	 */
-	public void noteOn(int channel, int  pitch, int velocity) {
-		P.println(channel, pitch, velocity);
-		if( midi != null ) { 
-//			if( midi.isMidiButtonOn( pitch ) == 0 ) {
-//				midi.noteOn( channel, pitch, velocity );
-//				try{ 
-//					handleInput( true );
-//				}
-//				catch( ArrayIndexOutOfBoundsException e ){println("noteOn BROKE!");}
-//			}
-		}
-	}
-	
-	/**
-	 * PApplet-level listener for MIDIBUS noteOff call
-	 */
-	public void noteOff(int channel, int  pitch, int velocity) {
-		P.println(channel, pitch, velocity);
-		if( midi != null ) midi.noteOff( channel, pitch, velocity );
-	}
-	
-	/**
-	 * PApplet-level listener for MIDIBUS CC signal
-	 */
-	public void controllerChange(int channel, int number, int value) {
-		if( midi != null ) midi.controllerChange( channel, number, value );
-		P.println(channel, number, value);
-	}
-	
-	/**
-	 * Test sending a signal out
-	 */
-	public void keyPressed() {
-		super.keyPressed();
-		if(p.key == ' ') {
-//			p.midi.sendMidiOut(true, 1, 58, 127);
-		}
-	}
-
-	
-}
+//package com.haxademic.sketch.hardware;
+//
+//import com.haxademic.core.app.P;
+//import com.haxademic.core.app.PAppletHax;
+//
+//public class MidiTest
+//extends PAppletHax {
+//	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
+//
+//	protected void overridePropsFile() {
+////		 p.appConfig.setProperty( "osc_active", "true" );
+//	}
+//
+//	public void setup() {
+//		super.setup();
+//	}
+//
+//	public void drawApp() {
+//		background(0);
+//	}
+//
+//	/**
+//	 * PApplet-level listener for MIDIBUS noteOn call
+//	 */
+//	public void noteOn(int channel, int  pitch, int velocity) {
+//		P.println(channel, pitch, velocity);
+//		if( midi != null ) { 
+////			if( midi.isMidiButtonOn( pitch ) == 0 ) {
+////				midi.noteOn( channel, pitch, velocity );
+////				try{ 
+////					handleInput( true );
+////				}
+////				catch( ArrayIndexOutOfBoundsException e ){println("noteOn BROKE!");}
+////			}
+//		}
+//	}
+//	
+//	/**
+//	 * PApplet-level listener for MIDIBUS noteOff call
+//	 */
+//	public void noteOff(int channel, int  pitch, int velocity) {
+//		P.println(channel, pitch, velocity);
+//		if( midi != null ) midi.noteOff( channel, pitch, velocity );
+//	}
+//	
+//	/**
+//	 * PApplet-level listener for MIDIBUS CC signal
+//	 */
+//	public void controllerChange(int channel, int number, int value) {
+//		if( midi != null ) midi.controllerChange( channel, number, value );
+//		P.println(channel, number, value);
+//	}
+//	
+//	/**
+//	 * Test sending a signal out
+//	 */
+//	public void keyPressed() {
+//		super.keyPressed();
+//		if(p.key == ' ') {
+////			p.midi.sendMidiOut(true, 1, 58, 127);
+//		}
+//	}
+//
+//	
+//}
