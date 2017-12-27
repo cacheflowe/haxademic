5b2361cb8b7af1b6bd18f785da9f8949857a8359
cacheflowe <cacheflowe@cacheflowe.com>
2017-12-27 09:44:24 -0700

    Improve defaults & null check

--- src/com/haxademic/core/hardware/shared/InputTrigger.java
+++ src/com/haxademic/core/hardware/shared/InputTrigger.java
@@ -6,12 +6,16 @@ import com.haxademic.core.hardware.keyboard.Keyboard;
 
 public class InputTrigger {
 	
-	protected Integer[] keyCodes;
-	protected String[] oscMessages;
-	protected String[] webControls;
-	protected Integer[] midiNotes;
-	protected Integer[] midiCC;
+	protected Integer[] keyCodes = new Integer[] {};
+	protected String[] oscMessages = new String[] {};
+	protected String[] webControls = new String[] {};
+	protected Integer[] midiNotes = new Integer[] {};
+	protected Integer[] midiCC = new Integer[] {};
 	protected float curValue = 0;
+	
+	public InputTrigger( char[] charList ) {
+		this( charList, null, null );
+	}
 	
 	public InputTrigger( char[] charList, Integer[] midiNotes ) {
 		this( charList, null, midiNotes );
@@ -22,19 +26,19 @@ public class InputTrigger {
 			keyCodes = new Integer[charList.length];
 			for (int i = 0; i < charList.length; i++) keyCodes[i] = Keyboard.keyCodeFromChar(charList[i]);
 		}
-		this.oscMessages = oscMessages;
-		this.midiNotes = midiNotes;
+		if(oscMessages != null) this.oscMessages = oscMessages;
+		if(midiNotes != null) this.midiNotes = midiNotes;
 	}
 	
 	public InputTrigger( char[] charList, String[] oscMessages, Integer[] midiNotes, Integer[] midiCCNotes ) {
 		this( charList, oscMessages, midiNotes );
-		this.midiCC = midiCCNotes;
+		if(midiCCNotes != null) this.midiCC = midiCCNotes;
 	}
 	
 	public InputTrigger( char[] charList, String[] oscMessages, Integer[] midiNotes, Integer[] midiCCNotes, String[] webControls ) {
 		this( charList, oscMessages, midiNotes );
-		this.midiCC = midiCCNotes;
-		this.webControls = webControls;
+		if(midiCCNotes != null) this.midiCC = midiCCNotes;
+		if(webControls != null) this.webControls = webControls;
 	}
 	
 	public float value() {
@@ -43,17 +47,13 @@ public class InputTrigger {
 	
 	public boolean triggered() {
 		// if triggered, also store the latest value
-		if(keyCodes != null) {
-			for( int i=0; i < keyCodes.length; i++ ) {
-				if( P.p.keyboardState.isKeyTriggered(keyCodes[i]) ) return true;
-			}
+		for( int i=0; i < keyCodes.length; i++ ) {
+			if( P.p.keyboardState.isKeyTriggered(keyCodes[i]) ) return true;
 		}
-		if(P.p.oscState != null) {
-			for( int i=0; i < oscMessages.length; i++ ) {
-				if( P.p.oscState.isValueTriggered(oscMessages[i])) {
-					curValue = P.p.oscState.getValue(oscMessages[i]);
-					return true;
-				}
+		for( int i=0; i < oscMessages.length; i++ ) {
+			if( P.p.oscState.isValueTriggered(oscMessages[i])) {
+				curValue = P.p.oscState.getValue(oscMessages[i]);
+				return true;
 			}
 		}
 		for( int i=0; i < webControls.length; i++ ) {
@@ -62,20 +62,16 @@ public class InputTrigger {
 				return true;
 			}
 		}
-		if(midiNotes != null) {
-			for( int i=0; i < midiNotes.length; i++ ) {
-				if( P.p.midiState.isMidiButtonTriggered(midiNotes[i])) {
-					curValue = P.p.midiState.midiButtonValue(midiNotes[i]);
-					return true;
-				}
+		for( int i=0; i < midiNotes.length; i++ ) {
+			if( P.p.midiState.isMidiButtonTriggered(midiNotes[i])) {
+				curValue = P.p.midiState.midiButtonValue(midiNotes[i]);
+				return true;
 			}
 		}
-		if(midiCC != null) {
-			for( int i=0; i < midiCC.length; i++ ) {
-				if( P.p.midiState.isMidiCCTriggered(midiCC[i])) {
-					curValue = P.p.midiState.midiCCPercent(0, midiCC[i]);
-					return true;
-				}
+		for( int i=0; i < midiCC.length; i++ ) {
+			if( P.p.midiState.isMidiCCTriggered(midiCC[i])) {
+				curValue = P.p.midiState.midiCCPercent(0, midiCC[i]);
+				return true;
 			}
 		}
 		return false;
