8dab8730fc80f3f13da7296f7217065b9ca4213a
cacheflowe <cacheflowe@cacheflowe.com>
2018-01-18 18:38:30 -0700

    Add current frame getter method

--- src/com/haxademic/core/render/AnimationLoop.java
+++ src/com/haxademic/core/render/AnimationLoop.java
@@ -5,15 +5,21 @@ import com.haxademic.core.app.P;
 public class AnimationLoop {
 
 	protected float frames;
+	protected float loopFrames;
 	protected float progress;
 	protected float progressRads;
 	
 	public AnimationLoop(float frames) {
 		this.frames = frames;
+		loopFrames = 0;
 	}
 	
 	public float frames() {
 		return frames;
+	}
+	
+	public float loopFrames() {
+		return loopFrames;
 	}
 	
 	public float progress() {
@@ -25,7 +31,7 @@ public class AnimationLoop {
 	}
 	
 	public void update() {
-		float loopFrames = (float) P.p.frameCount % frames;
+		loopFrames = (float) P.p.frameCount % frames;
 		progress = loopFrames / frames;
 		progressRads = progress * P.TWO_PI;
 		if(P.p.debugView != null) P.p.debugView.setValue("AnimationLoop.progress()", progress);
