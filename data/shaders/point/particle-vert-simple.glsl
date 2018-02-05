af571cd50a4b85f8505c287237f1c3544d6a6963
cacheflowe <cacheflowe@cacheflowe.com>
2018-02-05 15:30:45 -0700

    Particle demo updates

---
+++ data/shaders/point/particle-vert-simple.glsl
@@ -0,0 +1,126 @@
+/*
+  Originally part of the Processing project - http://processing.org
+  https://github.com/processing/processing/blob/master/core/src/processing/opengl/shaders/PointVert.glsl
+
+  Copyright (c) 2011-13 Ben Fry and Casey Reas
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License version 2.1 as published by the Free Software Foundation.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA  02111-1307  USA
+ */
+
+#define PROCESSING_POINT_SHADER
+
+uniform mat4 projection;
+uniform mat4 modelview;
+
+uniform vec4 viewport;
+uniform int perspective;
+
+attribute vec4 vertex;
+attribute vec4 color;
+attribute vec2 offset;
+
+varying vec4 vertColor;
+
+
+// added:
+uniform mat4 transform;
+uniform mat4 texMatrix;
+uniform mat3 normalMatrix;
+
+uniform sampler2D positionMap;
+uniform float pointSize = 1.;
+uniform float width = 256.;
+uniform float height = 256.;
+uniform float scale = 1.;
+
+attribute vec2 texCoord;
+attribute vec3 normal;
+
+varying vec4 vertTexCoord;
+varying vec3 vertNormal;
+varying vec3 vertLightDir;
+
+#define PI radians(180.)
+#define TWO_PI radians(360.)
+
+// default points shader function
+vec4 windowToClipVector(vec2 window, vec4 viewport, float clipw) {
+  vec2 xypos = (window / viewport.zw) * 2.0;
+  return vec4(xypos, 0.0, 0.0) * clipw;
+}
+
+vec2 hash( vec2 p ) {
+	p = vec2( dot(p,vec2(127.1,311.7)),
+			  dot(p,vec2(269.5,183.3)) );
+	return -1.0 + 2.0*fract(sin(p)*43758.5453123);
+}
+
+float noise( in vec2 p ) {
+    const float K1 = 0.366025404; // (sqrt(3)-1)/2;
+    const float K2 = 0.211324865; // (3-sqrt(3))/6;
+	vec2 i = floor( p + (p.x+p.y)*K1 );
+    vec2 a = p - i + (i.x+i.y)*K2;
+    vec2 o = (a.x>a.y) ? vec2(1.0,0.0) : vec2(0.0,1.0); //vec2 of = 0.5 + 0.5*vec2(sign(a.x-a.y), sign(a.y-a.x));
+    vec2 b = a - o + K2;
+	vec2 c = a - 1.0 + 2.0*K2;
+    vec3 h = max( 0.5-vec3(dot(a,a), dot(b,b), dot(c,c) ), 0.0 );
+	vec3 n = h*h*h*h*vec3( dot(a,hash(i+0.0)), dot(b,hash(i+o)), dot(c,hash(i+1.0)));
+    return dot( n, vec3(70.0) );
+}
+
+
+void main() {
+  float vertexIndex = float(gl_VertexID);
+
+  float lookupX = mod(vertexIndex, width);
+  float lookupY = (vertexIndex - lookupX) / width;
+
+  vec4 textureColor = texture2D( positionMap, vec2(vertex.x, vertex.y) ); // rgba color of displacement map
+  // vec4 textureColor = vec4(vertex.x, vertex.y, 0.5, 1.); // rgba color of displacement map
+
+  // calc index of this vertex for positioning use
+  float w = width * scale;
+  float h = height * scale;
+  float x = -w / 2. + textureColor.x * w;
+  float y = -h / 2. + textureColor.y * h;
+  float z = -h / 2. + textureColor.z * h;
+  vec4 vertPosition = vec4(x, y, z, 1.);
+
+  // custom point size - use color to grow point
+  float finalPointSize = pointSize;
+
+  // use custom vertex instead of Processing default (`vertex` uniform)
+  // Processing default shader positioning:
+  vec4 pos = modelview * vertPosition;
+  vec4 clip = projection * pos;
+
+  // Processing default shader positioning:
+  if (0 < perspective) {
+    // Perspective correction (points will look thiner as they move away
+    // from the view position).
+    gl_Position = clip + projection * vec4(offset.xy * finalPointSize, 0, 0);
+  } else {
+    // No perspective correction.
+    vec4 offset = windowToClipVector(offset.xy * finalPointSize, viewport, clip.w);
+    gl_Position = clip + offset;
+  }
+
+  // use original vertex color
+  // vertColor = color;
+  // or instead, use texture-mapped color :)
+  float colorMult = 1.;
+  vertColor = vec4(textureColor.rgb * colorMult, 1.);
+  // vertColor = vec4(1., 1., 1., 0.4);
+}
---
+++ src/com/haxademic/demo/draw/shapes/shader/Demo_VertexShader_GPUParticlesSimple.java
@@ -0,0 +1,118 @@
+package com.haxademic.demo.draw.shapes.shader;
+
+import com.haxademic.app.haxmapper.textures.BaseTexture;
+import com.haxademic.core.app.P;
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.constants.AppSettings;
+import com.haxademic.core.constants.PBlendModes;
+import com.haxademic.core.constants.PRenderers;
+import com.haxademic.core.draw.context.OpenGLUtil;
+import com.haxademic.core.draw.image.PerlinTexture;
+import com.haxademic.core.file.FileUtil;
+import com.haxademic.core.math.MathUtil;
+
+import processing.core.PConstants;
+import processing.core.PGraphics;
+import processing.core.PShape;
+import processing.opengl.PShader;
+
+public class Demo_VertexShader_GPUParticlesSimple 
+extends PAppletHax {
+	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
+
+	protected PerlinTexture perlin;
+	protected BaseTexture audioTexture;
+
+	protected PShape shape;
+	protected PGraphics bufferPositions;
+	protected PShader positionShader;
+	protected PShader randomShader;
+	protected PGraphics renderedParticles;
+	protected PShader particleVerticesShader;
+	int FRAMES = 300;
+
+	protected void overridePropsFile() {
+		p.appConfig.setProperty(AppSettings.LOOP_FRAMES, FRAMES);
+		p.appConfig.setProperty(AppSettings.WIDTH, 768);
+		p.appConfig.setProperty(AppSettings.HEIGHT, 768);
+		p.appConfig.setProperty(AppSettings.FILLS_SCREEN, false);
+		p.appConfig.setProperty(AppSettings.RENDERING_MOVIE, false);
+		p.appConfig.setProperty(AppSettings.RENDERING_MOVIE_START_FRAME, 1 + FRAMES);
+		p.appConfig.setProperty(AppSettings.RENDERING_MOVIE_STOP_FRAME, 1 + FRAMES * 2);
+		p.appConfig.setProperty(AppSettings.SHOW_DEBUG, true);
+	}
+	
+	protected void setupFirstFrame() {
+		// build random particle placement shader
+		positionShader = p.loadShader(FileUtil.getFile("shaders/textures/random-pixel-color.glsl"));
+
+		// create texture to store positions
+		int positionBufferSize = 64;
+		bufferPositions = p.createGraphics(positionBufferSize, positionBufferSize, PRenderers.P3D);
+		OpenGLUtil.setTextureQualityLow(bufferPositions);		// necessary for proper texel lookup!
+		p.debugView.setTexture(bufferPositions);
+		newPositions();
+		
+		// build final draw buffer
+		renderedParticles = p.createGraphics(p.width, p.height, PRenderers.P3D);
+		renderedParticles.smooth(8);
+		p.debugView.setTexture(renderedParticles);
+
+		// count vertices for debugView
+		int vertices = P.round(bufferPositions.width * bufferPositions.height); 
+		p.debugView.setValue("Vertices", vertices);
+		
+		// Build points vertices
+		shape = P.p.createShape();
+		shape.beginShape(PConstants.POINTS);
+		for (int i = 0; i < vertices; i++) {
+			float x = i % positionBufferSize;
+			float y = P.floor(i / positionBufferSize);
+			shape.vertex(x/(float)positionBufferSize, y/(float)positionBufferSize, 0); // x/y coords are used as UV coords for position map (0-1)
+		}
+		shape.endShape();
+		
+		// load shader
+		particleVerticesShader = p.loadShader(
+			FileUtil.getFile("shaders/point/point-frag.glsl"), 
+			FileUtil.getFile("shaders/point/particle-vert-simple.glsl")
+		);
+	}
+	
+	protected void newPositions() {
+		positionShader.set("offset", MathUtil.randRangeDecimal(0, 100), MathUtil.randRangeDecimal(0, 100));
+		bufferPositions.filter(positionShader);
+	}
+
+	public void keyPressed() {
+		super.keyPressed();
+		if(p.key == ' ') newPositions();
+	}
+	
+	public void drawApp() {
+		// clear the screen
+		background(0);
+		
+		// draw shape w/shader
+		particleVerticesShader.set("width", (float) bufferPositions.width);
+		particleVerticesShader.set("height", (float) bufferPositions.height);
+		particleVerticesShader.set("scale", 6f + 1f * P.sin(p.frameCount * 0.01f));
+		particleVerticesShader.set("positionMap", bufferPositions);
+		particleVerticesShader.set("pointSize", 6f);
+
+		renderedParticles.beginDraw();
+		renderedParticles.background(0);
+		renderedParticles.translate(renderedParticles.width/2, renderedParticles.height/2, 0);
+		renderedParticles.rotateY(P.map(p.mouseX, 0, p.width, -3f, 3f));
+		renderedParticles.rotateX(P.map(p.mouseY, 0, p.height, 3f, -3f));
+		renderedParticles.blendMode(PBlendModes.ADD);
+		renderedParticles.shader(particleVerticesShader);  	// update positions
+		renderedParticles.shape(shape);					// draw vertices
+		renderedParticles.resetShader();
+		renderedParticles.endDraw();
+
+		// draw buffer to screen
+		p.image(renderedParticles, 0, 0);
+	}
+		
+}
