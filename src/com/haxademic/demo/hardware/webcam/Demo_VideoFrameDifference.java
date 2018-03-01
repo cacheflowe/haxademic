bff14f82aed40ccc237d4f2c51f5d8751dbae565
cacheflowe <cacheflowe@cacheflowe.com>
2018-03-01 10:30:03 -0700

    Moved file

--- src/com/haxademic/demo/hardware/webcam/Demo_VideoFrameDifference.java
+++ src/com/haxademic/demo/hardware/webcam/Demo_VideoFrameDifference.java
@@ -1,4 +1,4 @@
-package com.haxademic.demo.draw.filters.shaders;
+package com.haxademic.demo.hardware.webcam;
 
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.constants.AppSettings;
