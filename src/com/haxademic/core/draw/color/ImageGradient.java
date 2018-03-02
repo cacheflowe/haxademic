591c0c2dc78b6ec375cf518a805ba9f8734d18fb
cacheflowe <cacheflowe@cacheflowe.com>
2018-03-02 13:58:26 -0700

    Update palette texture paths

--- src/com/haxademic/core/draw/color/ImageGradient.java
+++ src/com/haxademic/core/draw/color/ImageGradient.java
@@ -8,28 +8,28 @@ import processing.core.PGraphics;
 import processing.core.PImage;
 
 public class ImageGradient {
-	
+
 	protected PImage gradientImg;
 	protected float gradientProgress;
 	protected int sampleX;
 	protected int sampleY;
-	
+
 	public ImageGradient(PImage texture) {
 		gradientImg = texture;
 		gradientImg.loadPixels();
 		sampleY = P.round(gradientImg.height * 0.5f);
 	}
-	
+
 	public PImage texture() {
 		return gradientImg;
 	}
-	
+
 	public int getColorAtProgress(float progress) {
 		sampleX = P.round(gradientImg.width * progress);
 		sampleX = P.constrain(sampleX, 0, gradientImg.width - 1);
 		return ImageUtil.getPixelColor(gradientImg, sampleX, sampleY);
 	}
-	
+
 	public void drawDebug(PGraphics pg) {
 		pg.image(gradientImg, 0, 0);
 		pg.stroke(255, 0, 0);
@@ -38,25 +38,25 @@ public class ImageGradient {
 		pg.fill(255);
 		pg.noStroke();
 	}
-	
+
 	// Preset images ///////////////////////////////////////////////////////////////////////////////////////////////////////
-	
+
 	public static PImage BLACK_HOLE = null;
 	public static PImage BLACK_HOLE() {
-		if(BLACK_HOLE == null) BLACK_HOLE = P.p.loadImage(FileUtil.getFile("images/palettes/chandra-black-hole-burst.jpg"));
+		if(BLACK_HOLE == null) BLACK_HOLE = P.p.loadImage(FileUtil.getFile("haxademic/images/palettes/chandra-black-hole-burst.jpg"));
 		return BLACK_HOLE;
 	}
 
 	public static PImage PASTELS = null;
 	public static PImage PASTELS() {
-		if(PASTELS == null) PASTELS = P.p.loadImage(FileUtil.getFile("images/palettes/pastels-gradient.png"));
+		if(PASTELS == null) PASTELS = P.p.loadImage(FileUtil.getFile("haxademic/images/palettes/pastels-gradient.png"));
 		return PASTELS;
 	}
-	
+
 	public static PImage SPARKS_FLAMES = null;
 	public static PImage SPARKS_FLAMES() {
-		if(SPARKS_FLAMES == null) SPARKS_FLAMES = P.p.loadImage(FileUtil.getFile("images/palettes/sparks-flames.jpg"));
+		if(SPARKS_FLAMES == null) SPARKS_FLAMES = P.p.loadImage(FileUtil.getFile("haxademic/images/palettes/sparks-flames.jpg"));
 		return SPARKS_FLAMES;
 	}
-	
+
 }
--- src/com/haxademic/sketch/render/TrumpBomb.java
+++ src/com/haxademic/sketch/render/TrumpBomb.java
@@ -15,7 +15,7 @@ import com.haxademic.core.file.FileUtil;
 import processing.core.PImage;
 import processing.core.PShape;
 
-public class TrumpBomb 
+public class TrumpBomb
 extends PAppletHax {
 	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 
@@ -38,35 +38,35 @@ extends PAppletHax {
 	}
 
 	public void setup() {
-		super.setup();	
+		super.setup();
 		p.noiseSeed(20);
 	}
-	
+
 	protected void firstFrameSetup() {
 		// load texture
 //		img = p.loadImage(FileUtil.getFile("images/jupiter-360.jpg"));
 		img = p.loadImage(FileUtil.getFile("images/aholes-trmp.jpg"));
-		flameColors = new ImageGradient(p.loadImage(FileUtil.getFile("images/palettes/sparks-flames.jpg")));
-		
+		flameColors = new ImageGradient(p.loadImage(FileUtil.getFile("haxademic/images/palettes/sparks-flames.jpg")));
+
 		// build obj PShape and scale to window
-//		obj = p.loadShape( FileUtil.getFile("models/poly-hole-square.obj"));	
-//		obj = p.loadShape( FileUtil.getFile("models/skull-realistic.obj"));	
-//		obj = p.loadShape( FileUtil.getFile("models/man-lowpoly.obj"));	
-//		obj = p.loadShape( FileUtil.getFile("models/lego-man.obj"));	
-		obj = p.loadShape( FileUtil.getFile("models/bomb.obj"));	
+//		obj = p.loadShape( FileUtil.getFile("models/poly-hole-square.obj"));
+//		obj = p.loadShape( FileUtil.getFile("models/skull-realistic.obj"));
+//		obj = p.loadShape( FileUtil.getFile("models/man-lowpoly.obj"));
+//		obj = p.loadShape( FileUtil.getFile("models/lego-man.obj"));
+		obj = p.loadShape( FileUtil.getFile("models/bomb.obj"));
 //		obj = obj.getTessellation();
-//		obj = p.loadShape( FileUtil.getFile("models/poly-hole-tri.obj"));	
+//		obj = p.loadShape( FileUtil.getFile("models/poly-hole-tri.obj"));
 		PShapeUtil.centerShape(obj);
 		PShapeUtil.scaleShapeToExtent(obj, p.height);
-		
+
 		// add UV coordinates to OBJ based on model extents
 		float modelExtent = PShapeUtil.getMaxExtent(obj);
 		modelHeight = PShapeUtil.getMaxAbsY(obj);
 		P.println("modelExtent", modelExtent);
 		P.println("getObjHeight", modelHeight);
-		if(useTexture) 
+		if(useTexture)
 			PShapeUtil.addTextureUVToShape(obj, img, modelExtent, true);
-		
+
 		// build solid, deformable PShape object
 		objSolid = new PShapeSolid(obj);
 	}
@@ -75,35 +75,35 @@ extends PAppletHax {
 		if(p.frameCount == 1) firstFrameSetup();
 		float percentComplete = ((float)(p.frameCount%_frames)/_frames);
 		float radsComplete = percentComplete * P.TWO_PI;
-		
+
 		if(p.frameCount % 300 == 0)		img = p.loadImage(FileUtil.getFile("images/aholes-trmp.jpg"));
- 
+
 		p.pushMatrix();
 		background(0);
 		DrawUtil.setDrawCenter(p);
-		
+
 		// lights
 //		p.lights();
 		p.ambient(0);
-		p.lightSpecular(30, 30, 30); 
+		p.lightSpecular(30, 30, 30);
 		p.lightFalloff(1.0f, 0.001f, 0.0f);
-		p.directionalLight(100, 100, 100, -0.0f, -0.0f, 1); 
-		p.directionalLight(100, 100, 100, 0.0f, 0.0f, -1); 
-		p.specular(p.color(100)); 
-		p.shininess(0.0f); 
+		p.directionalLight(100, 100, 100, -0.0f, -0.0f, 1);
+		p.directionalLight(100, 100, 100, 0.0f, 0.0f, -1);
+		p.specular(p.color(100));
+		p.shininess(0.0f);
 
-		
+
 		// rotate
 		p.translate(
-				p.width/2f, 
-				p.height* (0.48f + 0.025f * P.sin(P.PI - 0.6f + radsComplete)), 
+				p.width/2f,
+				p.height* (0.48f + 0.025f * P.sin(P.PI - 0.6f + radsComplete)),
 				-width*1.5f - 50f + 50f * P.sin(P.PI + radsComplete));
 		p.rotateZ(P.PI);
-		p.rotateY(0.1f * P.sin(radsComplete)); // -P.HALF_PI + 
+		p.rotateY(0.1f * P.sin(radsComplete)); // -P.HALF_PI +
 		p.rotateX(-0.15f + 0.18f * P.sin(P.HALF_PI + radsComplete));
 //		p.rotateX(-P.PI/2f);
 
-		
+
 		// deform
 //		objSolid.updateWithTrig(true, percentComplete * 2f, 0.04f, 17.4f);
 //		objSolid.deformWithAudio();
@@ -118,8 +118,8 @@ extends PAppletHax {
 		// texture mapped with decent performance:
 		float subFromRed = 85f + 85f * P.sin(radsComplete);
 		p.tint(255, 255 - subFromRed, 255 - subFromRed);
-		PShapeUtil.drawTriangles(p.g, objSolid.shape(), img, 1f); // img	
-		
+		PShapeUtil.drawTriangles(p.g, objSolid.shape(), img, 1f); // img
+
 		// sparks
 		float msX = P.map(p.mouseX, 0, p.width, -1000f, 1000f);
 		float msY = P.map(p.mouseY, 0, p.height, -1000f, 1000f);
@@ -135,8 +135,8 @@ extends PAppletHax {
 
 			p.pushMatrix();
 			p.translate(
-					-sparkArea * 1.2f + sparkArea * 2.4f * p.noise(0,0,i + 0.4f * P.sin(i + radsComplete)), 
-					-sparkArea + sparkArea * 2f * p.noise(0,i + 0.4f * P.sin(i + radsComplete),0), 
+					-sparkArea * 1.2f + sparkArea * 2.4f * p.noise(0,0,i + 0.4f * P.sin(i + radsComplete)),
+					-sparkArea + sparkArea * 2f * p.noise(0,i + 0.4f * P.sin(i + radsComplete),0),
 					-sparkArea + sparkArea * 2f * p.noise(i + 0.4f * P.sin(i + radsComplete),0,0)
 					);
 			p.rotateX(i + radsComplete);
@@ -147,14 +147,14 @@ extends PAppletHax {
 			p.popMatrix();
 		}
 		p.popMatrix();
-				
+
 		p.popMatrix();
-		
+
 		SphereDistortionFilter.instance(p).setAmplitude(0.35f + 0.35f * P.sin(0.3f + radsComplete));
 		SphereDistortionFilter.instance(p).applyTo(p);
 		BrightnessFilter.instance(p).setBrightness(2.6f);
 		BrightnessFilter.instance(p).applyTo(p);
 		VignetteFilter.instance(p).applyTo(p);
 	}
-		
+
 }
