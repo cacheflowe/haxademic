40a4d450a56f8617bb4de2a244371d8c076e194e
cacheflowe <cacheflowe@cacheflowe.com>
2018-03-17 15:38:36 -0600

    Add textured quad demo

---
+++ src/com/haxademic/demo/draw/shapes/Demo_Shapes_drawTexturedQuad.java
@@ -0,0 +1,22 @@
+package com.haxademic.demo.draw.shapes;
+
+import com.haxademic.core.app.P;
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.draw.shapes.Shapes;
+import com.haxademic.core.file.DemoAssets;
+
+public class Demo_Shapes_drawTexturedQuad 
+extends PAppletHax {
+	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
+
+	public void drawApp() {
+		background(0);
+		lights();
+		translate(width/2, height/2, 0);
+
+		p.rotateY(P.map(p.mouseX, 0, p.width, 0, P.TWO_PI * 2));
+		p.rotateX(P.map(p.mouseY, 0, p.height, P.TWO_PI * 2, 0));
+		
+		Shapes.drawTexturedRect(p.g, DemoAssets.justin());
+	}
+}
