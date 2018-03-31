fc9d88890641e9f821f36124853ab13114a59a7f
Justin Gitlin <cacheflowe@users.noreply.github.com>
2018-03-31 10:51:28 -0600

    Whoops, swapped demos

--- src/com/haxademic/demo/hardware/webcam/Demo_BufferFrameDifference_sheetDeform.java
+++ src/com/haxademic/demo/hardware/webcam/Demo_BufferFrameDifference_sheetDeform.java
@@ -1,15 +1,14 @@
 package com.haxademic.demo.hardware.webcam;
 
-import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.constants.AppSettings;
 import com.haxademic.core.constants.PRenderers;
 import com.haxademic.core.draw.filters.shaders.BlurHFilter;
 import com.haxademic.core.draw.filters.shaders.BlurVFilter;
-import com.haxademic.core.draw.image.BufferActivityMonitor;
 import com.haxademic.core.draw.image.BufferFrameDifference;
-import com.haxademic.core.draw.image.TiledTexture;
+import com.haxademic.core.draw.image.ImageUtil;
 import com.haxademic.core.draw.shapes.Shapes;
+import com.haxademic.core.file.DemoAssets;
 import com.haxademic.core.file.FileUtil;
 import com.haxademic.core.hardware.webcam.IWebCamCallback;
 
@@ -24,22 +23,21 @@ implements IWebCamCallback {
 
 	protected PShape shape;
 	protected PImage texture;
+	protected PImage textureFlipped;
 	protected PShader displacementShader;
 	protected BufferFrameDifference bufferFrameDifference;
 
 	protected void overridePropsFile() {
 		p.appConfig.setProperty(AppSettings.WIDTH, 800);
 		p.appConfig.setProperty(AppSettings.HEIGHT, 800);
-		p.appConfig.setProperty(AppSettings.LOOP_FRAMES, 160);
 		p.appConfig.setProperty(AppSettings.WEBCAM_INDEX, 3 ); 
 	}
 	
 	protected void setupFirstFrame() {
 		// build sheet mesh
-		texture = p.loadImage(FileUtil.getFile("images/window/flexweave-texture-square.png"));
-		shape = Shapes.createSheet(100, texture);
+		texture = DemoAssets.squareTexture();
+		shape = Shapes.createSheet(200, texture);
 		shape.setTexture(texture);
-		p.debugView.setValue("shape.getVertexCount();", shape.getVertexCount());
 		
 		// load shader
 		displacementShader = loadShader(
@@ -47,42 +45,48 @@ implements IWebCamCallback {
 			FileUtil.getFile("shaders/vertex/brightness-displace-sheet-vert.glsl")
 		);
 		
-		// webcam
+		// webcam callback
 		p.webCamWrapper.setDelegate(this);
 	}
 	
 	
 	@Override
 	public void newFrame(PImage frame) {
-		// lazy load & compare frames
+		// lazy init frame difference object
 		if(bufferFrameDifference == null) {
 			bufferFrameDifference = new BufferFrameDifference(frame.width, frame.height);
 			bufferFrameDifference.diffThresh(0.1f);
-			bufferFrameDifference.falloffBW(0.03f);
+			bufferFrameDifference.falloffBW(0.08f);
+			textureFlipped = p.createGraphics(frame.width, frame.height, PRenderers.P2D);
 		}
-		bufferFrameDifference.update(frame);
-		// blur
+		
+		// flip webcam frame & update frame difference object
+		ImageUtil.copyImage(frame, textureFlipped);
+		ImageUtil.flipH(textureFlipped);
+		bufferFrameDifference.update(textureFlipped);
+		
+		// blur to smooth out displacement
 		BlurHFilter.instance(p).setBlurByPercent(1f, bufferFrameDifference.differenceBuffer().width);
 		BlurHFilter.instance(p).applyTo(bufferFrameDifference.differenceBuffer());
 		BlurVFilter.instance(p).setBlurByPercent(1f, bufferFrameDifference.differenceBuffer().height);
 		BlurVFilter.instance(p).applyTo(bufferFrameDifference.differenceBuffer());
+		
 		// debug view
 		p.debugView.setTexture(p.webCamWrapper.getImage());
 		p.debugView.setTexture(bufferFrameDifference.differenceBuffer());
+		p.debugView.setValue("shape.getVertexCount();", shape.getVertexCount());
 	}
 
 	public void drawApp() {
-		background(0);
+		p.background(0);
 		
-		// rotate
-		p.translate(p.width/2f, p.height/2f, -100);
-		p.rotateZ(P.PI);
-		
-		// draw mesh with texture or without
+		// update shader & draw mesh
 		if(bufferFrameDifference != null) {
+			shape.setTexture(textureFlipped);	// set texture to webcam
 			displacementShader.set("displacementMap", bufferFrameDifference.differenceBuffer());
 			displacementShader.set("displaceStrength", 300f);
 			p.shader(displacementShader);  
+			p.translate(p.width/2f, p.height/2f, -100);
 			p.shape(shape);
 			p.resetShader();
 		}
