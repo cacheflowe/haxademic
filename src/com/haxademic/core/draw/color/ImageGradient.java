3f83cc9666e3ec1d8db7121bcfe9af6fc01874d8
cacheflowe <cacheflowe@cacheflowe.com>
2018-04-12 16:33:31 -0600

    Update Coolors color scheme files & random loader

--- src/com/haxademic/core/draw/color/ImageGradient.java
+++ src/com/haxademic/core/draw/color/ImageGradient.java
@@ -1,5 +1,7 @@
 package com.haxademic.core.draw.color;
 
+import java.util.ArrayList;
+
 import com.haxademic.core.app.P;
 import com.haxademic.core.draw.image.ImageUtil;
 import com.haxademic.core.file.FileUtil;
@@ -60,24 +62,18 @@ public class ImageGradient {
 		return SPARKS_FLAMES;
 	}
 	
-	public static String[] coolors = new String[] {
-			"54f2f2-84a3cc-f2a9a9-f2ba8c-b4d693-crop.min.png",
-			"002145-fcff4b-ba274a-ffffff-2191fb.png.min.png",
-			"26547c-a805fa-f25757-e7e6f7-fcfcfc.png.min.png",
-			"c0caad-899e8b-99c5b5-5b6c5d-36453b.png.min.png",
-			"c1c1c1-1d3461-40484a-d6ae0e-fcfafa.png.min.png",
-			"da373c-1c3144-2e282a-e8c547-f6e8ea.png.min.png",
-			"ecd1be-fce097-a89f8e-dce4ed-4ed8a8.png.min.png",
-			"ee6400-2e2f26-8dd517-5e161c-d6cb8a.png.min.png",
-			"f4a0ac-e7d2f4-5783bf-d7926b-f5d4ac.png.min.png",
-			"f9ffef-abff19-98f000-78bd00-477000.png.min.png",
-			"fe7171-110085-ee4266-fac8cd-961876.png.min.png",
-			"ff9481-110085-fac8cd-f24b93-961876.png.min.png",
-			"ffb82b-5b2515-94dbf7-ffffff-000000.png.min.png",
-	};
+	public static ArrayList<String> coolors = null; 	
+	public static void getCoolorFilenames() {
+		String imagesPath = FileUtil.getFile("haxademic/images/palettes/coolors/");
+		coolors = FileUtil.getFilesInDirOfType(imagesPath, "png");
+		for (int i = 0; i < coolors.size(); i++) {
+			coolors.set(i, imagesPath + coolors.get(i)); 
+		}
+	}
 	
 	public static PImage randomCoolor() {
-		return P.p.loadImage(FileUtil.getFile("haxademic/images/palettes/coolors/" + coolors[MathUtil.randRange(0, coolors.length - 1)]));
+		if(coolors == null) getCoolorFilenames();
+		return P.p.loadImage(coolors.get(MathUtil.randRange(0, coolors.size() - 1)));
 	}
 
 }
