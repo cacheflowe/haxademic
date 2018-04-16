5450e236f626fe1c18a6bc7ef44c8052a68fd67a
Justin Gitlin <cacheflowe@users.noreply.github.com>
2018-04-16 08:54:31 -0600

    Default to Haxademic window

--- scripts/screen-capture-window.cmd
+++ scripts/screen-capture-window.cmd
@@ -1,1 +1,2 @@
-"C:\Program Files\ffmpeg\bin\ffmpeg.exe" -f gdigrab -framerate 30 -i title="REACT RUNNING" -c:v h264_nvenc -qp 0 output-4.mkv
+"C:\Program Files\ffmpeg\bin\ffmpeg.exe" -f gdigrab -framerate 30 -i title="Haxademic" -c:v h264_nvenc -qp 0 output-4.mkv
+REM -i desktop
