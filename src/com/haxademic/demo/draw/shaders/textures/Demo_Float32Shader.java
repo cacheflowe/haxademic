30ed7512e1d36c448c07af24407a245a9b333a5d
cacheflowe <cacheflowe@cacheflowe.com>
2018-05-29 12:25:01 -0600

    Update Pixelflow 32-bit float texture demos & Util

--- src/com/haxademic/demo/draw/shaders/textures/Demo_Float32Shader.java
+++ src/com/haxademic/demo/draw/shaders/textures/Demo_Float32Shader.java
@@ -1,14 +1,12 @@
 package com.haxademic.demo.draw.shaders.textures;
 
 import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.draw.context.OpenGL32Util;
 import com.haxademic.core.draw.image.ImageUtil;
 import com.haxademic.core.file.DemoAssets;
 import com.haxademic.core.file.FileUtil;
-import com.jogamp.opengl.GL;
-import com.thomasdiewald.pixelflow.java.DwPixelFlow;
 import com.thomasdiewald.pixelflow.java.dwgl.DwGLSLProgram;
 import com.thomasdiewald.pixelflow.java.dwgl.DwGLTexture;
-import com.thomasdiewald.pixelflow.java.imageprocessing.filter.DwFilter;
 
 import processing.opengl.PGraphics2D;
 
@@ -16,42 +14,41 @@ public class Demo_Float32Shader
 extends PAppletHax {
 	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 
-	protected DwPixelFlow pixelflow;
-	protected PGraphics2D buffer;
+	protected PGraphics2D buffer8;
 	protected DwGLSLProgram shader32;
-	protected DwGLTexture texture32 = new DwGLTexture();
+	protected DwGLTexture texture32;
 
 	protected void setupFirstFrame() {
-	    pixelflow = new DwPixelFlow(this);
-	    shader32 = pixelflow.createShader(FileUtil.getFile("haxademic/shaders/vertex/pixelflow-test.glsl"));
-	    buffer = (PGraphics2D) createGraphics(p.width, p.height, P2D);
-	    texture32.resize(pixelflow, GL.GL_RGBA32F, p.width, p.height, GL.GL_RGBA, GL.GL_FLOAT, GL.GL_NEAREST, GL.GL_REPEAT, 4, 4);	// GL_LINEAR -> GL_NEAREST?
+		// build float buffer/shader
+	    shader32 = OpenGL32Util.newShader(FileUtil.getFile("haxademic/shaders/float32/pixelflow-test.glsl"));
+	    buffer8 = OpenGL32Util.newPGraphics2D(p.width, p.height);
+	    texture32 = OpenGL32Util.newTexture32(buffer8.width, buffer8.height);
 
 		// draw something to the pg
-	    buffer.beginDraw();
-	    ImageUtil.drawImageCropFill(DemoAssets.textureNebula(), buffer, true);
-	    buffer.endDraw();
+	    buffer8.beginDraw();
+	    ImageUtil.drawImageCropFill(DemoAssets.textureNebula(), buffer8, true);
+	    buffer8.endDraw();
 
 		// send PGraphics image into float32 texture
-	    DwFilter.get(pixelflow).copy.apply(buffer, texture32);
+	    OpenGL32Util.pGraphics2dToTexture32(buffer8, texture32);
 	}
 
 	public void drawApp() {
 		// Draw shader output to DwGLTexture
-	    pixelflow.begin();
-	    pixelflow.beginDraw(texture32);
+	    OpenGL32Util.context().begin();
+	    OpenGL32Util.context().beginDraw(texture32);
 	    shader32.begin();
-	    shader32.uniformTexture("tex", texture32);
-	    shader32.uniform2f("wh", buffer.width, buffer.height);
+	    shader32.uniformTexture("texture", texture32);
+	    shader32.uniform2f("resolution", buffer8.width, buffer8.height);
 	    shader32.drawFullScreenQuad();
 	    shader32.end();	  
-	    pixelflow.endDraw();
-	    pixelflow.end();
+	    OpenGL32Util.context().endDraw();
+	    OpenGL32Util.context().end();
 	    
 	    // copy DWTexture back to PGraphics
-	    DwFilter.get(pixelflow).copy.apply(texture32, buffer);
+	    OpenGL32Util.texture32ToPGraphics2d(texture32, buffer8);
 
 	    // Render PGraphics object to screen
-	    p.image(buffer, 0, 0);
+	    p.image(buffer8, 0, 0);
 	}
 }
--- src/com/haxademic/demo/draw/shaders/textures/Demo_Float32ShaderMap.java
+++ src/com/haxademic/demo/draw/shaders/textures/Demo_Float32ShaderMap.java
@@ -1,6 +1,7 @@
 package com.haxademic.demo.draw.shaders.textures;
 
 import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.draw.context.OpenGL32Util;
 import com.haxademic.core.draw.image.ImageUtil;
 import com.haxademic.core.draw.shaders.textures.TextureShader;
 import com.haxademic.core.file.DemoAssets;
@@ -17,8 +18,7 @@ public class Demo_Float32ShaderMap
 extends PAppletHax {
 	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 
-	protected DwPixelFlow pixelflow;
-	protected PGraphics2D buffer;
+	protected PGraphics2D buffer8;
 	protected DwGLSLProgram shader32;
 	protected DwGLTexture texture32 = new DwGLTexture();
 	
@@ -28,50 +28,52 @@ extends PAppletHax {
 
 
 	protected void setupFirstFrame() {
-	    pixelflow = new DwPixelFlow(this);
-	    shader32 = pixelflow.createShader(FileUtil.getFile("haxademic/shaders/vertex/pixelflow-test-map.glsl"));
-	    texture32.resize(pixelflow, GL.GL_RGBA32F, p.width, p.height, GL.GL_RGBA, GL.GL_FLOAT, GL.GL_NEAREST, GL.GL_REPEAT, 4, 4);	// GL_LINEAR -> GL_NEAREST?
-	    textureNoise.resize(pixelflow, GL.GL_RGBA8, p.width, p.height, GL.GL_RGBA, GL.GL_FLOAT, GL.GL_NEAREST, GL.GL_REPEAT, 4, 4);	// GL_LINEAR -> GL_NEAREST?
-	    buffer = (PGraphics2D) createGraphics(p.width, p.height, P2D);
+		// create buffers
+	    buffer8 = OpenGL32Util.newPGraphics2D(p.width, p.height);
+	    texture32 = OpenGL32Util.newTexture32(buffer8.width, buffer8.height);
+	    shader32 = OpenGL32Util.newShader(FileUtil.getFile("haxademic/shaders/float32/pixelflow-test-map.glsl"));
 	    
+	    // create buffers for simplex noise
+	    textureNoise = OpenGL32Util.newTexture8(p.width, p.height);
 		noiseTexture = new TextureShader(TextureShader.noise_simplex_2d_iq);
 	    noiseBuffer = (PGraphics2D) createGraphics(p.width, p.height, P2D);
 
 		// draw something to the pg
-	    buffer.beginDraw();
-	    ImageUtil.drawImageCropFill(DemoAssets.smallTexture(), buffer, true);
-	    buffer.endDraw();
+	    buffer8.beginDraw();
+	    ImageUtil.drawImageCropFill(DemoAssets.textureJupiter(), buffer8, true);
+	    buffer8.endDraw();
 
 		// send PGraphics image into float32 texture
-	    DwFilter.get(pixelflow).copy.apply(buffer, texture32);
+	    OpenGL32Util.pGraphics2dToTexture32(buffer8, texture32);
 	}
 
 	public void drawApp() {
 		// update noise
 		noiseTexture.updateTime();
-		noiseTexture.shader().set("offset", 0f, p.frameCount * 0.01f);
-		noiseTexture.shader().set("rotation", 0f, p.frameCount * 0.01f);
-		noiseTexture.shader().set("zoom", 2f);
+//		noiseTexture.shader().set("offset", 0f, p.frameCount * 0.004f);
+//		noiseTexture.shader().set("rotation", 0f, p.frameCount * 0.004f);
+		noiseTexture.shader().set("zoom", 2.5f);
 		noiseBuffer.filter(noiseTexture.shader());
-	    DwFilter.get(pixelflow).copy.apply(noiseBuffer, textureNoise);
+	    OpenGL32Util.pGraphics2dToTexture32(noiseBuffer, textureNoise);
 	    p.debugView.setTexture(noiseBuffer);
 		
 		// Draw shader output to DwGLTexture
-	    pixelflow.begin();
-	    pixelflow.beginDraw(texture32);
+	    OpenGL32Util.context().begin();
+	    OpenGL32Util.context().beginDraw(texture32);
 	    shader32.begin();
-	    shader32.uniformTexture("tex", texture32);
+	    shader32.uniformTexture("texture", texture32);
 	    shader32.uniformTexture("map", textureNoise);
-	    shader32.uniform2f("wh", buffer.width, buffer.height);
+	    shader32.uniform1f("time", p.frameCount * 0.05f);
+	    shader32.uniform2f("resolution", buffer8.width, buffer8.height);
 	    shader32.drawFullScreenQuad();
 	    shader32.end();	  
-	    pixelflow.endDraw();
-	    pixelflow.end();
+	    OpenGL32Util.context().endDraw();
+	    OpenGL32Util.context().end();
 	    
 	    // copy DWTexture back to PGraphics
-	    DwFilter.get(pixelflow).copy.apply(texture32, buffer);
+	    OpenGL32Util.texture32ToPGraphics2d(texture32, buffer8);
 
 	    // Render PGraphics object to screen
-	    p.image(buffer, 0, 0);
+	    p.image(buffer8, 0, 0);
 	}
 }
