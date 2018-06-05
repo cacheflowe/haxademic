47252cb99a7e86759dcc53b83968756bf089cb48
cacheflowe <cacheflowe@cacheflowe.com>
2018-06-05 11:19:13 -0600

    Clean up

--- src/com/haxademic/sketch/render/ConcentricCubes.java
+++ src/com/haxademic/sketch/render/ConcentricCubes.java
@@ -15,28 +15,13 @@ extends PAppletHax {
 	protected int _x = 0;
 	protected int _y = 0;
 
-	protected int _frames = 40;
+	protected int _frames = 100;
 
 	protected void overridePropsFile() {
-		p.appConfig.setProperty( AppSettings.WIDTH, "800" );
-		p.appConfig.setProperty( AppSettings.HEIGHT, "800" );
+		p.appConfig.setProperty( AppSettings.WIDTH, 800 );
+		p.appConfig.setProperty( AppSettings.HEIGHT, 800 );
+		p.appConfig.setProperty( AppSettings.RENDERING_MOVIE, false );
 
-		p.appConfig.setProperty( AppSettings.RENDERING_MOVIE, "false" );
-		p.appConfig.setProperty( AppSettings.RENDERING_GIF, "false" );
-		p.appConfig.setProperty( AppSettings.RENDERING_GIF_FRAMERATE, "40" );
-		p.appConfig.setProperty( AppSettings.RENDERING_GIF_QUALITY, "15" );
-		p.appConfig.setProperty( AppSettings.RENDERING_GIF_START_FRAME, "2" );
-		p.appConfig.setProperty( AppSettings.RENDERING_GIF_STOP_FRAME, ""+Math.round(_frames+1) );
-
-	}
-
-	public void setup() {
-		super.setup();	
-		p.smooth( OpenGLUtil.SMOOTH_HIGH );
-
-		//		_cp5 = new ControlP5(this);
-		//		_cp5.addSlider("_x").setPosition(20,60).setWidth(200).setRange(0,p.displayWidth - p.width);
-		//		_cp5.addSlider("_y").setPosition(20,100).setWidth(200).setRange(0,p.displayHeight - p.height);
 	}
 
 	public void drawApp() {
@@ -60,7 +45,7 @@ extends PAppletHax {
 			p.pushMatrix();
 			p.stroke(127 + 127f * P.sin(i/20f), 127 + 127f * P.sin(i/30f), 127 + 127f * P.sin(i/10f));
 			p.strokeWeight(2.2f);
-			//			p.rotateY(0.5f * P.sin(P.TWO_PI * percentComplete + i/numCubes));
+//						p.rotateY(0.5f * P.sin(P.TWO_PI * percentComplete + i));
 			p.rotateY(1.1f * P.sin(P.TWO_PI * percentComplete + i * 0.03f));
 			p.rotateZ(2.1f * P.sin(P.TWO_PI * percentComplete + i * 0.03f));
 			p.rotateX(1.1f * P.sin(P.TWO_PI * percentComplete + i * 0.03f));
@@ -70,18 +55,7 @@ extends PAppletHax {
 			cubeSize -= p.width / cubScaleDown;
 			i++;
 		}
-
-		//		p.filter(P.BLUR);
-
-		if( p.frameCount == _frames * 2 + 2 ) {
-			if(p.appConfig.getBoolean("rendering", false) ==  true) {				
-				movieRenderer.stop();
-				P.println("render done!");
-			}
-		}
-
 	}
-
 }
 
 
