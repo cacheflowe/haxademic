5f56c092069af38347765a32b652ba7f92b8ba03
Justin Gitlin <cacheflowe@cacheflowe.local>
2018-07-20 10:29:01 -0600

    Update/add DMX demos

--- src/com/haxademic/demo/hardware/dmx/Demo_DmxUSBProVideoMap.java
+++ src/com/haxademic/demo/hardware/dmx/Demo_DmxUSBProVideoMap.java
@@ -2,11 +2,15 @@ package com.haxademic.demo.hardware.dmx;
 
 import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.draw.image.ImageUtil;
+import com.haxademic.core.file.DemoAssets;
 
 import dmxP512.DmxP512;
+import processing.core.PGraphics;
 import processing.serial.Serial;
+import processing.video.Movie;
 
-public class Demo_DmxUSBPro
+public class Demo_DmxUSBProVideoMap
 extends PAppletHax {
 	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 	
@@ -28,7 +32,8 @@ extends PAppletHax {
 	String DMXPRO_BAUDRATE = "DMXPRO_BAUDRATE";
 	String DMXPRO_UNIVERSE_SIZE = "DMXPRO_UNIVERSE_SIZE";
 	
-	protected boolean audioActive = false;
+	protected Movie video;
+	protected PGraphics videoBuffer;
 
 	protected void overridePropsFile() {
 		if(P.platform == P.MACOSX) {
@@ -43,39 +48,49 @@ extends PAppletHax {
 	}
 
 	public void setupFirstFrame() {
+		// init DMX
 		Serial.list();
 		dmx = new DmxP512(P.p, p.appConfig.getInt(DMXPRO_UNIVERSE_SIZE, 256), false);
 		dmx.setupDmxPro(p.appConfig.getString(DMXPRO_PORT, "COM1"), p.appConfig.getInt(DMXPRO_BAUDRATE, 115000));
+		
+		// init video
+		video = DemoAssets.movieFractalCube();
+		video.loop();
 	}
 
 	public void drawApp() {
-		background(0);
-		if(audioActive) {
-			// audio eq
-			dmx.set(1, P.constrain(P.round(255 * p.audioFreq(10)), 0, 255));
-			dmx.set(2, P.constrain(P.round(255 * p.audioFreq(20)), 0, 255));
-			dmx.set(3, P.constrain(P.round(255 * p.audioFreq(40)), 0, 255));
-			dmx.set(4, P.constrain(P.round(255 * p.audioFreq(60)), 0, 255));
-			dmx.set(5, P.constrain(P.round(255 * p.audioFreq(80)), 0, 255));
-			dmx.set(6, P.constrain(P.round(255 * p.audioFreq(100)), 0, 255));
-		} else {
+		// set context
+		p.background(0);
+		p.noStroke();
+		
+		// lazy-init video copy buffer
+		if(videoBuffer == null && video.width > 50) {
+			videoBuffer = p.createGraphics(video.width, video.height, P.P2D);
+		}
+		
+		if(videoBuffer != null) {
+			// copy video to buffer
+			ImageUtil.copyImage(video, videoBuffer);
+			
+			// access bitmap pixel data
+			videoBuffer.loadPixels();
+			int pixelX = 250;
+			int pixelY = 250;
+			int colorFromVideo = ImageUtil.getPixelColor(videoBuffer, pixelX, pixelY);
+			
+			// draw video 
+			p.fill(255);
+			p.image(videoBuffer, 0, 0);
+			
+			// debug pixel grabbing position
+			p.fill(0, 255, 0);
+			p.rect(pixelX, pixelY, 3, 3);
+			
 			// color cycle
-			dmx.set(1, round(127 + 127 * P.sin(p.frameCount * 0.2f)));
-			dmx.set(2, round(127 + 127 * P.sin(p.frameCount * 0.08f)));
-			dmx.set(3, round(127 + 127 * P.sin(p.frameCount * 0.02f)));
-			dmx.set(4, round(127 + 127 * P.sin(p.frameCount * 0.1f)));
-			dmx.set(5, round(127 + 127 * P.sin(p.frameCount * 0.07f)));
-			dmx.set(6, round(127 + 127 * P.sin(p.frameCount * 0.04f)));
+			dmx.set(1, P.round(p.red(colorFromVideo)));
+			dmx.set(2, P.round(p.green(colorFromVideo)));
+			dmx.set(3, P.round(p.blue(colorFromVideo)));
 		}
 	}
-	
-	public void keyPressed() {
-		super.keyPressed();
-		if(p.key == ' ') audioActive = !audioActive;
-	}
+
 }
-
-
-
-
-
