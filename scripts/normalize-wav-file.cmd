21df2fbcfcee8636876371e03ae805af4f74e5c1
Justin Gitlin <cacheflowe@users.noreply.github.com>
2018-08-11 17:24:56 -0600

    Add normalize wav file windows script

---
+++ scripts/normalize-wav-file.cmd
@@ -0,0 +1,22 @@
+REM @echo off
+
+REM "============== GET PARAMS"
+
+SET sox="C:\Program Files (x86)\sox-14-4-2\sox.exe"
+SET filePath=%1
+SET fileNormPath=%filePath%.norm.wav
+SET gain=%2
+
+echo "============== Normalizing audio: %fileNormPath%"
+%sox%  %filePath% %fileNormPath% norm %gain%
+
+echo "============== Deleting original: %filePath%"
+del %filePath%
+
+echo "============== Renaming normalized file back to source: %filePath%"
+For %%A in ("%filePath%") do (
+    Set fileName=%%~nxA
+)
+ren %fileNormPath% %fileName%
+
+echo "============== Finished normalize"
