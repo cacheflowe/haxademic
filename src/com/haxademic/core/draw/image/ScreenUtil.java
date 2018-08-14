9fb41ede16d91c6153820cb50a30d8ab3542749e
Justin Gitlin <cacheflowe@cacheflowe.local>
2018-08-14 13:45:50 -0600

    Add a screenshot method for multiple monitors

--- src/com/haxademic/core/draw/image/ScreenUtil.java
+++ src/com/haxademic/core/draw/image/ScreenUtil.java
@@ -1,8 +1,12 @@
 package com.haxademic.core.draw.image;
 
 import java.awt.AWTException;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
 import java.awt.Robot;
+import java.awt.geom.Rectangle2D;
 
 import com.haxademic.core.system.SystemUtil;
 
@@ -11,13 +15,13 @@ import processing.core.PGraphics;
 import processing.core.PImage;
 
 public class ScreenUtil {
-	
+
 	public static String saveScreenshot( PApplet p, String outputDir ) {
 		String filename = outputDir + SystemUtil.getTimestampFine( p ) + ".png";
 		p.saveFrame( filename );
 		return filename;
 	}
-	
+
 	public static void screenshotHiRes( PApplet p, int scaleFactor, String p5Renderer, String outputDir ) {
 		// from: http://amnonp5.wordpress.com/2012/01/28/25-life-saving-tips-for-processing/
 		PGraphics hires = p.createGraphics(p.width*scaleFactor, p.height*scaleFactor, p5Renderer );
@@ -32,14 +36,32 @@ public class ScreenUtil {
 		p.noSmooth();
 	}
 
-	public static PImage getScreenAsPImage( PApplet p ) {
-		return p.get();
-	}
-	
 	public static PImage getScreenShotAsPImage( PApplet p ) {
 		try {
 			Robot robot = new Robot();
 			PImage screenshot = new PImage(robot.createScreenCapture(new Rectangle(0,0,p.width,p.height)));
+			return screenshot;
+		} catch (AWTException e) { }
+		return null;
+	}
+
+	public static PImage getScreenShotAllMonitors() {
+		return getScreenShotAllMonitors(0,0);
+	}
+	
+	public static PImage getScreenShotAllMonitors(int x, int y) {
+		// NOTE! main monitor must be top left, otherwise, override x+y position  
+		try {
+			Rectangle2D result = new Rectangle2D.Double();
+			GraphicsEnvironment localGE = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			for (GraphicsDevice gd : localGE.getScreenDevices()) {
+				for (GraphicsConfiguration graphicsConfiguration : gd.getConfigurations()) {
+					Rectangle2D.union(result, graphicsConfiguration.getBounds(), result);
+				}
+			}
+
+			Robot robot = new Robot();
+			PImage screenshot = new PImage(robot.createScreenCapture(new Rectangle(x, y,(int) result.getWidth(), (int) result.getHeight())));
 			return screenshot;
 		} catch (AWTException e) { }
 		return null;
