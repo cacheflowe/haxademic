f247e584cf19160b5aff600765e87ab077190f0d
Justin Gitlin <cacheflowe@users.noreply.github.com>
2018-08-28 09:28:25 -0600

    Add notes & app launch demo for Sphinx4

---
+++ src/com/haxademic/demo/system/Demo_JavaFXAppLaunch.java
@@ -0,0 +1,38 @@
+package com.haxademic.demo.system;
+
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.constants.AppSettings;
+import com.haxademic.core.draw.context.DrawUtil;
+import com.haxademic.demo.net.Demo_WebViewTestApp;
+
+public class Demo_JavaFXAppLaunch
+extends PAppletHax {
+	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
+
+	protected void overridePropsFile() {
+		int FRAMES = 360;
+		p.appConfig.setProperty(AppSettings.LOOP_FRAMES, FRAMES);
+	}
+
+	public void setupFirstFrame() {
+        new Thread() {
+            @Override
+            public void run() {
+                javafx.application.Application.launch(Demo_WebViewTestApp.class);
+            }
+        }.start();
+	}
+	
+
+	public void drawApp() {
+		p.background(0);
+		DrawUtil.setCenterScreen(p);
+		
+		if(Demo_WebViewTestApp.instance != null) {
+			p.background(0,255,0);
+			//	WebViewApplicationTest.instance.printSomething();
+		}
+	}
+
+}
+
