235e832ce14df985c6aeee13d43e2a5217f574d1
Justin Gitlin <cacheflowe@users.noreply.github.com>
2018-09-26 15:04:52 -0600

    Merge new fine offsets feature in PGraphicsKeystone

--- src/com/haxademic/core/draw/mapping/PGraphicsKeystone.java
+++ src/com/haxademic/core/draw/mapping/PGraphicsKeystone.java
@@ -1,22 +1,30 @@
 package com.haxademic.core.draw.mapping;
 
+import com.haxademic.core.data.ConvertUtil;
+import com.haxademic.core.debug.DebugUtil;
+import com.haxademic.core.file.FileUtil;
+
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PGraphics;
 import processing.core.PImage;
+import processing.event.KeyEvent;
 
 public class PGraphicsKeystone
 extends BaseSavedQuadUI {
 
 	protected PGraphics pg;
 	protected float subDivideSteps;
-	protected float[] offsetsX = null;
-	protected float[] offsetsY = null;
-
+	
 	public PGraphicsKeystone( PApplet p, PGraphics pg, float subDivideSteps, String filePath ) {
 		super(p.width, p.height, filePath);
 		this.pg = pg;
 		this.subDivideSteps = subDivideSteps;
+	}
+	
+	public PGraphicsKeystone( PApplet p, PGraphics pg, float subDivideSteps, String filePath, String filePathFineOffsets ) {
+		this(p, pg, subDivideSteps, filePath);
+		initFineControl(filePathFineOffsets);
 	}
 	
 	public PGraphics pg() {
@@ -37,10 +45,12 @@ extends BaseSavedQuadUI {
 		
 	public void setOffsetsX(float[] offsetsX) {
 		this.offsetsX = offsetsX;
+		if(this.offsetsX.length != subDivideSteps + 1) DebugUtil.printErr("PGraphicsKeystone.offsetsX[] must have one more element than subDivideSteps");
 	}
 	
 	public void setOffsetsY(float[] offsetsY) {
 		this.offsetsY = offsetsY;
+		if(this.offsetsX.length != subDivideSteps + 1) DebugUtil.printErr("PGraphicsKeystone.offsetsY[] must have one more element than subDivideSteps");
 	}
 	
 	public void update( PGraphics canvas ) {
@@ -60,7 +70,7 @@ extends BaseSavedQuadUI {
 		// inspired by: https://github.com/davidbouchard/keystone & http://marcinignac.com/blog/projectedquads-source-code/
 		canvas.textureMode(PConstants.IMAGE);
 		canvas.noStroke();
-		// canvas.stroke(255);
+		if(selectedRowIndex != -1 || selectedColIndex != -1) canvas.stroke(255, 0, 0);
 		canvas.fill(255);
 		canvas.beginShape(PConstants.QUAD);
 		canvas.texture(texture);
@@ -78,10 +88,11 @@ extends BaseSavedQuadUI {
 				float uPercent = xPercent;
 				float uPercentNext = xPercentNext;
 
-				// add x offsets
+				// add x offsets if array exists and values aren't zero
 				if(offsetsX != null) {
-					if(offsetsX[(int) x] != 0) xPercent += offsetsX[(int) x]; 
-					if(x < stepsX - 1 && offsetsX[(int) x + 1] != 0) xPercentNext += offsetsX[(int) x + 1];
+					int fineIndex = (int) x;
+					if(offsetsX[fineIndex] != 0) xPercent += offsetsX[fineIndex]; 
+					if(offsetsX[fineIndex + 1] != 0) xPercentNext += offsetsX[fineIndex + 1];
 				}
 				
 				for( float y=0; y < stepsY; y += 1f ) {
@@ -92,10 +103,11 @@ extends BaseSavedQuadUI {
 					float vPercent = yPercent;
 					float vPercentNext = yPercentNext;
 
-					// add y offsets
+					// add y offsets if array exists and values aren't zero
 					if(offsetsY != null) {
-						if(offsetsY[(int) y] != 0) yPercent += offsetsY[(int) y]; 
-						if(y < stepsY - 1 && offsetsY[(int) y + 1] != 0) yPercentNext += offsetsY[(int) y + 1];
+						int fineIndexY = (int) y;
+						if(offsetsY[fineIndexY] != 0) yPercent += offsetsY[fineIndexY]; 
+						if(offsetsY[fineIndexY + 1] != 0) yPercentNext += offsetsY[fineIndexY + 1];
 					}
 					
 					// calc grid positions based on interpolating columns between corners
@@ -126,6 +138,40 @@ extends BaseSavedQuadUI {
 					canvas.vertex(quadBotLeftX, quadBotLeftY, 0, 	mapX + mapW * uPercent, 		mapY + mapH * vPercentNext);
 				}
 			}
+			
+			// draw fine control debug lines
+			if(offsetsX != null && selectedColIndex != -1) {
+				float xPercent = selectedColIndex/stepsX;
+				if(offsetsX[selectedColIndex] != 0) xPercent += offsetsX[selectedColIndex]; 
+
+				float colTopX = interp(topLeft.x, topRight.x, xPercent);
+				float colTopY = interp(topLeft.y, topRight.y, xPercent);
+				float colBotX = interp(bottomLeft.x, bottomRight.x, xPercent);
+				float colBotY = interp(bottomLeft.y, bottomRight.y, xPercent);
+
+				canvas.stroke(0, 255, 0);
+				canvas.vertex(colTopX, colTopY, 0, 		0, 0);
+				canvas.vertex(colTopX, colTopY, 0, 		0, 0);
+				canvas.vertex(colBotX, colBotY, 0, 		0, 0);
+				canvas.vertex(colBotX, colBotY, 0, 		0, 0);
+			}
+			if(offsetsY != null && selectedRowIndex != -1) {
+				float yPercent = selectedRowIndex/stepsY;
+				if(offsetsY[selectedRowIndex] != 0) yPercent += offsetsY[selectedRowIndex]; 
+				
+				float rowTopX = interp(topLeft.x, bottomLeft.x, yPercent);
+				float rowTopY = interp(topLeft.y, bottomLeft.y, yPercent);
+				float rowBotX = interp(topRight.x, bottomRight.x, yPercent);
+				float rowBotY = interp(topRight.y, bottomRight.y, yPercent);
+				
+				canvas.stroke(0, 255, 0);
+				canvas.vertex(rowTopX, rowTopY, 0, 		0, 0);
+				canvas.vertex(rowTopX, rowTopY, 0, 		0, 0);
+				canvas.vertex(rowBotX, rowBotY, 0, 		0, 0);
+				canvas.vertex(rowBotX, rowBotY, 0, 		0, 0);
+			}
+//			else canvas.noStroke();
+
 		} else {
 			// default single mapped quad
 			canvas.vertex(topLeft.x, topLeft.y, 0, 			mapX, mapY);
@@ -150,7 +196,6 @@ extends BaseSavedQuadUI {
 		
 		float spacingX = (float) pg.width / (float) subDivideSteps;
 		float spacingY = (float) pg.height / (float) subDivideSteps;
-		float spacing2x = spacingX * 2f;
 		
 		for( int x=0; x < subDivideSteps; x++) {
 			for( int y=0; y < subDivideSteps; y++) {
@@ -164,5 +209,118 @@ extends BaseSavedQuadUI {
 		}
 		pg.endDraw();
 	}
+	
+	//////////////////////////////////////////
+	// FINE CONTROLS 
+	//////////////////////////////////////////
+	
+	// fine subdivision controls
+	protected float[] offsetsX = null;
+	protected float[] offsetsY = null;
+	protected int selectedRowIndex = -1;
+	protected int selectedColIndex = -1;
+	protected float offsetPushAmp = 0.0005f;
+	protected String configFile;
 
+	protected void initFineControl(String filePathFineOffsets) {
+		configFile = filePathFineOffsets;
+		buildMappingOffsets();
+		loadConfig();
+	}
+	
+	// init
+	
+	protected void buildMappingOffsets() {
+		// build offsets arrays
+		offsetsX = new float[(int) subDivideSteps + 1];
+		for (int i = 0; i < offsetsX.length; i++) offsetsX[i] = 0;
+		setOffsetsX(offsetsX);
+
+		offsetsY = new float[(int) subDivideSteps + 1];
+		for (int i = 0; i < offsetsY.length; i++) offsetsY[i] = 0;
+		setOffsetsY(offsetsY);
+	}
+	
+	// public
+	
+	public void setActive(boolean debug) {
+		super.setActive(debug);
+		if(!active) {
+			 selectedRowIndex = -1;
+			 selectedColIndex = -1;
+		}
+	}
+	
+	// config file
+
+	protected void loadConfig() {
+		if(FileUtil.fileExists(configFile)) {
+			String[] textLines = FileUtil.readTextFromFile(configFile);
+			String[] numbersStrArrayX = textLines[0].split(",");
+			for (int i = 0; i < numbersStrArrayX.length; i++) {
+				float offset = ConvertUtil.stringToFloat(numbersStrArrayX[i]);
+				if(i < offsetsX.length) offsetsX[i] = offset;
+			}
+			String[] numbersStrArrayY = textLines[0].split(",");
+			for (int i = 0; i < numbersStrArrayY.length; i++) {
+				float offset = ConvertUtil.stringToFloat(numbersStrArrayY[i]);
+				if(i < offsetsY.length) offsetsY[i] = offset;
+			}
+		}
+	}
+	
+	protected void saveConfig() {
+		String floatsStr = "";
+		for (int i = 0; i < offsetsX.length; i++) {
+			if(i > 0) floatsStr += ","; 
+			floatsStr += offsetsX[i];
+		}
+		floatsStr += FileUtil.NEWLINE;
+		for (int i = 0; i < offsetsY.length; i++) {
+			if(i > 0) floatsStr += ","; 
+			floatsStr += offsetsY[i];
+		}
+		FileUtil.writeTextToFile(configFile, floatsStr);
+	}
+
+	// key commands
+	
+	public void keyEvent(KeyEvent e) {
+		super.keyEvent(e);
+		if(active == false) return;
+		if(e.getAction() == KeyEvent.PRESS) {
+			// fine controls
+			if(e.getKey() == 'Z') {
+				selectedColIndex--;
+				if(selectedColIndex < -1) selectedColIndex = offsetsX.length - 1;
+			}
+			if(e.getKey() == 'X') {
+				selectedColIndex++;
+				if(selectedColIndex >= offsetsX.length) selectedColIndex = -1;
+			}
+			if(e.getKey() == 'C') {
+				selectedRowIndex--;
+				if(selectedRowIndex < -1) selectedRowIndex = offsetsY.length - 1;
+			}
+			if(e.getKey() == 'V')  {
+				selectedRowIndex++;
+				if(selectedRowIndex >= offsetsY.length) selectedRowIndex = -1;
+			}
+			if(e.getKey() == '{')  fineControlAdjust(offsetsX, selectedColIndex, -1f);
+			if(e.getKey() == '}') fineControlAdjust(offsetsX, selectedColIndex, 1f);
+			if(e.getKey() == ':') fineControlAdjust(offsetsY, selectedRowIndex, -1f);
+			if(e.getKey() == '"') fineControlAdjust(offsetsY, selectedRowIndex, 1f);
+			if(e.getKey() == 'E') saveConfig();
+		}
+	}
+	
+	// navigate through rows/cols
+	
+	protected void fineControlAdjust(float[] offsets, int index, float amount) {
+		amount *= offsetPushAmp;
+		if(index != -1) {
+			offsets[index] += amount;
+		}
+	}
+	
 }
--- src/com/haxademic/demo/draw/mapping/Demo_PGraphicsKeystone_Fine.java
+++ src/com/haxademic/demo/draw/mapping/Demo_PGraphicsKeystone_Fine.java
@@ -33,52 +33,65 @@ extends PAppletHax {
 
 	public void setupFirstFrame() {
 		buildCanvas();
+		
+		p.debugView.setHelpLine("__ Key Commands", "__\n");
+		p.debugView.setHelpLine("ESC |", "Quit");
+		p.debugView.setHelpLine("D |", "Toggle keystone active");
+		p.debugView.setHelpLine("T |", "Test pattern toggle");
+		p.debugView.setHelpLine("R |", "Reset corners");
+		
+		p.debugView.setHelpLine("X |", "Next col");
+		p.debugView.setHelpLine("Z |", "Prev col");
+		p.debugView.setHelpLine("C |", "Next row");
+		p.debugView.setHelpLine("V |", "Prev row");
+		p.debugView.setHelpLine("[ |", "Adjust down");
+		p.debugView.setHelpLine("] |", "Adjust up");
+		p.debugView.setHelpLine("E |", "Export config");
 	}
-
+	
 	protected void buildCanvas() {
+		// build buffer and keystone object
 		buffer = p.createGraphics( p.width / 2, p.height / 2, P.P3D );
 		buffer.smooth(OpenGLUtil.SMOOTH_HIGH);
-		keystonedPG = new PGraphicsKeystone( p, buffer, subdivisions, FileUtil.getFile("text/keystoning/keystone-demo.txt") );
-		
-		// add offsets
-		offsetsX = new float[subdivisions];
-		for (int i = 0; i < offsetsX.length; i++) {
-			offsetsX[i] = P.sin(i) * 0.01f;
-		}
-		keystonedPG.setOffsetsX(offsetsX);
-
-		offsetsY = new float[subdivisions];
-		for (int i = 0; i < offsetsY.length; i++) {
-			offsetsY[i] = P.sin(i) * 0.01f;
-		}
-		keystonedPG.setOffsetsY(offsetsY);
+		keystonedPG = new PGraphicsKeystone( p, buffer, subdivisions, FileUtil.getFile("text/keystoning/keystone-demo.txt"), FileUtil.getFile("text/keystoning/fine-mapping-demo.txt") );
 	}
-
+	
 	public void drawApp() {
 		p.background(0);
 		
-		for (int i = 0; i < offsetsX.length; i++) {
-			offsetsX[i] = P.sin(i + p.frameCount * 0.03f) * 0.02f;
-			offsetsX[0] = 0;
-		}
-		for (int i = 0; i < offsetsY.length; i++) {
-			offsetsY[i] = P.sin(i + p.frameCount * 0.03f) * 0.02f;
-			offsetsY[0] = 0;
-		}
+		// oscuillateOffsets();
 		
+		// update mapped texture
 		buffer.beginDraw();
-		buffer.background(0,255,0);
-		buffer.fill(0);
-		buffer.rect(40, 40, 100, 100);
 		ImageUtil.cropFillCopyImage(DemoAssets.justin(), buffer, true);
 		buffer.endDraw();
 		
+		// draw keystone debug & to screen 
 		if(testPattern == true) keystonedPG.drawTestPattern();
 		keystonedPG.update(p.g);
 	}
-
+	
+//	protected void debugOffsets() {
+//		p.debugView.setValue("selectedRowIndex", selectedRowIndex);
+//		p.debugView.setValue("selectedColIndex", selectedColIndex);
+//		for (int i = 0; i < offsetsX.length; i++) {
+//			p.debugView.setValue("offsetsX["+i+"]", offsetsX[i]);
+//		}
+//	}
+	
+	protected void oscillateOffsets() {
+		for (int i = 0; i < offsetsX.length; i++) {
+			offsetsX[i] = P.sin(i + p.frameCount * 0.03f) * 0.02f;
+		}
+		for (int i = 0; i < offsetsY.length; i++) {
+			offsetsY[i] = P.sin(i + p.frameCount * 0.03f) * 0.02f;
+		}
+	}
+	
 	public void keyPressed() {
 		super.keyPressed();
+		
+		// standard controls
 		if(p.key == 'd') {
 			debug = !debug;
 			keystonedPG.setActive(debug);
@@ -86,5 +99,4 @@ extends PAppletHax {
 		if(p.key == 't') testPattern = !testPattern;
 		if(p.key == 'r') keystonedPG.resetCorners();
 	}
-
 }
