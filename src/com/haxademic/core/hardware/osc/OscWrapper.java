f1f761c0bf7f1c7849763b15138208e4b93eeb5c
Justin Gitlin <cacheflowe@users.noreply.github.com>
2018-10-23 16:24:43 -0600

    Try a more open OSC ip address for multicasting

--- src/com/haxademic/core/hardware/osc/OscWrapper.java
+++ src/com/haxademic/core/hardware/osc/OscWrapper.java
@@ -20,7 +20,8 @@ public class OscWrapper {
 
 	public OscWrapper() {
 //		_oscP5 = new OscP5(this, 12000);
-		_oscP5 = new OscP5(this,"239.0.0.1",7777);	// multicast! check the demo for more info
+//		_oscP5 = new OscP5(this,"239.0.0.1",7777);	// multicast! check the demo for more info
+		_oscP5 = new OscP5(this,"224.0.1.0",7777);	// multicast! check the demo for more info
 //		_remoteLocation = new NetAddress("127.0.0.1",12000);
 		oscMsgMap = new HashMap<String, Float>();
 		oscMsgState = new HashMap<String, InputState>();
