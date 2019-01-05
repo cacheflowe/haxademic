7968d3e7c7ded0cd5940471b2451d26f21f62a2a
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-01-05 11:20:11 -0700

    Load JSON from local file

--- src/com/haxademic/core/net/JsonUtil.java
+++ src/com/haxademic/core/net/JsonUtil.java
@@ -1,6 +1,7 @@
 package com.haxademic.core.net;
 
 import com.haxademic.core.app.P;
+import com.haxademic.core.file.FileUtil;
 
 import processing.data.JSONObject;
 
@@ -19,6 +20,17 @@ public class JsonUtil {
 			P.out("JSON.parse() failed"); 
 			return false;
 		}
-
 	}
+	
+	public static JSONObject jsonFromFile(String jsonPath) {
+		String[] fileLines = FileUtil.readTextFromFile(jsonPath);
+		String jsonStr = String.join("\n", fileLines);
+		try {
+			return JSONObject.parse(jsonStr);
+		} catch (Exception e) {
+			P.out("JSON.parse() failed"); 
+			return null;
+		}
+	}
+
 }
