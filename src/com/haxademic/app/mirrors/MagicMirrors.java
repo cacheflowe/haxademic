2f30986fa2768e2b45cdf3d90c90a833bea52c2c
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-01-15 11:31:30 -0700

    Fix imports

--- src/com/haxademic/app/mirrors/MagicMirrors.java
+++ src/com/haxademic/app/mirrors/MagicMirrors.java
@@ -1,7 +1,7 @@
 package com.haxademic.app.mirrors;
 
 import com.haxademic.core.app.PAppletHax;
-import com.haxademic.core.constants.AppSettings;
+import com.haxademic.core.app.config.AppSettings;
 import com.haxademic.core.data.constants.PRenderers;
 import com.haxademic.core.draw.filters.pgraphics.GPUParticlesLauncher;
 import com.haxademic.core.draw.filters.pgraphics.SmokeFeedback;
