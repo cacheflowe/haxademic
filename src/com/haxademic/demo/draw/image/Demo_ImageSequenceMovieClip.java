2cdd8861ba1fbb85f214c71a1f32e4ab168302dc
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-01-18 20:45:34 -0700

    Fix up ImageSequenceMovieClip

--- src/com/haxademic/demo/draw/image/Demo_ImageSequenceMovieClip.java
+++ src/com/haxademic/demo/draw/image/Demo_ImageSequenceMovieClip.java
@@ -1,5 +1,6 @@
 package com.haxademic.demo.draw.image;
 
+import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.app.config.AppSettings;
 import com.haxademic.core.draw.image.ImageSequenceMovieClip;
@@ -10,10 +11,12 @@ extends PAppletHax {
 	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 	
 	protected ImageSequenceMovieClip imageSequence;
+	protected ImageSequenceMovieClip imageSequenceCopy;
+	protected ImageSequenceMovieClip imageSequenceManual;
 
 	protected void overridePropsFile() {
 		p.appConfig.setProperty( AppSettings.WIDTH, 1344 );
-		p.appConfig.setProperty( AppSettings.HEIGHT, 600 );
+		p.appConfig.setProperty( AppSettings.HEIGHT, 700 );
 		p.appConfig.setProperty( AppSettings.FULLSCREEN, false );
 		p.appConfig.setProperty( AppSettings.SHOW_DEBUG, true );
 	}
@@ -36,7 +39,26 @@ extends PAppletHax {
 		imageSequence.update();
 		
 		p.image(imageSequence.image(), 0, 0);
+		
 		p.debugView.setValue("imageSequence.isFinished()", imageSequence.isFinished());
+
+		// create a copy & play it offset
+		if(imageSequenceCopy == null && imageSequence.numImageFiles() > 0) {
+			imageSequenceCopy = imageSequence.copy();
+			imageSequenceManual = imageSequence.copy();
+		}
+		if(imageSequenceCopy != null) {
+			if(p.frameCount % 100 == 70) {
+				imageSequenceCopy.play();
+			}
+			imageSequenceCopy.update();
+			p.image(imageSequenceCopy.image(), 0, 50);
+			p.debugView.setValue("imageSequenceCopy.isFinished()", imageSequenceCopy.isFinished());
+			
+			// manual copy
+			p.image(imageSequenceManual.getFrameByProgress(p.mousePercentX()), 0, 100);
+		}
+		
 	}
 	
 }
