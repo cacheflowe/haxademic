ce849631300ce079aa8e718cf5f2ec98d9f0f4d1
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-01-20 23:10:21 -0500

    Update color-distance-two-textures.glsl

--- data/haxademic/shaders/filters/color-distance-two-textures.glsl
+++ data/haxademic/shaders/filters/color-distance-two-textures.glsl
@@ -16,8 +16,8 @@ void main() {
   vec4 color1 = texture2D(tex1, vertTexCoord.xy);
   vec4 color2 = texture2D(tex2, vertTexCoord.xy);
   float colorDistance = distance(color1.rgb, color2.rgb) / 3.;
-  if(color1.a < 0.1 || color2.a < 0.1) {
-    gl_FragColor = vec4(vec3(0), 1.);
+  if(color1.a + color2.a < 1.5) {
+    gl_FragColor = vec4(vec3(0., 0., 0.), 1.);
   } else {
     gl_FragColor = vec4(vec3(colorDistance), 1.);
   }
