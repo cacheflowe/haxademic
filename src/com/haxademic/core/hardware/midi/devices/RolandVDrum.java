82619e228c2b52e4a02d418d388ee959aec30065
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-03-28 15:36:03 -0600

    Add V-Drum midi notes

---
+++ src/com/haxademic/core/hardware/midi/devices/RolandVDrum.java
@@ -0,0 +1,14 @@
+package com.haxademic.core.hardware.midi.devices;
+
+public class RolandVDrum {
+	public static int SNARE = 38;
+	public static int HAT = 46;
+	public static int CYMBAL_LEFT = 49;
+	public static int CYMBAL_RIGHT = 51;
+	public static int TOM_1 = 48;
+	public static int TOM_2 = 45;
+	public static int TOM_3 = 43;
+	public static int KICK = 36;
+	public static int HAT_PEDAL = 44;
+	public static int HAT_PEDAL_VELOCITY_CC = 4;
+}
