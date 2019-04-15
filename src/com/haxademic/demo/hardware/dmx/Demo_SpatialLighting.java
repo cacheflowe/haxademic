1b30f5bf2a8e722a8c01a21a30681003a0ce3450
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-04-15 15:47:43 -0600

    Wrapped up basic spatial lighting app

--- src/com/haxademic/demo/hardware/dmx/Demo_SpatialLighting.java
+++ src/com/haxademic/demo/hardware/dmx/Demo_SpatialLighting.java
@@ -8,8 +8,10 @@ import com.haxademic.core.app.config.AppSettings;
 import com.haxademic.core.draw.color.EasingColor;
 import com.haxademic.core.draw.context.DrawUtil;
 import com.haxademic.core.draw.shapes.Shapes;
+import com.haxademic.core.file.DemoAssets;
 import com.haxademic.core.hardware.dmx.DMXWrapper;
 
+import processing.core.PGraphics;
 import processing.core.PImage;
 import processing.core.PVector;
 
@@ -34,6 +36,7 @@ extends PAppletHax {
 	protected String ROOM_SIZE = 				SPATIAL_LIGHTING_PREFIX + "ROOM_SIZE";
 	protected String FLOORPLAN_ALPHA = 			SPATIAL_LIGHTING_PREFIX + "FLOORPLAN_ALPHA";
 	protected String ROOM_ROTATION = 			SPATIAL_LIGHTING_PREFIX + "ROOM_ROTATION";
+	protected String ORTHO_VIEW = 				SPATIAL_LIGHTING_PREFIX + "ORTHO_VIEW";
 	
 	// color generation
 	protected String NOISE_FREQ = 				SPATIAL_LIGHTING_PREFIX + "NOISE_FREQ";
@@ -47,15 +50,14 @@ extends PAppletHax {
 	float roomH = 300;
 	float roomD = 300;
 	protected PImage floorplan;
+	protected PImage elevation;
 
 	// dmx communication
 	protected DMXWrapper dmxWrapper;
-	
+	protected String LIGHT_DEBUG_CHANNEL = 		SPATIAL_LIGHTING_PREFIX + "LIGHT_DEBUG_CHANNEL";
+
 	// dmx lights
 	protected ArrayList<DMXLightRGB> lights = new ArrayList<DMXLightRGB>();
-	protected DMXLightRGB light1;
-	protected DMXLightRGB light2;
-	protected DMXLightRGB light3;
 
 	
 	protected void overridePropsFile() {
@@ -65,11 +67,14 @@ extends PAppletHax {
 	}
 	
 	public void setupFirstFrame () {
-		// load assets
-		floorplan = DemoAssets.textureNebula(); 
-		
+		loadAssets();
 		buildControls();
 		initDMX();
+	}
+	
+	protected void loadAssets() {
+		floorplan = DemoAssets.textureNebula();	
+		elevation = DemoAssets.textureJupiter();	
 	}
 	
 	protected void buildControls() {
@@ -77,6 +82,7 @@ extends PAppletHax {
 		p.prefsSliders.addSlider(FLOORPLAN_ALPHA, 1, 0, 1, 0.01f);
 		p.prefsSliders.addSliderVector(ROOM_SIZE, 500, 200, 1500f, 1f, false);
 		p.prefsSliders.addSliderVector(ROOM_ROTATION, 0, -1f, 1f, 0.001f, false);
+		p.prefsSliders.addSlider(ORTHO_VIEW, 0, 0, 1f, 1f, false);
 		
 		// noise for color
 		p.prefsSliders.addSlider(NOISE_FREQ, 0.001f, 0.0001f, 0.003f, 0.00001f);
@@ -91,77 +97,94 @@ extends PAppletHax {
 	protected void initDMX() {
 		// build DMX lights
 		dmxWrapper = new DMXWrapper("COM7", 9600);
+		p.prefsSliders.addSlider(LIGHT_DEBUG_CHANNEL, 0, 0, 255, 1f);
 
 		// init lights
-		light1 = new DMXLightRGB(4, true);
-		light2 = new DMXLightRGB(1, true);
-		light3 = new DMXLightRGB(7, true);
-		
-		// build array
-		lights.add(light1);
-		lights.add(light2);
-		lights.add(light3);
+		lights.add(new DMXLightRGB(1, true));
+		lights.add(new DMXLightRGB(4, true));
+		lights.add(new DMXLightRGB(7, true));
 	}
 	
 	public void drawApp() {
-		// bg components
 		p.background(0);
 		
-		// 3d rotation
-		// p.ortho();
-		p.perspective();
-		p.lights();
-		DrawUtil.setCenterScreen(p.g);
-		DrawUtil.setDrawCenter(p.g);
-		p.translate(0, 0, p.prefsSliders.value(ROOM_ROTATION + "_Z") * p.width);
-		p.rotateX(p.prefsSliders.value(ROOM_ROTATION + "_X") * P.TWO_PI);
-		p.rotateY(p.prefsSliders.value(ROOM_ROTATION + "_Y") * P.TWO_PI);
+		pg.beginDraw();
+		setContext();
+		buildRoom();
+		drawFloorplan();
+		updateLightSource();
+		updateDrawLights();
+		drawColorGrid();
+		pg.endDraw();
 		
-		///////////////////////////
-		// DEFINE ROOM SIZE
-		///////////////////////////
+		p.image(pg, 0, 0);
+	}
+	
+	protected void setContext() {
+		pg.background(0);
+		if(p.prefsSliders.value(ORTHO_VIEW) > 0.5f) pg.ortho();
+		else pg.perspective();
+		pg.lights();
+		DrawUtil.setCenterScreen(pg);
+		DrawUtil.setDrawCenter(pg);
+		pg.translate(0, 0, p.prefsSliders.value(ROOM_ROTATION + "_Z") * pg.width);
+		pg.rotateX(p.prefsSliders.value(ROOM_ROTATION + "_X") * P.TWO_PI);
+		pg.rotateY(p.prefsSliders.value(ROOM_ROTATION + "_Y") * P.TWO_PI);	
+	}
+	
+	protected void buildRoom() {
 		roomW = p.prefsSliders.value(ROOM_SIZE + "_X");
 		roomH = p.prefsSliders.value(ROOM_SIZE + "_Y");
 		roomD = p.prefsSliders.value(ROOM_SIZE + "_Z");
 		
 		// draw bounding box
-		p.noFill();
-		p.stroke(255);
-		Shapes.drawDashedBox(p.g, roomW, roomH, roomD, 20, true);
-		p.noStroke();
-
-		///////////////////////////
-		// DRAW FLOOR PLAN
-		///////////////////////////;
+		pg.noFill();
+		pg.stroke(255);
+		Shapes.drawDashedBox(pg, roomW, roomH, roomD, 20, true);
+		pg.noStroke();
+	}
+	
+	protected void drawFloorplan() {
 		float floorplanAlpha = p.prefsSliders.value(FLOORPLAN_ALPHA);
 		if(floorplanAlpha > 0) {
-			p.noStroke();
-			DrawUtil.push(p.g);
-			p.translate(0, roomH / 2f, 0);
-			p.rotateX(P.HALF_PI);
-			DrawUtil.setPImageAlpha(p.g, floorplanAlpha);
-			p.image(floorplan, 0, 0, roomW, roomD);
-			DrawUtil.resetPImageAlpha(p.g);
-			DrawUtil.pop(p.g);
+			pg.noStroke();
+			
+			// floor
+			if(floorplan != null) {
+				DrawUtil.push(pg);
+				pg.translate(0, roomH / 2f, 0);
+				pg.rotateX(P.HALF_PI);
+				DrawUtil.setPImageAlpha(pg, floorplanAlpha);
+				pg.image(floorplan, 0, 0, roomW, roomD);
+				DrawUtil.resetPImageAlpha(pg);
+				DrawUtil.pop(pg);
+			}
+			
+			// elevation
+			if(elevation != null) {
+				DrawUtil.push(pg);
+				pg.translate(0, 0, -roomD / 2f);
+				DrawUtil.setPImageAlpha(pg, floorplanAlpha);
+				pg.image(elevation, 0, 0, roomW, roomH);
+				DrawUtil.resetPImageAlpha(pg);
+				DrawUtil.pop(pg);
+			}
 		}
-		
-		///////////////////////////
-		// CALC VOLUMETRIC LIGHTS
-		///////////////////////////
-		noiseGlobalOffset.add(p.prefsSliders.value(NOISE_SCROLL_DIRECTION + "_X"), p.prefsSliders.value(NOISE_SCROLL_DIRECTION + "_Y"), p.prefsSliders.value(NOISE_SCROLL_DIRECTION + "_Z"));
-		
-		///////////////////////////
-		// DRAW LIGHTS
-		///////////////////////////
-		
-		DrawUtil.setDrawCorner(p.g);
+	}
+	
+	protected void updateDrawLights() {
+		DrawUtil.setDrawCorner(pg);
 		for (int i = 0; i < lights.size(); i++) {
-			lights.get(i).update();
+			lights.get(i).update(pg);
 		}
-		
-		///////////////////////////
-		// DRAW COLOR GUIDE/GRID
-		///////////////////////////
+	}
+	
+	protected void updateLightSource() {
+		// update scroll direction
+		noiseGlobalOffset.add(p.prefsSliders.value(NOISE_SCROLL_DIRECTION + "_X"), p.prefsSliders.value(NOISE_SCROLL_DIRECTION + "_Y"), p.prefsSliders.value(NOISE_SCROLL_DIRECTION + "_Z"));
+	}
+	
+	protected void drawColorGrid() {
 		float noiseSpacing = p.prefsSliders.value(NOISE_GRID_SPACING);
 		for (int x = 0; x <= roomW; x += noiseSpacing) {
 			for (int y = 0; y <= roomH; y += noiseSpacing) {
@@ -170,16 +193,20 @@ extends PAppletHax {
 					float gridY = -roomH/2f + y;
 					float gridZ = -roomD/2f + z;
 					int cellColor = colorFromPosition(gridX, gridY, gridZ);
-					p.fill(cellColor);
-					p.pushMatrix();
-					p.translate(gridX, gridY, gridZ);
-					p.box(p.prefsSliders.value(NOISE_GRID_CUBE_SIZE) * (1f * P.p.brightness(cellColor)/255f));
-					p.popMatrix();
+					pg.fill(cellColor);
+					pg.pushMatrix();
+					pg.translate(gridX, gridY, gridZ);
+					pg.box(p.prefsSliders.value(NOISE_GRID_CUBE_SIZE) * (1f * P.p.brightness(cellColor)/255f));
+					pg.popMatrix();
 				}	
 			}
 		}
 	}
 	
+	///////////////////////////
+	// COLOR GENERATOR 3D
+	///////////////////////////
+
 	protected int colorFromPosition(float x, float y, float z) {
 		float noiseAmp = p.prefsSliders.value(NOISE_FREQ);
 
@@ -237,27 +264,47 @@ extends PAppletHax {
 		public float mapHigh() { return mapHigh; }
 		public DMXLightRGB setMapLowHigh(float mapLow, float mapHigh) { this.mapLow = mapLow; this.mapHigh = mapHigh; return this; }
 		
-		public void update() {
-			// lerp color
-			color.update();
-			
-			// use slider to set a single light's positino
+		public void update(PGraphics pg) {
+			updatePosition();
+			updateColor();
+			drawLight(pg);
+			sendDMX();
+		}
+		
+		protected void updatePosition() {
 			if(this.hasSlider) {
 				position.set(p.prefsSliders.value(posSliderKey + "_X"), p.prefsSliders.value(posSliderKey + "_Y"), p.prefsSliders.value(posSliderKey + "_Z"));
+			}	
+		}
+		
+		protected void updateColor() {
+			int colorOut = colorFromPosition(position.x * roomW, position.y * roomH, position.z * roomD);
+			color.setTargetInt(colorOut);
+			
+			// override color if testing channel
+			if(dmxChannel == p.prefsSliders.valueInt(LIGHT_DEBUG_CHANNEL)) {
+				int colorComponent = (P.round(p.frameCount / 8) % 2 == 0) ? 0 : 255;
+				color.setCurrentInt(p.color(colorComponent));
 			}
 			
-			// draw light in 3d space
-			DrawUtil.push(p.g);
-			p.fill(colorFromPosition(position.x * roomW, position.y * roomH, position.z * roomD));
-			p.translate(position.x * roomW, position.y * roomH, position.z * roomD);
-			p.sphere(20);
+			// lerp color
+			color.update();	
+		}
+		
+		protected void drawLight(PGraphics pg) {
+			DrawUtil.push(pg);
+			pg.fill(color.colorInt());
+			pg.translate(position.x * roomW, position.y * roomH, position.z * roomD);
+			pg.sphere(20);
 			// draw debug channel text
-			p.translate(25, 0, 0);
-			p.fill(0, 255, 0);
-			p.text(name, 0, 0);
-			DrawUtil.pop(p.g);
-			
-			// send dmx
+			pg.translate(25, 0, 0);
+			pg.fill(0, 255, 0);
+			pg.text(name, 0, 0);
+			// pop
+			DrawUtil.pop(pg);
+		}
+		
+		protected void sendDMX() {
 			dmxWrapper.setValue(dmxChannel + 0, P.round(P.map(color.r(), 0, 255, mapLow, mapHigh)));
 			dmxWrapper.setValue(dmxChannel + 1, P.round(P.map(color.g(), 0, 255, mapLow, mapHigh)));
 			dmxWrapper.setValue(dmxChannel + 2, P.round(P.map(color.b(), 0, 255, mapLow, mapHigh)));
