de31d790d4eb1a94e07550b0b6d6ec2aef19e318
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-04-19 13:50:02 -0600

    Fix colors

--- src/com/haxademic/core/draw/textures/pgraphics/TextureSphereOfCubes.java
+++ src/com/haxademic/core/draw/textures/pgraphics/TextureSphereOfCubes.java
@@ -2,6 +2,7 @@ package com.haxademic.core.draw.textures.pgraphics;
 
 import com.haxademic.core.app.P;
 import com.haxademic.core.data.constants.PStrokeCaps;
+import com.haxademic.core.draw.color.ColorsHax;
 import com.haxademic.core.draw.context.DrawUtil;
 import com.haxademic.core.draw.textures.pgraphics.shared.BaseTexture;
 import com.haxademic.core.math.MathUtil;
@@ -13,12 +14,11 @@ extends BaseTexture {
 	protected float frames = 0;
 	protected EasingFloat lineWeight = new EasingFloat(2, 6);
 
-	protected EasingFloat baseRadius = new EasingFloat(100, 6);
-	protected EasingFloat boxSize = new EasingFloat(20, 6);
-	protected EasingFloat numCubes = new EasingFloat(32, 6);
-	protected EasingFloat twist = new EasingFloat(1, 6);
+	protected EasingFloat baseRadius = new EasingFloat(100, 20);
+	protected EasingFloat boxSize = new EasingFloat(20, 12);
+	protected EasingFloat numCubes = new EasingFloat(32, 20);
+	protected EasingFloat twist = new EasingFloat(1, 20);
 	protected int _baseColor = 0xffffffff;
-	protected float startPhi = 0;
 
 	
 	public TextureSphereOfCubes( int width, int height ) {
@@ -55,14 +55,16 @@ extends BaseTexture {
 			// get sphere coordinate position
 			float phi = P.acos( -1f + ( 2f * i - 1f ) / numCubes.value() );
 			float theta = P.sqrt( numCubes.value() * P.PI ) * phi;
-     		float pointX = baseRadius.value() * 1.2f * P.cos(theta)* P.sin(phi);
+     		float pointX = baseRadius.value() * P.cos(theta)* P.sin(phi);
      		float pointY = baseRadius.value() * P.sin(theta)* P.sin(phi);
      		float pointZ = baseRadius.value() * P.cos(phi);
 
      		// get size and alpha and draw cube
      		float size = boxSize.value() * (1f + P.p.audioFreq(i + 20));
-     		float oscInc = P.p.frameCount * 0.03f;
+     		float oscInc = P.p.frameCount * 0.04f;
 			
+     		_texture.fill(ColorsHax.COLOR_GROUPS[0][i % 4]);
+     		
      		// position, rotate, draw
      		_texture.pushMatrix();
      		_texture.translate(pointX, pointY, pointZ);
@@ -70,9 +72,9 @@ extends BaseTexture {
      		_texture.rotateY( twist.value() * i );
      		_texture.rotateZ( twist.value() * i );
 			_texture.box(
-					size * (1f + 0.6f * P.sin(i+0 + oscInc)), 
-					size * (1f + 0.6f * P.sin(i+1 + oscInc)), 
-					size * (1f + 0.6f * P.sin(i+2 + oscInc))
+					size * (1f + 0.9f * P.sin(i+0 + oscInc)), 
+					size * (1f + 0.9f * P.sin(i+1 + oscInc)), 
+					size * (1f + 0.9f * P.sin(i+2 + oscInc))
 			);
 			_texture.popMatrix();
 		}
@@ -80,18 +82,27 @@ extends BaseTexture {
 	
 	public void updateTiming() {
 		if(MathUtil.randBoolean(P.p)) twist.setTarget(MathUtil.randRangeDecimal(0, 0.25f));
-		if(MathUtil.randBoolean(P.p)) baseRadius.setTarget(MathUtil.randRangeDecimal(_texture.height * 0.25f, _texture.height * 0.254f));
+		if(MathUtil.randBoolean(P.p)) {
+			if(P.p.frameCount % 1200 < 600)
+				baseRadius.setTarget(MathUtil.randRangeDecimal(_texture.height * 0.25f, _texture.height * 0.35f));
+			else
+				baseRadius.setTarget(MathUtil.randRangeDecimal(_texture.height * 0.85f, _texture.height * 1.2f));
+		}
 		
 		if(MathUtil.randBoolean(P.p)) {
 			float curNumCubes = numCubes.value();
 			curNumCubes = curNumCubes + MathUtil.randRange(-10, 10);
 			curNumCubes = P.constrain(curNumCubes, 50, 100);
+//			float curNumCubes = baseRadius.target() * (0.4f + MathUtil.randRangeDecimal(-0.1f, 0.1f));
 			numCubes.setTarget(curNumCubes);
 		}
+		
+		float low = baseRadius.target() / 15f;
+		float high = low * 2f;
+		boxSize.setTarget(MathUtil.randRangeDecimal(low, high));
 	}
 	
 	public void updateTimingSection() {
-		boxSize.setTarget(MathUtil.randRangeDecimal(10, 40));
 //		wobbleFreq.setTarget(MathUtil.randRangeDecimal(0.001f, 0.01f));
 //		wobbleAmp.setTarget(MathUtil.randRangeDecimal(-0.2f, 0.2f));
 //		spacing.setTarget(MathUtil.randRangeDecimal(30, 70));
