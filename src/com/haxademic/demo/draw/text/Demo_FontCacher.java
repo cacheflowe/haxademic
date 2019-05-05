d50dbe9271e51f9060090e3cfd12a398040c103a
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-05-04 20:34:59 -0600

    Update demo font paths

--- src/com/haxademic/demo/draw/text/Demo_FontCacher.java
+++ src/com/haxademic/demo/draw/text/Demo_FontCacher.java
@@ -3,6 +3,7 @@ package com.haxademic.demo.draw.text;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.data.constants.PTextAlign;
 import com.haxademic.core.draw.text.FontCacher;
+import com.haxademic.core.file.DemoAssets;
 import com.haxademic.core.file.FileUtil;
 
 import processing.core.PFont;
@@ -14,12 +15,12 @@ extends PAppletHax {
 	public void drawApp() {
 		background(0);
 
-		String fontFile = "haxademic/fonts/OpenSans-Regular.ttf";
+		String fontFile = DemoAssets.fontOpenSansPath;
 		PFont font = FontCacher.getFont(fontFile, 42);
 		FontCacher.setFontOnContext(p.g, font, p.color(255), 1f, PTextAlign.LEFT, PTextAlign.TOP);
 		p.text(fontFile, 20, 20);
 		
-		String fontFile2 = "haxademic/fonts/DSEG7ClassicMini-Regular.ttf";
+		String fontFile2 = DemoAssets.fontDSEG7Path;
 		font = FontCacher.getFont(fontFile2, 18);
 		FontCacher.setFontOnContext(p.g, font, p.color(0, 255, 0), 1.8f, PTextAlign.LEFT, PTextAlign.TOP);
 		p.text(fontFile2 + FileUtil.NEWLINE + fontFile2, 20, 100);
