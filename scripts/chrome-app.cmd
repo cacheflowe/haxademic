8926501bd1ae521322ec8e82aa3111cdad39d1f4
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-06-26 12:18:25 -0600

    Add flags to allow getUserMedia in Chromium without user interaction

--- scripts/chrome-app.cmd
+++ scripts/chrome-app.cmd
@@ -1,2 +1,2 @@
 echo Starting the browser app
-"C:\Users\cacheflowe\AppData\Local\Chromium\Application\chrome.exe" --app=%1 --chrome-frame --incognito --disable-pinch --overscroll-history-navigation=0 --disable-session-crashed-bubble --disable-infobars --window-size=800,600
+"C:\Users\cacheflowe\AppData\Local\Chromium\Application\chrome.exe" --app=%1 --chrome-frame --incognito --disable-pinch --overscroll-history-navigation=0 --disable-session-crashed-bubble --disable-infobars --allow-file-access-from-files --allow-running-insecure-content --use-fake-ui-for-media-stream --autoplay-policy=no-user-gesture-required --window-size=800,600
