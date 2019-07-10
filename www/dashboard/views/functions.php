ca5a1231df8c6088d552416e0bd5096f2efb2183
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-07-10 01:35:56 -0400

    Fix offline class in dashboard when also relaunched

--- www/dashboard/views/functions.php
+++ www/dashboard/views/functions.php
@@ -107,8 +107,8 @@ function html_checkin_detail($jsonFile, $project_id, $showTitle, $isMostRecent=f
 
 
   // add classes for relaunch and offline
-  $relaunchClass = (isset($checkinJSON['relaunch'])) ? 'relaunched' : '';
-  $offlineClass = (($hoursAgo > 3 || $daysAgo > 1) && $isMostRecent == true) ? 'offline' : '';
+  $relaunchClass = (isset($checkinJSON['relaunch'])) ? ' relaunched' : '';
+  $offlineClass = (($hoursAgo > 3 || $daysAgo > 1) && $isMostRecent == true) ? ' offline' : '';
 
   // build html string with json data
   $html = "" .
