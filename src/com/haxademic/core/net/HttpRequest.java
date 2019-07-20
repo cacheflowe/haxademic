420bb2021584dfcd265143962495dac638b74d0f
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-07-20 13:19:53 -0600

    Basic web text retrieval object & demo

---
+++ src/com/haxademic/core/net/HttpRequest.java
@@ -0,0 +1,124 @@
+package com.haxademic.core.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
+import com.haxademic.core.app.P;
+
+public class HttpRequest {
+	
+	public interface IHttpRequestCallback {
+		public void postSuccess(String responseText, int responseCode);
+		public void postFailure(String errorMessage);
+	}
+
+	protected String requestURL;
+	protected String responseOut;
+	protected IHttpRequestCallback delegate;
+	protected Thread requestThread;
+	protected String responseText = null;
+	protected int responseCode = 0;
+	public static boolean DEBUG = false;
+
+	public HttpRequest(IHttpRequestCallback delegate) {
+		this.delegate = delegate;
+	}
+	
+	public void send(String requestURL) {
+		this.requestURL = requestURL;
+		new Thread(new Runnable() { public void run() {
+			makeRequest();
+		}}).start();
+	}
+
+	protected void makeRequest() {
+		// create request id
+		responseCode = 0;
+		
+		try {
+			HttpURLConnection httpcon = (HttpURLConnection) ((new URL(requestURL).openConnection()));
+			HttpURLConnection.setFollowRedirects(true);
+			httpcon.setDoOutput(true);
+	//		httpcon.setRequestProperty("Content-Type", "application/json");
+	//		httpcon.setRequestProperty("Accept", "application/json");
+			httpcon.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+			httpcon.setRequestMethod("GET");
+			httpcon.connect();
+			getFullResponse(httpcon);
+
+		} catch (ProtocolException e) {
+			e.printStackTrace();
+			if(delegate != null) delegate.postFailure(e.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+			if(delegate != null) delegate.postFailure(e.getMessage());
+		}
+	} 
+	
+	// from: https://github.com/eugenp/tutorials/blob/master/core-java-modules/core-java-networking/src/main/java/com/baeldung/http/FullResponseBuilder.java
+	protected void getFullResponse(HttpURLConnection con) throws IOException {
+        StringBuilder fullResponseBuilder = new StringBuilder();
+
+        fullResponseBuilder.append(con.getResponseCode())
+            .append(" ")
+            .append(con.getResponseMessage())
+            .append("\n");
+
+        con.getHeaderFields()
+            .entrySet()
+            .stream()
+            .filter(entry -> entry.getKey() != null)
+            .forEach(entry -> {
+
+                fullResponseBuilder.append(entry.getKey())
+                    .append(": ");
+
+                List<String> headerValues = entry.getValue();
+                Iterator<String> it = headerValues.iterator();
+                if (it.hasNext()) {
+                    fullResponseBuilder.append(it.next());
+
+                    while (it.hasNext()) {
+                        fullResponseBuilder.append(", ")
+                            .append(it.next());
+                    }
+                }
+
+                fullResponseBuilder.append("\n");
+            });
+
+        Reader streamReader = null;
+
+        responseCode = con.getResponseCode();
+        if (responseCode > 299) {
+            streamReader = new InputStreamReader(con.getErrorStream());
+        } else {
+            streamReader = new InputStreamReader(con.getInputStream());
+        }
+
+        BufferedReader in = new BufferedReader(streamReader);
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+
+        in.close();
+
+        fullResponseBuilder.append("Response: ").append(content);
+        if(DEBUG) {
+        	P.out("===========================");
+        	P.out(fullResponseBuilder.toString());
+        	P.out("===========================");
+        }
+        
+		if(delegate != null) delegate.postSuccess(content.toString(), responseCode);
+    }
+}		
---
+++ src/com/haxademic/demo/net/Demo_HttpRequest.java
@@ -0,0 +1,44 @@
+package com.haxademic.demo.net;
+
+import com.haxademic.core.app.P;
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.draw.context.PG;
+import com.haxademic.core.net.HttpRequest;
+import com.haxademic.core.net.HttpRequest.IHttpRequestCallback;
+
+public class Demo_HttpRequest
+extends PAppletHax
+implements IHttpRequestCallback {
+	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
+	
+	protected HttpRequest request;
+	
+	public void drawApp() {
+		// background
+		pg.beginDraw();
+		PG.setDrawCenter(pg);
+		pg.background((float)mouseX/width * 255,(float)mouseY/height * 255,0);
+		pg.fill(255);
+ 	}
+	
+	protected void makeRequest() {
+		HttpRequest.DEBUG = true;
+		if(request == null) request = new HttpRequest(this);
+		request.send("http://cacheflowe.com");
+	}
+	
+	public void keyPressed() {
+		super.keyPressed();
+		if(p.key == ' ') makeRequest();
+	}
+
+	// IHttpRequestCallback methods
+	
+	public void postSuccess(String responseText, int responseCode) {
+		P.out("HttpRequest success:", responseText);
+	}
+
+	public void postFailure(String errorMessage) {
+		P.out("HttpRequest error:", errorMessage);
+	}
+}
