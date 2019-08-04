6687e2439d5900fdc22dd8fbe095cc3c320c93db
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-08-03 18:33:17 -0600

    New abstract particle class

---
+++ src/com/haxademic/core/draw/particle/Particle.java
@@ -0,0 +1,130 @@
+package com.haxademic.core.draw.particle;
+
+import com.haxademic.core.math.MathUtil;
+import com.haxademic.core.math.easing.LinearFloat;
+import com.haxademic.core.math.easing.Penner;
+
+import processing.core.PGraphics;
+import processing.core.PImage;
+import processing.core.PVector;
+
+public class Particle {
+	
+	protected PVector pos = new PVector(0, 0, 0);
+	protected PVector speed = new PVector(0, 0, 0);
+	protected PVector speedMin = new PVector(0, 0);
+	protected PVector speedMax = new PVector(0, 0);
+
+	protected PVector gravity = new PVector(0, 0, 0);
+	protected PVector gravityMin = new PVector(0, 0);
+	protected PVector gravityMax = new PVector(0, 0);
+	
+	protected float size = 10f;
+	protected float sizeMin = 10f;
+	protected float sizeMax = 10f;
+	
+	protected float rotationMin = 0f;
+	protected float rotationMax = 0f;
+	
+	protected float lifespan = 60;
+	protected float lifespanMin = 60;
+	protected float lifespanMax = 60;
+	protected LinearFloat sizeProgress = new LinearFloat(0, 1f/lifespan);
+	
+	protected int color = 0xffffffff;
+	protected PImage image;
+	
+	public Particle() {}
+	
+	// Random range setters
+	
+	public Particle setSizeMinMax(float sizeMin, float sizeMax) {
+		this.sizeMin = sizeMin;
+		this.sizeMax = sizeMax;
+		return this;
+	}
+	
+	public Particle setLifespan(float lifespanMin, float lifespanMax) {
+		this.lifespanMin = lifespanMin; 
+		this.lifespanMax = lifespanMax; 
+		return this;
+	}
+	
+	public Particle setRotation(float rotationMin, float rotationMax) {
+		this.rotationMin = rotationMin;
+		this.rotationMax = rotationMax;
+		return this;
+	}
+	
+	public Particle setSpeed(float speedMinX, float speedMaxX, float speedMinY, float speedMaxY) {
+		this.speedMin.set(speedMinX, speedMinY);
+		this.speedMax.set(speedMaxX, speedMaxY);
+		return this;
+	}
+	
+	public Particle setGravity(float gravityMinX, float gravityMaxX, float gravityMinY, float gravityMaxY) {
+		this.gravityMin.set(gravityMinX, gravityMinY);
+		this.gravityMax.set(gravityMaxX, gravityMaxY);
+		return this;
+	}
+	
+	public Particle setColor(int color) {
+		this.color = color;
+		return this;
+	}
+	
+	// Launch!
+	
+	public Particle launch(float x, float y, PImage img) {
+		// get random particle texture
+		image = img;
+		
+		// random params
+		sizeProgress.setInc(1f/MathUtil.randRange(lifespanMin, lifespanMax));
+		sizeProgress.setCurrent(0);
+		sizeProgress.setTarget(1);
+		size = MathUtil.randRangeDecimal(sizeMin, sizeMax);
+		
+		// set motion properties
+		pos.set(x, y, 0);
+		speed.set(
+				MathUtil.randRangeDecimal(speedMin.x, speedMax.x), 
+				MathUtil.randRangeDecimal(speedMin.y, speedMax.y), 
+				MathUtil.randRangeDecimal(rotationMin, rotationMax));
+		gravity.set(
+				MathUtil.randRangeDecimal(gravityMin.x, gravityMax.x), 
+				MathUtil.randRangeDecimal(gravityMin.y, gravityMax.y));
+		
+		return this;
+	}
+	
+	// animate
+	
+	public void update(PGraphics pg) {
+		if(available(pg)) return;
+		
+		// update position
+		speed.add(gravity);
+		pos.add(speed);
+		
+		// update size
+		sizeProgress.update();
+		float curSize = size * Penner.easeOutExpo(sizeProgress.value(), 0, 1, 1);
+		if(sizeProgress.value() == 1) sizeProgress.setTarget(0);
+		
+		// draw image
+		pg.pushMatrix();
+		pg.translate(pos.x, pos.y);
+		pg.rotate(pos.z);
+		pg.tint(color);
+		pg.image(image, 0, 0, curSize, curSize);
+		pg.tint(255);
+		pg.popMatrix();
+	}
+	
+	public boolean available(PGraphics pg) {
+		boolean finished = (sizeProgress.value() == 0 && sizeProgress.target() == 0);
+		return finished || pos.y < -100 || pos.x < -100 || pos.y > pg.height + 100 || pos.x > pg.height + 100;
+	}
+}
+
--- src/com/haxademic/demo/draw/particle/Demo_ParticlesFromMap.java
+++ src/com/haxademic/demo/draw/particle/Demo_ParticlesFromMap.java
@@ -2,20 +2,18 @@ package com.haxademic.demo.draw.particle;
 
 import java.util.ArrayList;
 
-import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.app.config.AppSettings;
+import com.haxademic.core.data.constants.PBlendModes;
 import com.haxademic.core.draw.color.ColorUtil;
 import com.haxademic.core.draw.context.PG;
 import com.haxademic.core.draw.image.ImageUtil;
+import com.haxademic.core.draw.particle.Particle;
+import com.haxademic.core.file.FileUtil;
 import com.haxademic.core.math.MathUtil;
-import com.haxademic.core.math.easing.LinearFloat;
-import com.haxademic.core.math.easing.Penner;
 import com.haxademic.core.media.DemoAssets;
 
-import processing.core.PGraphics;
 import processing.core.PImage;
-import processing.core.PVector;
 import processing.video.Movie;
 
 public class Demo_ParticlesFromMap 
@@ -23,11 +21,13 @@ extends PAppletHax {
 	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 
 	protected Movie video;
-	protected ArrayList<ShapeParticle> shapes = new ArrayList<ShapeParticle>();
+	protected ArrayList<Particle> particles = new ArrayList<Particle>();
 	protected PImage[] particleImages;
+	protected int MAX_ATTEMPTS = 2000;
+	protected int MAX_LAUNCHES_PER_FRAME = 50;
+	protected boolean useMultipleParticles = true;
 	
 	protected void overridePropsFile() {
-		p.appConfig.setProperty( AppSettings.LOOP_FRAMES, 160 );
 		p.appConfig.setProperty( AppSettings.WIDTH, 800 );
 		p.appConfig.setProperty( AppSettings.HEIGHT, 800 );
 	}
@@ -38,9 +38,17 @@ extends PAppletHax {
 		video.loop();
 		
 		// load particle source textures
-		particleImages = new PImage[] {
-			DemoAssets.particle(),
-		};
+		if(useMultipleParticles) {
+			ArrayList<PImage> particles = FileUtil.loadImagesFromDir(FileUtil.getFile("haxademic/images/particles/"), "png");
+			particleImages = new PImage[particles.size()];
+			for (int i = 0; i < particles.size(); i++) {
+				particleImages[i] = particles.get(i);
+			}
+		} else {
+			particleImages = new PImage[] {
+					DemoAssets.particle(),
+			};
+		}
 	}
 	
 	public void drawApp() {
@@ -55,104 +63,61 @@ extends PAppletHax {
 		
 		// launch particles
 		int numLaunched = 0;
-		for (int i = 0; i < 2000; i++) {
+		for (int i = 0; i < MAX_ATTEMPTS; i++) {
 			int checkX = MathUtil.randRange(0, pg.width);
 			int checkY = MathUtil.randRange(0, pg.height);
 			int pixelColor = ImageUtil.getPixelColor(pg, checkX, checkY);
 			float redColor = (float) ColorUtil.redFromColorInt(pixelColor) / 255f;
-			if(redColor > 0.5f && numLaunched < 150) {
-				launchShape(checkX, checkY);
+			if(redColor > 0.5f && numLaunched < MAX_LAUNCHES_PER_FRAME) {
+				launchParticle(checkX, checkY);
 				numLaunched++;
 			}
 		}
 		// draw particles on top
 		pg.beginDraw();
 		PG.setDrawCenter(pg);
-		for (int i = 0; i < shapes.size(); i++) {
-			shapes.get(i).update(pg);
+		if(useMultipleParticles) pg.blendMode(PBlendModes.SCREEN);
+		else pg.blendMode(PBlendModes.ADD);
+		for (int i = 0; i < particles.size(); i++) {
+			particles.get(i).update(pg);
 		}
 		PG.setDrawCorner(pg);
+		pg.blendMode(PBlendModes.BLEND);
 		pg.endDraw();
 		
-		p.debugView.setValue("shapes.size()", shapes.size());
+		p.debugView.setValue("shapes.size()", particles.size());
 		p.image(pg, 0, 0);
 	}
 	
-	protected void launchShape(float x, float y) {
+	protected void launchParticle(float x, float y) {
 		// look for an available shape
-		for (int i = 0; i < shapes.size(); i++) {
-			if(shapes.get(i).available()) {
-				shapes.get(i).launch(x, y);
+		for (int i = 0; i < particles.size(); i++) {
+			if(particles.get(i).available(pg)) {
+				launch(particles.get(i), x, y);
 				return;
 			}
 		}
 		// didn't find one
-		if(shapes.size() < 10000) {
-			ShapeParticle newShape = new ShapeParticle();
-			newShape.launch(x, y);
-			shapes.add(newShape);
+		if(particles.size() < 10000) {
+			Particle newShape = new Particle();
+			launch(newShape, x, y);
+			particles.add(newShape);
 		}
 	}
 	
-	
-	public class ShapeParticle {
-		
-		protected PVector pos = new PVector(0, -100, 0);
-		protected PVector speed = new PVector(0, 0, 0);
-		protected PVector gravity = new PVector(0, 0, 0);
-		protected float size = 10f;
-		protected float rotation = 0f;
-		protected LinearFloat sizeProgress = new LinearFloat(0, 0.04f);
-		protected int color;
-		protected PImage image;
-		
-		public ShapeParticle() {
-			
-		}
-		
-		public void update(PGraphics pg) {
-			if(available()) return;
-			
-			// update position
-			gravity.x *= 0.97f;
-			speed.add(gravity);
-			pos.add(speed);
-			rotation += gravity.z;
-			
-			// update size
-			sizeProgress.update();
-			float curSize = size * Penner.easeOutBack(sizeProgress.value(), 0, 1, 1);
-			if(sizeProgress.value() == 1) sizeProgress.setTarget(0);
-			
-			// draw image
-			pg.pushMatrix();
-			pg.translate(pos.x, pos.y);
-			pg.rotate(rotation);
-			pg.image(image, 0, 0, curSize * 2f, curSize * 2f);
-			pg.popMatrix();
-		}
-		
-		public void launch(float x, float y) {
-			// get random particle texture
-			if(particleImages != null) image = particleImages[MathUtil.randRange(0, particleImages.length - 1)];
-			
-			// random params
-			size = MathUtil.randRangeDecimal(10, 20);
-			sizeProgress.setCurrent(0);
-			sizeProgress.setTarget(1);
-			
-			// set motion properties
-			pos.set(x, y, 0);
-			speed.set(0, -0.5f, 0);
-			rotation = P.p.random(P.TWO_PI);
-			gravity.set(MathUtil.randRangeDecimal(-0.05f, 0.05f), MathUtil.randRangeDecimal(-0.02f, -0.09f), MathUtil.randRangeDecimal(-0.02f, 0.02f)); // z is rotation!
-		}
-		
-		public boolean available() {
-			return pos.y < -100 || pos.y > p.height + 100 || (sizeProgress.value() == 0 && sizeProgress.target() == 0);
-		}
+	protected void launch(Particle shape, float x, float y) {
+		shape
+			.setSpeed(-1f, 1f, -1f, 0.5f)
+			.setGravity(0, 0, 0f, -0.05f)
+			.setLifespan(10, 50)
+			.setRotation(-0.1f, 0.1f)
+			.setSizeMinMax(10, 40)
+			.setColor(p.color(p.random(200, 255), p.random(200, 255), p.random(200, 255)))
+			.launch(x, y, randomImg());
 	}
-
-
+	
+	protected PImage randomImg() {
+		return particleImages[MathUtil.randRange(0, particleImages.length - 1)];
+	}
 		
 }
