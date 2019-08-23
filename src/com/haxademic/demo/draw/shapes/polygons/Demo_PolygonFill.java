09db609f07bec10b922ba434849fef43e86cbda1
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-08-23 10:06:54 -0600

    Update latest haxademic.jar

---
+++ src/com/haxademic/demo/draw/shapes/polygons/Demo_PolygonFill.java
@@ -0,0 +1,25 @@
+package com.haxademic.demo.draw.shapes.polygons;
+
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.draw.shapes.polygons.Polygon;
+
+public class Demo_PolygonFill 
+extends PAppletHax {
+	public static void main(String args[]) { PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
+
+	protected Polygon poly;
+
+	public void setupFirstFrame() {
+		poly = Polygon.buildShape(p.width/2, p.height/2, 3, 200);
+	}
+
+	public void drawApp() {
+		p.background(0);
+
+		// draw toxiclibs mesh
+		p.fill(255);
+		poly.bgColor(0xff00ff00);
+		poly.draw(p.g, false);
+	}
+
+}
