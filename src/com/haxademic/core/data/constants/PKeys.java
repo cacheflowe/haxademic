e468c8fdd0243a9ccbbe9e0beec0717e570ffee8
Justin Gitlin <cacheflowe@users.noreply.github.com>
2019-12-05 12:09:41 -0700

    Create PKeys.java

---
+++ src/com/haxademic/core/data/constants/PKeys.java
@@ -0,0 +1,12 @@
+package com.haxademic.core.data.constants;
+
+import java.awt.event.KeyEvent;
+
+public class PKeys
+extends KeyEvent {
+	private static final long serialVersionUID = -7769910774718411912L;
+
+	public PKeys() {
+		super(null, 0, 0, 0, 0, 'c');	// dummy contructor - never init one of these
+	}
+}
