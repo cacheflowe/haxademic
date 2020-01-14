2dd4ab4072d188f92b61d5600db2138e7b97ab05
Justin Gitlin <cacheflowe@users.noreply.github.com>
2020-01-14 12:32:46 -0700

    Update ISocketClientDelegate.java

--- src/com/haxademic/core/net/ISocketClientDelegate.java
+++ src/com/haxademic/core/net/ISocketClientDelegate.java
@@ -1,5 +1,7 @@
 package com.haxademic.core.net;
 
 public interface ISocketClientDelegate {
+	public void socketConnected(String connection);
+	public void socketDisconnected(String connection);
 	public void messageReceived(String message);
 }
