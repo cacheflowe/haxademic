ac175ca0f8e0a1f0fa18d9e516b166fed7e47638
Justin Gitlin <cacheflowe@users.noreply.github.com>
2020-01-21 13:32:56 -0800

    Update Demo_CubeMap.java

--- src/com/haxademic/demo/draw/mapping/Demo_CubeMap.java
+++ src/com/haxademic/demo/draw/mapping/Demo_CubeMap.java
@@ -7,6 +7,7 @@ import com.haxademic.core.draw.context.PG;
 import com.haxademic.core.draw.image.ImageUtil;
 import com.haxademic.core.draw.textures.pshader.TextureShader;
 import com.haxademic.core.media.DemoAssets;
+import com.haxademic.core.ui.UI;
 
 import processing.core.PConstants;
 import processing.core.PGraphics;
@@ -18,10 +19,26 @@ extends PAppletHax {
 
 	protected PGraphics image;
 	protected TextureShader curShader;
+	
+	// split original image into 4 quads
+	protected float midX1 = 0.2f;
+	protected float midX2 = 1f - midX1;
+	protected int subdivisions = 10;
+	protected float xPad = 0;
+	protected String SUBDIVISIONS = "SUBDIVISIONS";
+	protected String CORNER_X_PAD = "CORNER_X_PAD";
+	protected String SCALE = "SCALE";
+
 
 	protected void firstFrame()	{
+		// image setup
 		image = PG.newPG(1000, 500);
 		curShader = new TextureShader(TextureShader.cacheflowe_down_void);
+		
+		// ui
+		UI.addSlider(SUBDIVISIONS, 10, 1, 20, 1, false);
+		UI.addSlider(CORNER_X_PAD, 0, 0, 0.5f, 0.001f, false);
+		UI.addSlider(SCALE, 1, 0.5f, 5f, 0.01f, false);
 	}
 
 	protected void drawApp() {
@@ -38,6 +55,9 @@ extends PAppletHax {
 		curShader.setTimeMult(0.003f);
 		curShader.updateTime();
 //		image.filter(curShader.shader());
+		
+		// draw UV mapping on texture itself
+		drawUvVisualization();
 
 		// prep context w/camera rotate
 		p.background(0);
@@ -47,46 +67,84 @@ extends PAppletHax {
 		p.translate(0, -100, -500);
 		PG.basicCameraFromMouse(p.g);
 		p.pushMatrix();
+		p.scale(UI.value(SCALE));
 		
-		// split original image into 4 quads
-		float midX1 = 0.2f;
-		float midX2 = 1f - midX1;
-
+		// mapping calcs
+		subdivisions = UI.valueInt(SUBDIVISIONS);
+		xPad = UI.value(CORNER_X_PAD);
+		
 		// draw back wall
-		textureQuadSubdivided(p.g, image, 10, 
-				-400, -200, 0,  0f, 0,
-				400, -200,  0,  1, 0,
+		textureQuadSubdivided(p.g, image, subdivisions, 
+				-400, -200, 0,  xPad, 0,
+				400, -200,  0,  1 - xPad, 0,
 				400, 200,   0,  midX2, 0.5f,
 				-400, 200,  0,  midX1, 0.5f);
 
 		// draw floor
-		textureQuadSubdivided(p.g, image, 10, 
+		textureQuadSubdivided(p.g, image, subdivisions, 
 				-400, 200, 0,     midX1, 0.5f,
 				400,  200, 0,     midX2, 0.5f,
-				400,  200, 400,   1f, 1f,
-				-400, 200, 400,   0f, 1f);
-
+				400,  200, 400,   1 - xPad, 1f,
+				-400, 200, 400,   xPad, 1f);
 		
 		// draw left wall
-		textureQuadSubdivided(p.g, image, 10, 
-				-400, -200, 400,  0.0f, 0.5f,
-				-400, -200, 0,    0f, 0f,
+		textureQuadSubdivided(p.g, image, subdivisions, 
+				-400, -200, 400,  0, 0.5f,
+				-400, -200, 0,    xPad, 0,
 				-400, 200,  0,    midX1, 0.5f,
-				-400, 200,  400,  0f, 1f);
-
+				-400, 200,  400,  xPad, 1);
 
 		// draw right wall
-		textureQuadSubdivided(p.g, image, 10, 
-				400, -200, 400,  1.0f, 0.5f,
-				400, -200, 0,    1f, 0f,
+		textureQuadSubdivided(p.g, image, subdivisions, 
+				400, -200, 400,  1, 0.5f,
+				400, -200, 0,    1 - xPad, 0,
 				400, 200,  0,    midX2, 0.5f,
-				400, 200,  400,  1f, 1f);
+				400, 200,  400,  1 - xPad, 1);
 
 		// end
 		p.popMatrix();
-
-		// debug
-		// p.image(image, 0, 0);
+	}
+	
+	protected void drawUvVisualization() {
+		// draw visualization
+		image.beginDraw();
+		image.stroke(255, 0, 0);
+		image.strokeWeight(10);
+		image.noFill();
+		
+		// back wall
+		image.beginShape();
+		image.vertex(image.width * (xPad), image.height * 0);
+		image.vertex(image.width * (1 - xPad), image.height * 0);
+		image.vertex(image.width * midX2, image.height * 0.5f);
+		image.vertex(image.width * midX1, image.height * 0.5f);
+		image.endShape(P.CLOSE);
+		
+		// floor
+		image.beginShape();
+		image.vertex(image.width * midX1, image.height * 0.5f);
+		image.vertex(image.width * midX2, image.height * 0.5f);
+		image.vertex(image.width * (1 - xPad), image.height * 1);
+		image.vertex(image.width * xPad, image.height * 1);
+		image.endShape(P.CLOSE);
+		
+		// left wall
+		image.beginShape();
+		image.vertex(image.width * 0, image.height * 0.5f);
+		image.vertex(image.width * xPad, image.height * 0);
+		image.vertex(image.width * midX1, image.height * 0.5f);
+		image.vertex(image.width * xPad, image.height * 1);
+		image.endShape(P.CLOSE);
+		
+		// right wall
+		image.beginShape();
+		image.vertex(image.width * 1, image.height * 0.5f);
+		image.vertex(image.width * (1 - xPad), image.height * 0);
+		image.vertex(image.width * midX2, image.height * 0.5f);
+		image.vertex(image.width * (1 - xPad), image.height * 1);
+		image.endShape(P.CLOSE);
+		
+		image.endDraw();
 	}
 
 	public static void textureQuadSubdivided(PGraphics pg, PImage texture, int subDivideSteps, 
@@ -171,8 +229,8 @@ extends PAppletHax {
 				pg.vertex(quadBotLeftX, quadBotLeftY, quadBotLeftZ, quadBotLeftU, quadBotLeftV);
 			}
 		}
-		pg.endShape();
-
+		
+		pg.endShape(P.CLOSE);
 	}
 	
 }
