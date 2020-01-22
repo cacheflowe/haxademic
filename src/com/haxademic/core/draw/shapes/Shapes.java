94e86711aa8c5e1958dc8a861ca6e560d0f78439
Justin Gitlin <cacheflowe@users.noreply.github.com>
2020-01-21 17:37:25 -0800

    Add Shapes.textureQuadSubdivided()

--- src/com/haxademic/core/draw/shapes/Shapes.java
+++ src/com/haxademic/core/draw/shapes/Shapes.java
@@ -379,6 +379,93 @@ public class Shapes {
 		pg.endShape();
 	}
 	
+	public static void textureQuadSubdivided(PGraphics pg, PImage texture, int subDivideSteps, 
+			float x1, float y1, float z1, float u1, float v1,
+			float x2, float y2, float z2, float u2, float v2,
+			float x3, float y3, float z3, float u3, float v3,
+			float x4, float y4, float z4, float u4, float v4
+		) {
+		
+		pg.beginShape(P.QUAD);
+		pg.textureMode(PConstants.NORMAL);
+		pg.texture(texture);
+
+		// subdivide quad for better resolution
+		// vertices go top-left, clockwise to bottom left
+		float stepsX = subDivideSteps;
+		float segmentX = 1f / subDivideSteps;
+		float stepsY = subDivideSteps;
+		float segmentY = 1f / stepsY;
+
+		// draw a subdivided grid
+		for( float x=0; x < stepsX; x++ ) {
+			// calculate spread of mesh grid and uv coordinates
+			float xNorm = x * segmentX;
+			float xNormNext = (x+1) * segmentX;
+
+			for( float y=0; y < stepsY; y++ ) {
+				// calculate grid cells' uv coordinates
+				float yNorm = y * segmentY;
+				float yNormNext = (y+1) * segmentY;
+
+				// calc grid positions based on interpolating columns between corners
+				// we only need the xProgress for this, since we're slicing by column
+				float colTopX = P.lerp(x1, x2, xNorm);
+				float colTopY = P.lerp(y1, y2, xNorm);
+				float colTopZ = P.lerp(z1, z2, xNorm);
+				float colTopU = P.lerp(u1, u2, xNorm);
+				float colTopV = P.lerp(v1, v2, xNorm);
+				float colBotX = P.lerp(x4, x3, xNorm);
+				float colBotY = P.lerp(y4, y3, xNorm);
+				float colBotZ = P.lerp(z4, z3, xNorm);
+				float colBotU = P.lerp(u4, u3, xNorm);
+				float colBotV = P.lerp(v4, v3, xNorm);
+
+				float nextColTopX = P.lerp(x1, x2, xNormNext);
+				float nextColTopY = P.lerp(y1, y2, xNormNext);
+				float nextColTopZ = P.lerp(z1, z2, xNormNext);
+				float nextColTopU = P.lerp(u1, u2, xNormNext);
+				float nextColTopV = P.lerp(v1, v2, xNormNext);
+				float nextColBotX = P.lerp(x4, x3, xNormNext);
+				float nextColBotY = P.lerp(y4, y3, xNormNext);
+				float nextColBotZ = P.lerp(z4, z3, xNormNext);
+				float nextColBotU = P.lerp(u4, u3, xNormNext);
+				float nextColBotV = P.lerp(v4, v3, xNormNext);
+
+				// calc quad coords
+				float quadTopLeftX = P.lerp(colTopX, colBotX, yNorm);
+				float quadTopLeftY = P.lerp(colTopY, colBotY, yNorm);
+				float quadTopLeftZ = P.lerp(colTopZ, colBotZ, yNorm);
+				float quadTopLeftU = P.lerp(colTopU, colBotU, yNorm);
+				float quadTopLeftV = P.lerp(colTopV, colBotV, yNorm);
+				float quadTopRightX = P.lerp(nextColTopX, nextColBotX, yNorm);
+				float quadTopRightY = P.lerp(nextColTopY, nextColBotY, yNorm);
+				float quadTopRightZ = P.lerp(nextColTopZ, nextColBotZ, yNorm);
+				float quadTopRightU = P.lerp(nextColTopU, nextColBotU, yNorm);
+				float quadTopRightV = P.lerp(nextColTopV, nextColBotV, yNorm);
+				float quadBotRightX = P.lerp(nextColTopX, nextColBotX, yNormNext);
+				float quadBotRightY = P.lerp(nextColTopY, nextColBotY, yNormNext);
+				float quadBotRightZ = P.lerp(nextColTopZ, nextColBotZ, yNormNext);
+				float quadBotRightU = P.lerp(nextColTopU, nextColBotU, yNormNext);
+				float quadBotRightV = P.lerp(nextColTopV, nextColBotV, yNormNext);
+				float quadBotLeftX = P.lerp(colTopX, colBotX, yNormNext);
+				float quadBotLeftY = P.lerp(colTopY, colBotY, yNormNext);
+				float quadBotLeftZ = P.lerp(colTopZ, colBotZ, yNormNext);
+				float quadBotLeftU = P.lerp(colTopU, colBotU, yNormNext);
+				float quadBotLeftV = P.lerp(colTopV, colBotV, yNormNext);
+
+				// draw subdivided quads
+				pg.vertex(quadTopLeftX, quadTopLeftY, quadTopLeftZ, quadTopLeftU, quadTopLeftV);
+				pg.vertex(quadTopRightX, quadTopRightY, quadTopRightZ, quadTopRightU, quadTopRightV);
+				pg.vertex(quadBotRightX, quadBotRightY, quadBotRightZ, quadBotRightU, quadBotRightV);
+				pg.vertex(quadBotLeftX, quadBotLeftY, quadBotLeftZ, quadBotLeftU, quadBotLeftV);
+			}
+		}
+		
+		pg.endShape(P.CLOSE);
+	}
+
+	
 	public static void drawTexturedLine(PGraphics pg, PImage texture, float xStart, float yStart, float xEnd, float yEnd, int color, float thickness, float texOffset) {
 		drawTexturedLine(pg, texture, xStart, yStart, xEnd, yEnd, color, thickness, texOffset, 0);
 	}
--- src/com/haxademic/demo/draw/mapping/Demo_CubeMap.java
+++ src/com/haxademic/demo/draw/mapping/Demo_CubeMap.java
@@ -5,13 +5,12 @@ import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.debug.DebugView;
 import com.haxademic.core.draw.context.PG;
 import com.haxademic.core.draw.image.ImageUtil;
+import com.haxademic.core.draw.shapes.Shapes;
 import com.haxademic.core.draw.textures.pshader.TextureShader;
 import com.haxademic.core.media.DemoAssets;
 import com.haxademic.core.ui.UI;
 
-import processing.core.PConstants;
 import processing.core.PGraphics;
-import processing.core.PImage;
 
 public class Demo_CubeMap
 extends PAppletHax {
@@ -85,28 +84,28 @@ extends PAppletHax {
 		midX2 = 1f - midX1;
 		
 		// draw back wall
-		textureQuadSubdivided(p.g, image, subdivisions, 
+		Shapes.textureQuadSubdivided(p.g, image, subdivisions, 
 				-400, -200, 0,  xPad, 0,
 				400, -200,  0,  1 - xPad, 0,
 				400, 200,   0,  midX2, 0.5f,
 				-400, 200,  0,  midX1, 0.5f);
 
 		// draw floor
-		textureQuadSubdivided(p.g, image, subdivisions, 
+		Shapes.textureQuadSubdivided(p.g, image, subdivisions, 
 				-400, 200, 0,     midX1, 0.5f,
 				400,  200, 0,     midX2, 0.5f,
 				400,  200, 400,   1 - xPad, 1f,
 				-400, 200, 400,   xPad, 1f);
 		
 		// draw left wall
-		textureQuadSubdivided(p.g, image, subdivisions, 
+		Shapes.textureQuadSubdivided(p.g, image, subdivisions, 
 				-400, -200, 400,  0, 0.5f,
 				-400, -200, 0,    xPad, 0,
 				-400, 200,  0,    midX1, 0.5f,
 				-400, 200,  400,  xPad, 1);
 
 		// draw right wall
-		textureQuadSubdivided(p.g, image, subdivisions, 
+		Shapes.textureQuadSubdivided(p.g, image, subdivisions, 
 				400, -200, 400,  1, 0.5f,
 				400, -200, 0,    1 - xPad, 0,
 				400, 200,  0,    midX2, 0.5f,
@@ -158,90 +157,4 @@ extends PAppletHax {
 		image.endDraw();
 	}
 
-	public static void textureQuadSubdivided(PGraphics pg, PImage texture, int subDivideSteps, 
-			float x1, float y1, float z1, float u1, float v1,
-			float x2, float y2, float z2, float u2, float v2,
-			float x3, float y3, float z3, float u3, float v3,
-			float x4, float y4, float z4, float u4, float v4
-		) {
-		
-		pg.beginShape(P.QUAD);
-		pg.textureMode(PConstants.NORMAL);
-		pg.texture(texture);
-
-		// subdivide quad for better resolution
-		// vertices go top-left, clockwise to bottom left
-		float stepsX = subDivideSteps;
-		float segmentX = 1f / stepsX;
-		float stepsY = subDivideSteps;
-		float segmentY = 1f / stepsY;
-
-		// draw a subdivided grid
-		for( float x=0; x < stepsX; x++ ) {
-			// calculate spread of mesh grid and uv coordinates
-			float xNorm = x * segmentX;
-			float xNormNext = (x+1) * segmentX;
-
-			for( float y=0; y < stepsY; y++ ) {
-				// calculate grid cells' uv coordinates
-				float yNorm = y * segmentY;
-				float yNormNext = (y+1) * segmentY;
-
-				// calc grid positions based on interpolating columns between corners
-				// we only need the xProgress for this, since we're slicing by column
-				float colTopX = P.lerp(x1, x2, xNorm);
-				float colTopY = P.lerp(y1, y2, xNorm);
-				float colTopZ = P.lerp(z1, z2, xNorm);
-				float colTopU = P.lerp(u1, u2, xNorm);
-				float colTopV = P.lerp(v1, v2, xNorm);
-				float colBotX = P.lerp(x4, x3, xNorm);
-				float colBotY = P.lerp(y4, y3, xNorm);
-				float colBotZ = P.lerp(z4, z3, xNorm);
-				float colBotU = P.lerp(u4, u3, xNorm);
-				float colBotV = P.lerp(v4, v3, xNorm);
-
-				float nextColTopX = P.lerp(x1, x2, xNormNext);
-				float nextColTopY = P.lerp(y1, y2, xNormNext);
-				float nextColTopZ = P.lerp(z1, z2, xNormNext);
-				float nextColTopU = P.lerp(u1, u2, xNormNext);
-				float nextColTopV = P.lerp(v1, v2, xNormNext);
-				float nextColBotX = P.lerp(x4, x3, xNormNext);
-				float nextColBotY = P.lerp(y4, y3, xNormNext);
-				float nextColBotZ = P.lerp(z4, z3, xNormNext);
-				float nextColBotU = P.lerp(u4, u3, xNormNext);
-				float nextColBotV = P.lerp(v4, v3, xNormNext);
-
-				// calc quad coords
-				float quadTopLeftX = P.lerp(colTopX, colBotX, yNorm);
-				float quadTopLeftY = P.lerp(colTopY, colBotY, yNorm);
-				float quadTopLeftZ = P.lerp(colTopZ, colBotZ, yNorm);
-				float quadTopLeftU = P.lerp(colTopU, colBotU, yNorm);
-				float quadTopLeftV = P.lerp(colTopV, colBotV, yNorm);
-				float quadTopRightX = P.lerp(nextColTopX, nextColBotX, yNorm);
-				float quadTopRightY = P.lerp(nextColTopY, nextColBotY, yNorm);
-				float quadTopRightZ = P.lerp(nextColTopZ, nextColBotZ, yNorm);
-				float quadTopRightU = P.lerp(nextColTopU, nextColBotU, yNorm);
-				float quadTopRightV = P.lerp(nextColTopV, nextColBotV, yNorm);
-				float quadBotRightX = P.lerp(nextColTopX, nextColBotX, yNormNext);
-				float quadBotRightY = P.lerp(nextColTopY, nextColBotY, yNormNext);
-				float quadBotRightZ = P.lerp(nextColTopZ, nextColBotZ, yNormNext);
-				float quadBotRightU = P.lerp(nextColTopU, nextColBotU, yNormNext);
-				float quadBotRightV = P.lerp(nextColTopV, nextColBotV, yNormNext);
-				float quadBotLeftX = P.lerp(colTopX, colBotX, yNormNext);
-				float quadBotLeftY = P.lerp(colTopY, colBotY, yNormNext);
-				float quadBotLeftZ = P.lerp(colTopZ, colBotZ, yNormNext);
-				float quadBotLeftU = P.lerp(colTopU, colBotU, yNormNext);
-				float quadBotLeftV = P.lerp(colTopV, colBotV, yNormNext);
-
-				// draw subdivided quads
-				pg.vertex(quadTopLeftX, quadTopLeftY, quadTopLeftZ, quadTopLeftU, quadTopLeftV);
-				pg.vertex(quadTopRightX, quadTopRightY, quadTopRightZ, quadTopRightU, quadTopRightV);
-				pg.vertex(quadBotRightX, quadBotRightY, quadBotRightZ, quadBotRightU, quadBotRightV);
-				pg.vertex(quadBotLeftX, quadBotLeftY, quadBotLeftZ, quadBotLeftU, quadBotLeftV);
-			}
-		}
-		
-		pg.endShape(P.CLOSE);
-	}
-	
 }
