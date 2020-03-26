42d973b821c90c89a61a4cf7b67c025242ec04d7
Justin Gitlin <cacheflowe@users.noreply.github.com>
2020-03-26 09:14:14 -0600

    Add ArrayUtil.crossfadeEnds()

--- src/com/haxademic/core/data/ArrayUtil.java
+++ src/com/haxademic/core/data/ArrayUtil.java
@@ -1,6 +1,8 @@
 package com.haxademic.core.data;
 
+import com.haxademic.core.app.P;
 import com.haxademic.core.math.MathUtil;
+import com.haxademic.core.math.easing.Penner;
 
 public class ArrayUtil {
 
@@ -18,5 +20,23 @@ public class ArrayUtil {
 		return false;
 	}
 	
+	public static void crossfadeEnds(float[] array, float fadeSize /* 0.1 */) {
+	    // number of elements to fade on either end
+	    int numToFade = Math.round(array.length * fadeSize);
+	    // average of start/end values
+	    float endAvg = (array[array.length - 1] + array[0]) / 2f;
+	    for(int i = 0; i <= numToFade; i++) {
+	    	// lerp strength increases
+	    	float lerpStrength = (float) i / numToFade;
+	    	// indices go from inland towards the edges, increasingly fading towards the ends average
+	    	int endIndex = array.length - numToFade - 1 + i;
+	    	float linearValEnd = P.lerp(endAvg, array[array.length - numToFade - 1], Penner.easeInOutSine(1-lerpStrength));
+	    	array[endIndex] = P.lerp(array[endIndex], linearValEnd, Penner.easeInOutSine(lerpStrength));
+	    	int startIndex = numToFade - i;
+	    	float linearVal = P.lerp(endAvg, array[numToFade], Penner.easeInOutSine(lerpStrength));
+	    	array[i] = P.lerp(array[i], linearVal, Penner.easeInOutSine(1-lerpStrength));
+//	    	P.out(lerpStrength);
+	    }
+	}
 
 }
