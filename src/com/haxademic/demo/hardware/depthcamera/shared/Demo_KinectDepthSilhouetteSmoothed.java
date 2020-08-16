4b3f4fad07404502e7c5ad509b2323779fa632c6
Justin Gitlin <cacheflowe@users.noreply.github.com>
2020-08-16 17:20:41 -0600

    Update Demo_KinectDepthSilhouetteSmoothed.java

--- src/com/haxademic/demo/hardware/depthcamera/shared/Demo_KinectDepthSilhouetteSmoothed.java
+++ src/com/haxademic/demo/hardware/depthcamera/shared/Demo_KinectDepthSilhouetteSmoothed.java
@@ -19,6 +19,11 @@ extends PAppletHax {
 	protected KinectDepthSilhouetteSmoothed kinectSilhouetteSmoothed;
 	protected String KINECT_NEAR = "KINECT_NEAR";
 	protected String KINECT_FAR = "KINECT_FAR";
+	protected String SILHOUETTE_FRAME_BLEND = "SILHOUETTE_FRAME_BLEND";
+	protected String SILHOUETTE_SMOOTH = "SILHOUETTE_SMOOTH";
+	protected String SILHOUETTE_THRESHOLD_PRE_BRIGHTNESS = "SILHOUETTE_THRESHOLD_PRE_BRIGHTNESS";
+	protected String SILHOUETTE_THRESHOLD_CUTOFF = "SILHOUETTE_THRESHOLD_CUTOFF";
+	protected String SILHOUETTE_POST_BLUR = "SILHOUETTE_POST_BLUR";
 
 	protected void config() {
 		Config.setProperty( AppSettings.RENDERING_MOVIE, false );
@@ -29,7 +34,7 @@ extends PAppletHax {
 
 	protected void firstFrame() {
 		// init depth cam
-		DepthCamera.instance(DepthCameraType.KinectV1);
+		DepthCamera.instance(DepthCameraType.Realsense);
 		IDepthCamera depthCamera = DepthCamera.instance().camera;
 		kinectSilhouetteSmoothed = new KinectDepthSilhouetteSmoothed(depthCamera, 5);
 		
@@ -42,6 +47,11 @@ extends PAppletHax {
 		UI.addTitle("Depth Camera Config");
 		UI.addSlider(KINECT_NEAR, 300, 300, 3000, 10, false);
 		UI.addSlider(KINECT_FAR, 1500, 500, 6000, 10, false);
+		UI.addSlider(SILHOUETTE_FRAME_BLEND, 0.25f, 0, 1, 0.01f, false);
+		UI.addSlider(SILHOUETTE_SMOOTH, 0.25f, 0, 2, 0.01f, false);
+		UI.addSlider(SILHOUETTE_THRESHOLD_PRE_BRIGHTNESS, 1.25f, 0, 3, 0.01f, false);
+		UI.addSlider(SILHOUETTE_THRESHOLD_CUTOFF, 0.4f, 0, 1, 0.01f, false);
+		UI.addSlider(SILHOUETTE_POST_BLUR, 0, 0, 4, 0.01f, false);
 	}
 	
 	protected void drawApp() {
@@ -52,6 +62,11 @@ extends PAppletHax {
 		KinectDepthSilhouetteSmoothed.KINECT_FAR = UI.valueInt(KINECT_FAR);
 
 		// do depth processing & draw to screen
+		kinectSilhouetteSmoothed.setFrameBlend(UI.value(SILHOUETTE_FRAME_BLEND));
+		kinectSilhouetteSmoothed.setSmoothing(UI.value(SILHOUETTE_SMOOTH));
+		kinectSilhouetteSmoothed.setThresholdPreBrightness(UI.value(SILHOUETTE_THRESHOLD_PRE_BRIGHTNESS));
+		kinectSilhouetteSmoothed.setThresholdCutoff(UI.value(SILHOUETTE_THRESHOLD_CUTOFF));
+		kinectSilhouetteSmoothed.setPostBlur(UI.value(SILHOUETTE_POST_BLUR));
 		kinectSilhouetteSmoothed.update();
 		ImageUtil.cropFillCopyImage(kinectSilhouetteSmoothed.image(), p.g, false);
 	}
