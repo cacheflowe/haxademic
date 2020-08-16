68c8b076b9fe29b526b4f9d5aa86e1facef9b23a
Justin Gitlin <cacheflowe@users.noreply.github.com>
2020-08-16 17:19:19 -0600

    Update threshold.glsl

--- data/haxademic/shaders/filters/threshold.glsl
+++ data/haxademic/shaders/filters/threshold.glsl
@@ -13,7 +13,7 @@ varying vec4 vertColor;
 varying vec4 vertTexCoord;
 
 void main() {
-  // better version from gene kogan
+  // better version from gene kogan: https://github.com/genekogan/Processing-Shader-Examples/blob/master/TextureShaders/data/threshold.glsl
   vec3 col = texture2D(texture, vertTexCoord.st).rgb;
   float bright = 0.33333 * (col.r + col.g + col.b);
   float b = mix(0.0, 1.0, step(cutoff, bright));
