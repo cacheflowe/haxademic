85927eb47cd1e8d0c07032ac5c78b4a3f8446e32
Justin Gitlin <cacheflowe@users.noreply.github.com>
2020-10-28 17:16:05 -0400

    Fix ParticleSystemSwirl UI yb calling up to super() for prefixed UI keys

--- src/com/haxademic/core/draw/particle/ParticleSystemSwirl.java
+++ src/com/haxademic/core/draw/particle/ParticleSystemSwirl.java
@@ -16,23 +16,8 @@ extends ParticleSystem {
 	
 	public void enableUI(String prefix, boolean saves) {
 		usingUI = true;
-		UI.addSlider(prefix + MAX_ATTEMPTS, 2000, 10, 5000, 5, saves);
-		UI.addSlider(prefix + MAX_LAUNCHES, 8, 1, 100, 1, saves);
-		UI.addSlider(prefix + POOL_MAX_SIZE, 10000, 10, 20000, 1, saves);
-		UI.addSlider(prefix + SPEED_X_MIN, 0, -5f, 5, 0.01f, saves);
-		UI.addSlider(prefix + SPEED_X_MAX, 0.66f, -5, 5, 0.01f, saves);
-		UI.addSlider(prefix + SPEED_Y_MIN, 0f, -5f, 5, 0.01f, saves);
-		UI.addSlider(prefix + SPEED_Y_MAX, 0.44f, -5, 5, 0.01f, saves);
-		UI.addSlider(prefix + GRAVITY_X_MIN, 0, -0.5f, 0, 0.001f, saves);
-		UI.addSlider(prefix + GRAVITY_X_MAX, 0.002f, 0, 0.5f, 0.001f, saves);
-		UI.addSlider(prefix + GRAVITY_Y_MIN, -0.33f, -0.5f, 0, 0.001f, saves);
-		UI.addSlider(prefix + GRAVITY_Y_MAX, 0.12f, 0, 0.5f, 0.001f, saves);
-		UI.addSlider(prefix + LIFESPAN_MIN, 10, 10, 50, 1, saves);
-		UI.addSlider(prefix + LIFESPAN_MAX, 50, 10, 200, 1, saves);
-		UI.addSlider(prefix + ROTATION_MIN, 0, -1, 0, 0.001f, saves);
-		UI.addSlider(prefix + ROTATION_MAX, 0.281f, 0, 1f, 0.001f, saves);
-		UI.addSlider(prefix + SIZE_MIN, 10, 1, 40, 0.1f, saves);
-		UI.addSlider(prefix + SIZE_MAX, 90, 10, 200, 0.1f, saves);
+		super.enableUI(prefix, saves);
+		P.out("ENABLEUI YO", MAX_ATTEMPTS);
 	}
 
 	public void launchParticles(PGraphics pg) {
