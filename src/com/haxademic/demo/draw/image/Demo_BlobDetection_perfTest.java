c0bb76697c9b172a064dca1bb65492c404c7a0db
Justin Gitlin <cacheflowe@users.noreply.github.com>
2020-12-14 23:05:32 -0500

    Update Demo_BlobDetection_perfTest.java

--- src/com/haxademic/demo/draw/image/Demo_BlobDetection_perfTest.java
+++ src/com/haxademic/demo/draw/image/Demo_BlobDetection_perfTest.java
@@ -4,10 +4,9 @@ import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.app.config.AppSettings;
 import com.haxademic.core.app.config.Config;
-import com.haxademic.core.draw.filters.pgraphics.archive.FastBlurFilter;
 import com.haxademic.core.draw.filters.pshader.BlurHFilter;
-import com.haxademic.core.draw.filters.pshader.BlurProcessingFilter;
 import com.haxademic.core.draw.filters.pshader.BlurVFilter;
+import com.haxademic.core.draw.image.ImageUtil;
 import com.haxademic.core.media.DemoAssets;
 
 import blobDetection.Blob;
@@ -21,107 +20,71 @@ public class Demo_BlobDetection_perfTest
 extends PAppletHax {
 	public static void main(String args[]) { arguments = args; PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 	
-	Movie _movie;
-	
+	Movie video;
 	BlobDetection theBlobDetection;
-	PImage blobBufferImg;
 	PGraphics blobBufferGraphics;
-	boolean _usingPimg = true;
-
-	protected int _canvasW;
-	protected int _canvasH;
-	public PGraphics _canvas;
-
 
 	protected void config() {
-		Config.setProperty( AppSettings.WIDTH, "640" );
-		Config.setProperty( AppSettings.HEIGHT, "640" );
+		Config.setProperty( AppSettings.WIDTH, 640);
+		Config.setProperty( AppSettings.HEIGHT, 640);
 	}
 
-
 	protected void firstFrame() {
-	
-
-		_canvasW = p.width;
-		_canvasH = p.height;
-		_canvas = P.p.createGraphics(_canvasW, _canvasH, P.P2D);
-		
 		initMovie();
 		initBlobDetection();
 	}
 
 	protected void drawApp() {
 		background(0);
-		
-//		drawVideo();
-		runBlobDetection( _movie );
+		runBlobDetection(video);
 		drawEdges();
 
-		p.image(_canvas, 0, 0);
-//		p.image(_movie, 0, 0);
-		
-		if(_usingPimg == true) {
-			p.image(blobBufferImg, 0, 0);
-		} else {
-			p.image(blobBufferGraphics, 0, 0);
-		}
+		p.image(pg, 0, 0);		
+		p.image(blobBufferGraphics, 0, 0);
 	}
 	
 	protected void initMovie() {
-		_movie = DemoAssets.movieFractalCube();
-		_movie.play();
-		_movie.loop();
-		_movie.jump(0);
+		video = DemoAssets.movieFractalCube();
+		video.play();
+		video.loop();
+		video.jump(0);
 	}
 	
 	protected void initBlobDetection() {
+		// build scaled-down canvas
 		float scaleDownForBlobDetect = 0.2f;
-		int blurImgW = (int)(_canvasW * scaleDownForBlobDetect);
-		int blurImgH = (int)(_canvasH * scaleDownForBlobDetect);
-		
-		blobBufferImg = new PImage(blurImgW, blurImgH); 
+		int blurImgW = (int)(p.width * scaleDownForBlobDetect);
+		int blurImgH = (int)(p.width * scaleDownForBlobDetect);
 		blobBufferGraphics = P.p.createGraphics(blurImgW, blurImgH, P.P2D);
 		
-		float shaderBlurAmount = 1f;
-		BlurHFilter.instance(P.p).setBlurByPercent(shaderBlurAmount, blobBufferGraphics.width);
-		BlurVFilter.instance(P.p).setBlurByPercent(shaderBlurAmount, blobBufferGraphics.width);
-		BlurProcessingFilter.instance(P.p).setSigma(10f);
-		BlurProcessingFilter.instance(P.p).setBlurSize(10);
-
-		theBlobDetection = new BlobDetection( blobBufferImg.width, blobBufferImg.height );
+		// init blob obj
+		theBlobDetection = new BlobDetection(blobBufferGraphics.width, blobBufferGraphics.height);
 		theBlobDetection.setPosDiscrimination(true);	// true if looking for bright areas
-		theBlobDetection.setThreshold(0.5f); // will detect bright areas whose luminosity > threshold
+		theBlobDetection.setThreshold(0.5f); 			// will detect bright areas whose luminosity > threshold
 	}
 
 	protected void runBlobDetection( PImage source ) {
-		if(_usingPimg == true) {
-			blobBufferImg.copy(source, 0, 0, source.width, source.height, 0, 0, blobBufferImg.width, blobBufferImg.height);
-			FastBlurFilter.blur(blobBufferImg, 3);
-			theBlobDetection.computeBlobs(blobBufferImg.pixels);
-		} else {
-//			blobBufferGraphics.beginDraw();
-			blobBufferGraphics.copy(source, 0, 0, source.width, source.height, 0, 0, blobBufferGraphics.width, blobBufferGraphics.height);
-//			blobBufferGraphics.endDraw();
-//			blobBufferImg.copy(source, 0, 0, source.width, source.height, 0, 0, blobBufferImg.width, blobBufferImg.height);
-
-//			BlurProcessingFilter.instance(P.p).applyTo(blobBufferGraphics);
+		// copy & blur source image
+		ImageUtil.copyImage(source, blobBufferGraphics);
+		float shaderBlurAmount = 1f;
+		BlurHFilter.instance(P.p).setBlurByPercent(shaderBlurAmount, blobBufferGraphics.width);
+		BlurVFilter.instance(P.p).setBlurByPercent(shaderBlurAmount, blobBufferGraphics.width);
+		for (int i = 0; i < 2; i++) {
 			BlurHFilter.instance(P.p).applyTo(blobBufferGraphics);
 			BlurVFilter.instance(P.p).applyTo(blobBufferGraphics);
-//			blobBufferGraphics.filter(_blurH);
-//			blobBufferGraphics.filter(_blurV);
-//			theBlobDetection.computeBlobs(blobBufferImg.pixels);
-			theBlobDetection.computeBlobs(blobBufferGraphics.get().pixels);
 		}
+		
+		// send pixels data to algorithm
+		blobBufferGraphics.loadPixels();
+		theBlobDetection.computeBlobs(blobBufferGraphics.pixels);
 	}
 
 	protected void drawEdges() {
-		
-		_canvas.beginDraw();
-		_canvas.clear();
-		_canvas.background(0);
-
-		_canvas.stroke(255);
-		_canvas.fill(255);
+		// set blob output context
+		pg.beginDraw();
+		pg.background(0);
+		pg.stroke(255);
+		pg.fill(127);
 
 		// do edge detection
 		Blob b;
@@ -129,34 +92,31 @@ extends PAppletHax {
 		for (int n=0 ; n < theBlobDetection.getBlobNb() ; n++) {
 			b = theBlobDetection.getBlob(n);
 			if ( b != null ) {
-				_canvas.beginShape();
+				pg.beginShape();
 				for (int m = 0; m < b.getEdgeNb(); m++) {
 					eA = b.getEdgeVertexA(m);
 					eB = b.getEdgeVertexB(m);	// should store these too?
 					if (eA !=null && eB !=null) {
-						_canvas.vertex( eA.x * _canvasW, eA.y * _canvasH );
-						_canvas.vertex( eB.x * _canvasW, eB.y * _canvasH );
-//						_canvas.line(eA.x * _canvasW, eA.y * _canvasH, eB.x * _canvasW, eB.y * _canvasH);
+						pg.vertex( eA.x * pg.width, eA.y * pg.height );
+						pg.vertex( eB.x * pg.width, eB.y * pg.height );
+//						_canvas.line(eA.x * _canvasW, eA.y * pg.height, eB.x * _canvasW, eB.y * pg.height);
 					}
 				}
 				// connect last vertex to first
 				eA = b.getEdgeVertexA(0);
 				eB = b.getEdgeVertexB(0);
-				_canvas.vertex( eA.x * _canvasW, eA.y * _canvasH );
-				_canvas.vertex( eB.x * _canvasW, eB.y * _canvasH );
+				pg.vertex( eA.x * pg.width, eA.y * pg.height );
+				pg.vertex( eB.x * pg.width, eB.y * pg.height );
 
 				// complete shape
-				_canvas.endShape();
+				pg.endShape();
 			}
 		}
-		_canvas.endDraw();
+		pg.endDraw();
 	}
 	
 	public void keyPressed() {
 		super.keyPressed();
-		if( p.key == 'd' ){
-			_usingPimg = !_usingPimg;
-		}
 	}
 
 }
