98f81794aeed66b786a6d37e2875144292da665d
Justin Gitlin <cacheflowe@users.noreply.github.com>
2021-01-25 17:29:16 -0500

    Update Chromium launcher script with window size arguments

--- scripts/chrome-app.cmd
+++ scripts/chrome-app.cmd
@@ -1,2 +1,4 @@
 echo Starting the browser app
-"%SYSTEMDRIVE%%HOMEPATH%\AppData\Local\Chromium\Application\chrome.exe" --app=%1 --chrome-frame --incognito --disable-pinch --overscroll-history-navigation=0 --disable-session-crashed-bubble --disable-infobars --allow-file-access-from-files --allow-running-insecure-content --use-fake-ui-for-media-stream --autoplay-policy=no-user-gesture-required --unsafely-treat-insecure-origin-as-secure="http://localhost" --window-size=800,600
+echo "URL: %1"
+echo "Width: %2, %3"
+"%SYSTEMDRIVE%%HOMEPATH%\AppData\Local\Chromium\Application\chrome.exe" --app=%1 --chrome-frame --incognito --disable-pinch --overscroll-history-navigation=0 --disable-session-crashed-bubble --disable-infobars --allow-file-access-from-files --allow-running-insecure-content --use-fake-ui-for-media-stream --autoplay-policy=no-user-gesture-required --unsafely-treat-insecure-origin-as-secure="http://localhost" --window-position=50,50 --window-size=%2,%3
