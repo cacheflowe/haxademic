60b143dff293e4d1e4b491c59c5062d238197640
cacheflowe <cacheflowe@cacheflowe.com>
2021-03-27 23:08:13 -0400

    Update Demo_PShapeUtil_shapeFromImage.java

--- src/com/haxademic/demo/draw/shapes/Demo_PShapeUtil_shapeFromImage.java
+++ src/com/haxademic/demo/draw/shapes/Demo_PShapeUtil_shapeFromImage.java
@@ -1,11 +1,11 @@
 package com.haxademic.demo.draw.shapes;
 
+import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.app.config.AppSettings;
 import com.haxademic.core.app.config.Config;
 import com.haxademic.core.draw.context.PG;
 import com.haxademic.core.draw.shapes.PShapeUtil;
-import com.haxademic.core.media.DemoAssets;
 
 import processing.core.PShape;
 
@@ -14,6 +14,7 @@ extends PAppletHax {
 	public static void main(String args[]) { arguments = args; PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 
 	protected PShape shape;
+	protected PShape shape2;
 	
 	protected void config() {
 		Config.setProperty( AppSettings.WIDTH, 1200 );
@@ -21,10 +22,20 @@ extends PAppletHax {
 	}
 	
 	protected void firstFrame() {
-		shape = PShapeUtil.shapeFromImage(DemoAssets.textureCursor());
-		PShapeUtil.centerShape(shape);
-		PShapeUtil.scaleVertices(shape, 1, 1, 4);
-		PShapeUtil.scaleShapeToExtent(shape, p.height * 0.2f);
+//		shape = PShapeUtil.shapeFromImage(DemoAssets.textureCursor());
+//		shape = PShapeUtil.shapeFromImage(P.getImage("images/_sketch/pixel-objects/power-glove.png"));
+//		shape = PShapeUtil.shapeFromImage(P.getImage("images/_sketch/pixel-objects/hand-peace.png"));
+//		shape = PShapeUtil.shapeFromImage(P.getImage("images/_sketch/pixel-objects/mario-mushroom.png"));
+//		shape = PShapeUtil.shapeFromImage(P.getImage("images/_sketch/pixel-objects/mario-cloud-smile.png"));
+		shape = PShapeUtil.shapeFromImage(P.getImage("images/_sketch/pixel-objects/skull-mini.png"));
+		shape2 = PShapeUtil.shapeFromImage(P.getImage("images/_sketch/pixel-objects/sunglasses-deal-with-it.png"));
+		
+		PShapeUtil.scaleVertices(shape, 40, 40, 80);
+//		PShapeUtil.scaleShapeToWidth(shape, p.height * 0.2f);
+//		PShapeUtil.scaleShapeToWidth(shape, p.height * 0.01f * 40);
+
+		PShapeUtil.scaleVertices(shape2, 40, 40, 80);
+//		PShapeUtil.scaleShapeToWidth(shape2, p.height * 0.01f * 40);
 	}
 	
 	protected void drawApp() {
@@ -36,5 +47,9 @@ extends PAppletHax {
 
 		// draw
 		p.shape(shape);
+		
+		p.translate(0, 0, 80);
+		p.shape(shape2);
+		
 	}
 }
