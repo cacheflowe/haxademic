02a13c5ed31762a6a031de71bde4198756548e9f
cacheflowe <cacheflowe@cacheflowe.com>
2021-04-27 12:06:44 -0400

    Update Demo_AllFilters.java

--- src/com/haxademic/demo/draw/filters/shaders/Demo_AllFilters.java
+++ src/com/haxademic/demo/draw/filters/shaders/Demo_AllFilters.java
@@ -7,7 +7,6 @@ import com.haxademic.core.app.config.Config;
 import com.haxademic.core.data.constants.PRenderers;
 import com.haxademic.core.debug.DebugView;
 import com.haxademic.core.draw.color.ImageGradient;
-import com.haxademic.core.draw.context.PG;
 import com.haxademic.core.draw.filters.pshader.AlphaStepFilter;
 import com.haxademic.core.draw.filters.pshader.BadTVGlitchFilter;
 import com.haxademic.core.draw.filters.pshader.BadTVLinesFilter;
