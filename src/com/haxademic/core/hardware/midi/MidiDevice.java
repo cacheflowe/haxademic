7aba370e1842d3b799a7c9d54750f5c7ef46911f
cacheflowe <cacheflowe@cacheflowe.com>
2021-09-06 22:13:29 -0400

    Add String id for MIDI devices

--- src/com/haxademic/core/hardware/midi/MidiDevice.java
+++ src/com/haxademic/core/hardware/midi/MidiDevice.java
@@ -9,6 +9,7 @@ public class MidiDevice {
 	public MidiBus midiBus;
 	
 	public static MidiDevice instance;
+	public static boolean listedDevices = false;
 	
 	public static MidiDevice init(int midiDeviceInIndex, int midiDeviceOutIndex) {
 		if(instance != null) return instance;
@@ -22,14 +23,44 @@ public class MidiDevice {
 		return instance;
 	}
 
+	public static MidiDevice init(String midiDeviceName) {
+		if(instance != null) return instance;
+		instance = new MidiDevice(midiDeviceName, null);
+		return instance;
+	}
+	
+	public static MidiDevice init(String midiDeviceName, SimpleMidiListener delegate) {
+		if(instance != null) return instance;
+		instance = new MidiDevice(midiDeviceName, delegate);
+		return instance;
+	}
+	
 	public static MidiDevice instance() {
 		return instance;
 	}
 	
+	public static void printDevices() {
+		if(!listedDevices) {
+			MidiBus.list();
+			listedDevices = true;
+		}
+	}
+	
 	public MidiDevice(int midiDeviceInIndex, int midiDeviceOutIndex, SimpleMidiListener delegate) {
-		MidiBus.list();
+		printDevices();
 		new Thread(new Runnable() { public void run() {
 			midiBus = new MidiBus(this, midiDeviceInIndex, midiDeviceOutIndex);
+			midiBus.addMidiListener((MidiListener) MidiState.instance());
+			if(delegate != null) {
+				midiBus.addMidiListener(delegate);
+			}
+		}}).start();
+	}
+	
+	public MidiDevice(String midiDeviceName, SimpleMidiListener delegate) {
+		printDevices();		
+		new Thread(new Runnable() { public void run() {
+			midiBus = new MidiBus(this, midiDeviceName, midiDeviceName);
 			midiBus.addMidiListener((MidiListener) MidiState.instance());
 			if(delegate != null) {
 				midiBus.addMidiListener(delegate);
