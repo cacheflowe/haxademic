72fae144e791dcf4706934ae8c27ffd6652be83d
cacheflowe <cacheflowe@cacheflowe.com>
2021-09-16 10:43:52 -0400

    Update SpoutTexture to work with CEF Spout browser

--- src/com/haxademic/demo/draw/textures/Demo_SpoutTexture.java
+++ src/com/haxademic/demo/draw/textures/Demo_SpoutTexture.java
@@ -1,9 +1,7 @@
 package com.haxademic.demo.draw.textures;
 
 import com.haxademic.core.app.PAppletHax;
-import com.haxademic.core.draw.filters.pshader.GlitchShakeFilter;
-import com.haxademic.core.draw.filters.pshader.VignetteFilter;
-import com.haxademic.core.draw.image.ImageUtil;
+import com.haxademic.core.debug.DebugView;
 import com.haxademic.core.draw.textures.SpoutTexture;
 
 public class Demo_SpoutTexture 
@@ -14,22 +12,27 @@ extends PAppletHax {
 
 	protected void firstFrame() {
 		// SpoutTexture dimensions must match sender dimensions!
-		spoutTexture = new SpoutTexture(1120, 630);
+//		spoutTexture = new SpoutTexture(1920, 1080);
+//		spoutTexture = new SpoutTexture(960, 540);
+		spoutTexture = new SpoutTexture(1280, 720, "CEF_0");
+		spoutTexture.setFlipY();
+		DebugView.setTexture("spoutTexture.texture()", spoutTexture.texture());
 	}
 
 	protected void drawApp() {
-		background(0);
+		background(127);
 		
 		// update & draw to screen
 		spoutTexture.update();
-		ImageUtil.cropFillCopyImage(spoutTexture.texture(), p.g, true);
+//		ImageUtil.cropFillCopyImage(spoutTexture.texture(), p.g, true);
+		p.image(spoutTexture.texture(), 0, 0);
 		
 		// some post-processing, just for fun
-		GlitchShakeFilter.instance(p).setAmp(0.03f);
-		GlitchShakeFilter.instance(p).setTime(p.frameCount * 0.1f);
-		GlitchShakeFilter.instance(p).setGlitchSpeed(0.002f);
-		GlitchShakeFilter.instance(p).applyTo(p);
-		VignetteFilter.instance(p).applyTo(p);
+//		GlitchShakeFilter.instance(p).setAmp(0.03f);
+//		GlitchShakeFilter.instance(p).setTime(p.frameCount * 0.1f);
+//		GlitchShakeFilter.instance(p).setGlitchSpeed(0.002f);
+//		GlitchShakeFilter.instance(p).applyTo(p);
+//		VignetteFilter.instance(p).applyTo(p);
 	}
 
 }
