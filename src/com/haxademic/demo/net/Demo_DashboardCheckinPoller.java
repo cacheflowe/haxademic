c65133b7d15156f3b01a9bf52cd3aba4551a26b6
cacheflowe <cacheflowe@cacheflowe.com>
2021-09-16 11:02:10 -0400

    Update Demo_DashboardCheckinPoller.java

--- src/com/haxademic/demo/net/Demo_DashboardCheckinPoller.java
+++ src/com/haxademic/demo/net/Demo_DashboardCheckinPoller.java
@@ -1,19 +1,31 @@
 package com.haxademic.demo.net;
 
+import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.data.store.IAppStoreListener;
+import com.haxademic.core.debug.DebugView;
 import com.haxademic.core.draw.context.PG;
 import com.haxademic.core.net.DashboardCheckinPoller;
 
+import processing.core.PGraphics;
+import processing.core.PImage;
+
 public class Demo_DashboardCheckinPoller
 extends PAppletHax {
+extends PAppletHax
+implements IAppStoreListener {
 	public static void main(String args[]) { arguments = args; PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 	
 	protected DashboardCheckinPoller dashboardPoster;
 	
 	protected void firstFrame() {
 		DashboardCheckinPoller.DEBUG = true;
-		dashboardPoster = new DashboardCheckinPoller("test-app-2", "TEST APP 2", "http://localhost/haxademic/www/dashboard-new/", 5, 23, 0.5f);
-		dashboardPoster.setExtraImage(p.g, 20);
+		DashboardCheckinPoller.JPG_QUALITY = 0.85f;
+		dashboardPoster = new DashboardCheckinPoller("test-app-3", "TEST APP 3", "http://localhost/haxademic/www/dashboard-new/", 5, 10, 0.5f);
+		dashboardPoster.setCustomImage(p.g, 20);
+		
+		// listen for updates
+		P.store.addListener(this);
 	}
 	
 	protected void drawApp() {
@@ -38,5 +50,20 @@ extends PAppletHax {
 		dashboardPoster.setCustomValue("mouseX", ""+p.mouseX);
 		dashboardPoster.setCustomValue("mouseY", p.mouseY);
  	}
+	
+	public void updatedNumber(String key, Number val) {}
+	public void updatedString(String key, String val) {
+		DebugView.setValue(key, val);
+		if(key.equals(DashboardCheckinPoller.IMAGE_READY_SCREENSHOT_64)) P.out("NEW [IMAGE_READY_SCREENSHOT_64]");
+		if(key.equals(DashboardCheckinPoller.IMAGE_READY_CUSTOM_64)) P.out("NEW [IMAGE_READY_CUSTOM_64]");
+	}
+	public void updatedBoolean(String key, Boolean val) {}	
+	public void updatedImage(String key, PImage val) {
+		DebugView.setTexture(key, val);
+	}
+	public void updatedBuffer(String key, PGraphics val) {
+		DebugView.setTexture(key, val);
+	}
+
 	
 }
