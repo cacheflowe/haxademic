0c35757122ec64e8c431e89d2e8a3cb50743c155
cacheflowe <cacheflowe@cacheflowe.com>
2021-09-16 10:45:45 -0400

    Update SpoutTexture.java

--- src/com/haxademic/core/draw/textures/SpoutTexture.java
+++ src/com/haxademic/core/draw/textures/SpoutTexture.java
@@ -1,8 +1,10 @@
 package com.haxademic.core.draw.textures;
 
 import com.haxademic.core.app.P;
+import com.haxademic.core.draw.context.PG;
+import com.haxademic.core.draw.filters.pshader.FlipVFilter;
+import com.haxademic.core.draw.image.ImageUtil;
 
-import processing.core.PConstants;
 import processing.core.PGraphics;
 import spout.Spout;
 
@@ -10,17 +12,49 @@ public class SpoutTexture {
 
 	protected Spout spout;
 	protected PGraphics texture;
+	protected PGraphics textureFlipped;
+	protected String channelID;
+	protected boolean flipY = false;
 
 	public SpoutTexture(int w, int h) {
+		this(w, h, null);
+	}
+	
+	public SpoutTexture(int w, int h, String channelID) {
 		spout = new Spout(P.p);
-		texture = P.p.createGraphics(w, h, PConstants.P2D);
+		texture = PG.newPG(w, h);
+		if(channelID != null) {
+			this.channelID = channelID;
+			spout.setReceiverName(channelID);
+			spoutReconnect();
+		}
+	}
+	
+	public void setFlipY() {
+		flipY = true;
+		textureFlipped = PG.newPG(texture.width, texture.height);
+	}
+	
+	protected void spoutReconnect() {	
+		spout.closeReceiver();
+		spout.createReceiver(channelID);
 	}
 
 	public PGraphics texture() {
-		return texture;
+		return (flipY) ? textureFlipped : texture;
 	}
 
 	public void update() {
-		spout.receiveTexture(texture);
+		if(spout.receiveTexture()) {
+			PGraphics curTexture = spout.receiveTexture(texture);
+			if(texture.width != curTexture.width || texture.height != curTexture.height) {
+				texture = PG.newPG(curTexture.width, curTexture.height);
+				if(flipY) PG.newPG(texture.width, texture.height);
+			}
+		}
+		if(flipY) {
+			ImageUtil.copyImage(texture, textureFlipped);
+			FlipVFilter.instance(P.p).applyTo(textureFlipped);
+		}
 	}
 }
