5b6609ade03f7621935b3eaa6bc07830664acbd5
cacheflowe <cacheflowe@cacheflowe.com>
2021-10-12 23:28:05 -0400

    Update Demo_Joons_RenderBasicShape.java

--- src/com/haxademic/demo/render/joons/Demo_Joons_RenderBasicShape.java
+++ src/com/haxademic/demo/render/joons/Demo_Joons_RenderBasicShape.java
@@ -1,5 +1,6 @@
 package com.haxademic.demo.render.joons;
 
+import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.app.config.AppSettings;
 import com.haxademic.core.app.config.Config;
@@ -13,7 +14,7 @@ extends PAppletHax {
 	protected void config() {
 		Config.setProperty( AppSettings.SUNFLOW, true );
 		Config.setProperty( AppSettings.SUNFLOW_ACTIVE, true );
-		Config.setProperty( AppSettings.SUNFLOW_QUALITY, AppSettings.SUNFLOW_QUALITY_LOW );
+		Config.setProperty( AppSettings.SUNFLOW_QUALITY, AppSettings.SUNFLOW_QUALITY_HIGH );
 
 		Config.setProperty( AppSettings.WIDTH, 1280 );
 		Config.setProperty( AppSettings.HEIGHT, 720 );
@@ -32,30 +33,44 @@ extends PAppletHax {
 
 		setUpRoom();
 		
-		// set material
-		p.fill( 50, 200, 50 );
-		joons.jr.fill(JoonsWrapper.MATERIAL_SHINY, 50, 200, 50, 0.75f);
-
-		// draw box
-		p.rotateY(1);
-		p.rotateZ(1);
+		// draw boxes
+		p.rotateX(-0.3f);
+		p.rotateY(P.QUARTER_PI);
+		
+		
+		p.fill( 50, 100, 50 );
+		joons.jr.fill(JoonsWrapper.MATERIAL_SHINY, 50, 100, 50, 0.75f);
 		p.box(180);
+		
+		p.fill( 255, 255, 255 );
+		joons.jr.fill(JoonsWrapper.MATERIAL_SHINY, 255, 255, 255, 0.75f);
+		p.box(90, 360, 90);
+		
+		// draw 2nd plane box
+		p.fill( 20, 20, 20 );
+		joons.jr.fill(JoonsWrapper.MATERIAL_SHINY, 20, 20, 20, 0.75f);
+		p.box(1000, 10, 1000);
+
+	}
+	
+	public void keyPressed() {
+		if (key == 'r' || key == 'R') Renderer.instance().joons.jr.render(); // Press 'r' key to start rendering.
 	}
 
 	protected void setUpRoom() {
 		JoonsWrapper joons = Renderer.instance().joons;
 		p.pushMatrix();
 		translate(0, 0, 0);
-		float radiance = 20;
+		float radiance = 30;
 		int samples = 16;
 		joons.jr.background(JoonsWrapper.CORNELL_BOX, 
 				width * 6, height * 4, 3000,			// width, height, depth
 				radiance, radiance, radiance, samples,  // radiance rgb & samples
-				40, 40, 40, 							// left rgb
-				40, 40, 40, 							// right rgb
-				60, 60, 60,	 							// back rgb
-				60, 60, 60, 							// top rgb
-				60, 60, 60  							// bottom rgb
+				120, 120, 120, 							// left rgb
+				120, 120, 120, 							// right rgb
+				120, 120, 120,	 						// back rgb
+				120, 120, 120, 							// top rgb
+				120, 120, 120  							// bottom rgb
 		); 
 		popMatrix();		
 	}
