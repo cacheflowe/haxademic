7050910069088740e70308e6a913e9275153345d
cacheflowe <cacheflowe@cacheflowe.com>
2021-10-18 11:59:15 -0400

    ColorsHax - add more color groups and safe group/color getter

--- src/com/haxademic/core/draw/color/ColorsHax.java
+++ src/com/haxademic/core/draw/color/ColorsHax.java
@@ -37,7 +37,44 @@ public class ColorsHax {
 		new int[] {0xff31cbff, 0xfff6e098, 0xffff5734, 0xff9a39eb, 0xff58f74a},
 		new int[] {0xffa91ae5, 0xfffff600, 0xff81ff00, 0xffea27c1, 0xff000000},
 		new int[] {0xffdc1a27, 0xfff5328f, 0xff008c7f, 0xffdef427, 0xffed8100},
+		new int[] {0xff264653, 0xff2A9D8F, 0xffE9C46A, 0xffF4A261, 0xffE76F51},
+		new int[] {0xffEF476F, 0xffFFD166, 0xff06D6A0, 0xff118AB2, 0xff073B4C},
+		new int[] {0xff011627, 0xffFDFFFC, 0xff2EC4B6, 0xffE71D36, 0xffFF9F1C},
+		new int[] {0xff0081A7, 0xff00AFB9, 0xffFDFCDC, 0xffFED9B7, 0xffF07167},
+		new int[] {0xff1A535C, 0xff4ECDC4, 0xffF7FFF7, 0xffFF6B6B, 0xffFFE66D},
+		new int[] {0xff3D348B, 0xff7678ED, 0xffF7B801, 0xffF18701, 0xffF35B04},
+		new int[] {0xff0D3B66, 0xffFAF0CA, 0xffF4D35E, 0xffEE964B, 0xffF95738},
+		new int[] {0xffCFDBD5, 0xffE8EDDF, 0xffF5CB5C, 0xff242423, 0xff333533},
+		new int[] {0xff001524, 0xff15616D, 0xffFFECD1, 0xffFF7D00, 0xff78290F},
+		new int[] {0xffDDFFF7, 0xff93E1D8, 0xffFFA69E, 0xffAA4465, 0xff861657},
+		new int[] {0xff156064, 0xff00C49A, 0xffF8E16C, 0xffFFC2B4, 0xffFB8F67},
+		new int[] {0xff227C9D, 0xff17C3B2, 0xffFFCB77, 0xffFEF9EF, 0xffFE6D73},
+		new int[] {0xff432371, 0xff714674, 0xff9F6976, 0xffCC8B79, 0xffFAAE7B},
+		new int[] {0xffD6D6D6, 0xffFFEE32, 0xffFFD100, 0xff202020, 0xff333533},
 	};
+	
+	// get more from Coolors.co by running this, then clicking on the info box. check the console for output
+	/*
+	document.body.addEventListener('click', (e) => {
+	  let colorContainer = e.target.closest('.explore-palette');
+	  if(colorContainer) {
+	    let colorHexSpans = colorContainer.querySelectorAll('.explore-palette_colors span');
+	    let colorCodeStr = "new int[] {"; 
+	    colorHexSpans.forEach((el, i) => {
+	      colorCodeStr += '0xff' + el.innerText;
+	      if(i < colorHexSpans.length - 1) colorCodeStr += ', ';
+	    });
+	    colorCodeStr += '}';
+	    console.log(colorCodeStr);
+	  }
+	})
+	*/
+	
+	public static int colorFromGroupAt(int group, int color) {
+		int groupLooped = group % COLOR_GROUPS.length;
+		int[] colorGroup = COLOR_GROUPS[groupLooped];
+		return colorGroup[color % colorGroup.length];
+	}
 	
 	public static int PRIDE[] = new int[]{
 		0xFFFF0000,
