2236751830b76aa13ecec143b2e09139a2ff3d4d
cacheflowe <cacheflowe@cacheflowe.com>
2021-12-08 20:23:32 -0500

    Hoff's Voronoi tests

---
+++ src/com/haxademic/demo/draw/image/Demo_VoronoiHoffs.java
@@ -0,0 +1,117 @@
+package com.haxademic.demo.draw.image;
+
+import com.haxademic.core.app.P;
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.app.config.AppSettings;
+import com.haxademic.core.app.config.Config;
+import com.haxademic.core.ui.UI;
+
+public class Demo_VoronoiHoffs 
+extends PAppletHax {
+	public static void main(String args[]) { arguments = args; PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
+
+	// from: https://openprocessing.org/sketch/352817/
+	// with some adjustments to work in newer Processing
+	
+	protected float hoffOrthoFactor;
+	protected String NUM_CELLS = "NUM_CELLS";
+	protected String CELL_DETAIL = "CELL_DETAIL";
+	protected int MAX_CELLS = 1000;
+	protected Cell cells[] = new Cell[1000];
+
+	protected void config() {
+		Config.setProperty( AppSettings.WIDTH, 1280 );
+		Config.setProperty( AppSettings.HEIGHT, 1280 );
+		Config.setProperty( AppSettings.SHOW_DEBUG, true );
+		Config.setProperty( AppSettings.SHOW_UI, true );
+		Config.setProperty( AppSettings.SHOW_FPS_IN_TITLE, true );
+	}
+
+	protected void firstFrame() {
+		UI.addTitle("VORONOI");
+		UI.addSlider(NUM_CELLS, 30, 3, MAX_CELLS, 1, false);
+		UI.addSlider(CELL_DETAIL, 30, 3, 100, 1, false);
+		
+		//		WebCam.instance().setDelegate(this);
+//		pg = PG.newPG(p.width, p.height, false, false);
+//		pg.noSmooth();
+		
+		for (int i = 0; i < MAX_CELLS; i++) {
+			cells[i] = new Cell(
+				random(width), random(height), 
+				random(-1, 1), random(-1, 1)
+			);
+		}
+	}
+
+	protected void drawApp() {
+		
+		// set up context & draw voronoi to pg
+		pg.beginDraw();
+//		PG.setDrawFlat2d(pg, true);
+		pg.background(127);
+		pg.noStroke();
+		pg.ortho();		
+		hoffOrthoFactor = dist(0, 0, pg.width, pg.height);
+		for (int i = 0; i < UI.valueInt(NUM_CELLS); i++) {
+			pg.fill(sin(i) * 127 + 127, sin(i+1) * 127 + 127, sin(i+2) * 127 + 127);
+//			pg.fill(i % 2 == 0 ? 0 : 255);
+			cells[i].show();
+			cells[i].advance();
+		}
+		pg.endDraw();
+		
+		// draw to screen
+		p.image(pg, 0, 0);
+	}
+
+
+	public class Cell {
+		protected float x, y, sx, sy;
+
+		Cell(float x, float y, float sx, float sy) {
+			this.x = x;
+			this.y = y; 
+			this.sx = sx; 
+			this.sy = sy;
+		}
+
+		void show() {
+			pg.pushMatrix();
+			pg.translate(this.x, this.y, 0);
+			float rads = 0;
+			float segmentRads = P.TWO_PI / UI.value(CELL_DETAIL);
+			float rFix = hoffOrthoFactor * 0.5f;
+			pg.beginShape();
+			for (int i = 0; i < UI.value(CELL_DETAIL); i++) {
+//				pg.fill(sin(this.x/70 + i/3) * 127 + 127, sin(this.y/70 + i/3+1) * 127 + 127, sin(this.y/70 + i/1+2) * 127 + 127);
+
+				rads = segmentRads * i;
+				pg.vertex(0, 0, -rFix);
+				pg.vertex(hoffOrthoFactor * cos(rads), hoffOrthoFactor * sin(rads), -hoffOrthoFactor);
+				pg.vertex(hoffOrthoFactor * cos(rads + segmentRads), hoffOrthoFactor * sin(rads + segmentRads), -hoffOrthoFactor);
+			}
+			pg.endShape();
+			pg.fill(0);
+//			pg.ellipse(0, 0, 5, 5);
+			pg.popMatrix();
+		}
+
+		void advance() {
+//			this.sx = 0;
+//			this.sy = 1;
+			this.x += this.sx;
+			this.y += this.sy;
+			if (this.x < 0 || this.x > pg.width) {
+				this.sx = -this.sx;
+			}     
+//			if (this.y < 0 || this.y > pg.height * 2) {
+			if (this.y > pg.height * 2) {
+//				this.sy = -this.sy;
+				this.y = -pg.height;
+				this.sy = p.random(0.5f, 6f);
+				this.sx = p.random(-1f, 1f);
+			}
+		}
+	}
+}
---
+++ src/com/haxademic/demo/draw/image/Demo_VoronoiHoffsCachedShapes.java
@@ -0,0 +1,200 @@
+package com.haxademic.demo.draw.image;
+
+import com.haxademic.core.app.P;
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.app.config.AppSettings;
+import com.haxademic.core.app.config.Config;
+import com.haxademic.core.debug.DebugView;
+import com.haxademic.core.draw.context.PG;
+import com.haxademic.core.draw.filters.pshader.BlurProcessingFilter;
+import com.haxademic.core.draw.filters.pshader.FakeLightingFilter;
+import com.haxademic.core.draw.image.ImageUtil;
+import com.haxademic.core.math.MathUtil;
+import com.haxademic.core.render.FrameLoop;
+import com.haxademic.core.ui.UI;
+
+import processing.core.PGraphics;
+import processing.core.PShape;
+
+public class Demo_VoronoiHoffsCachedShapes 
+extends PAppletHax {
+	public static void main(String args[]) { arguments = args; PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
+
+	// from: https://openprocessing.org/sketch/352817/
+	// with some adjustments to work in newer Processing
+	
+	protected float hoffOrthoFactor;
+	protected PGraphics blurMap;
+	protected String NUM_CELLS = "NUM_CELLS";
+	protected String CELL_DETAIL = "CELL_DETAIL";
+	protected int MAX_CELLS = 6000;
+	protected Cell cells[] = new Cell[MAX_CELLS];
+
+	protected void config() {
+		int scaleUp = 1;
+		Config.setAppSize( 1920 * scaleUp, 1080 * scaleUp );
+		Config.setPgSize( 1920 * scaleUp, 1080 * scaleUp );
+//		Config.setProperty( AppSettings.FULLSCREEN, true );
+//		Config.setProperty( AppSettings.SCREEN_X, 0 );
+//		Config.setProperty( AppSettings.SCREEN_Y, 0 );
+		Config.setProperty( AppSettings.SHOW_DEBUG, true );
+		Config.setProperty( AppSettings.SHOW_UI, true );
+		Config.setProperty( AppSettings.SHOW_FPS_IN_TITLE, true );
+		Config.setProperty( AppSettings.LOOP_FRAMES, 600 );
+	}
+
+	protected void firstFrame() {
+		UI.addTitle("VORONOI");
+		UI.addSlider(NUM_CELLS, 1200, 3, MAX_CELLS, 10, false);
+		UI.addSlider(CELL_DETAIL, 60, 3, 100, 1, false);
+		
+		blurMap = PG.newPG(pg.width/4, pg.height/4);
+		
+		for (int i = 0; i < MAX_CELLS; i++) {
+			cells[i] = new Cell(
+				random(pg.width), random(pg.height), 
+				random(-1, 1), random(-1, 1)
+			);
+		}
+	}
+
+	protected void drawApp() {
+		
+		// set up context & draw voronoi to pg
+		pg.beginDraw();
+//		PG.setDrawFlat2d(pg, true);
+		pg.background(127);
+		pg.noStroke();
+		pg.ortho();		
+		hoffOrthoFactor = dist(0, 0, pg.width, pg.height);
+		for (int i = 0; i < UI.valueInt(NUM_CELLS); i++) {
+			pg.fill(sin(i) * 127 + 127, sin(i+1) * 127 + 127, sin(i+2) * 127 + 127);
+//			pg.fill(i % 2 == 0 ? 0 : 255);
+			
+			// grid
+			/*
+			int gridRes = P.floor(P.sqrt(UI.valueInt(NUM_CELLS)));
+			float spacingH = pg.width / gridRes * 1.1f;
+			float spacingV = pg.height / gridRes * 1.1f;
+			float xIndex = MathUtil.gridColFromIndex(i, gridRes);
+			float yIndex = MathUtil.gridRowFromIndex(i, gridRes);
+			if(xIndex % 2 == 0) {
+				xIndex -= 0.5f;
+				yIndex += 0.5f;
+			}
+			if(xIndex % 3 == 0) {
+				xIndex += 0.5f;
+				yIndex += 0.5f;
+			}
+			cells[i].setPosition(xIndex * spacingH, yIndex * spacingV);
+			*/
+			
+			// spiral
+			/*
+			float segmentRads = P.TWO_PI / 36;
+			float rads = p.frameCount * 0.001f + i * segmentRads; 
+			float radius = i * 1.5f;
+			if(i%2 == 0) {
+				cells[i].setPosition(pg.width/2 + P.cos(rads) * radius, pg.height/2 + P.sin(rads) * radius);
+			} else {
+				cells[i].setPosition(pg.width/2 + P.cos(-rads) * radius/1.f, pg.height/2 + P.sin(-rads) * radius/1.f);
+			}
+			*/
+
+			// circles
+//			float numVertices = 28;
+//			float segmentRads = P.TWO_PI / numVertices;
+//			float rads = i * segmentRads; 
+//			float radius = P.floor(i / numVertices) * segmentRads * 200f;
+//			float offsetX = FrameLoop.noiseLoop(0.9f, i) * 0.1f;
+//			float offsetY = FrameLoop.noiseLoop(0.9f, i) * 0.1f;
+//			float offsetScale = FrameLoop.noiseLoop(0.9f, i*2) * 0.4f;
+//			radius *= 1f + offsetScale;
+//			cells[i].setPosition(pg.width/2 + P.cos(rads + offsetX) * radius, pg.height/2 + P.sin(rads + offsetY) * radius);
+			
+			
+			cells[i].show();
+			cells[i].advance();
+		}
+		pg.endDraw();
+		
+		// make blur map
+//		ImageUtil.copyImage(pg, blurMap);
+//		BlurProcessingFilter.instance(p).setBlurSize(4);
+//		BlurProcessingFilter.instance(p).setSigma(20);
+//		BlurProcessingFilter.instance(p).applyTo(blurMap);
+//		DebugView.setTexture("blurMap", blurMap);
+		
+		// draw to screen
+		p.image(pg, 0, 0);
+	}
+
+
+	public class Cell {
+		protected float x, y, sx, sy;
+		protected PShape shape;
+
+		Cell(float x, float y, float sx, float sy) {
+			this.x = x;
+			this.y = y; 
+			this.sx = sx; 
+			this.sy = sy;
+			randomDirection();
+		}
+		
+		protected void randomDirection() {
+			this.sy = p.random(0.25f, 1f);
+			this.sx = 0; //p.random(-0.5f, 0.5f);
+//			this.sx = P.map(this.x, 0, pg.width, -1f, 1f);
+		}
+		
+		public Cell setPosition(float x, float y) {
+			this.x = x;
+			this.y = y; 
+			return this;
+		}
+		public float x() { return x; }
+		public float y() { return y; }
+
+		public void show() {
+			float rads = 0;
+			float segmentRads = P.TWO_PI / UI.value(CELL_DETAIL);
+			float rFix = hoffOrthoFactor * 0.5f;
+			if(shape == null) {
+				shape = p.createShape();
+				shape.beginShape();
+				for (int i = 0; i < UI.value(CELL_DETAIL); i++) {
+	//				pg.fill(sin(this.x/70 + i/3) * 127 + 127, sin(this.y/70 + i/3+1) * 127 + 127, sin(this.y/70 + i/1+2) * 127 + 127);
+	
+					rads = segmentRads * i;
+					shape.vertex(0, 0, -rFix);
+					shape.vertex(hoffOrthoFactor * cos(rads), hoffOrthoFactor * sin(rads), -hoffOrthoFactor);
+					shape.vertex(hoffOrthoFactor * cos(rads + segmentRads), hoffOrthoFactor * sin(rads + segmentRads), -hoffOrthoFactor);
+				}
+				shape.endShape();
+				shape.disableStyle();
+			}
+			pg.pushMatrix();
+			pg.translate(this.x, this.y, 0);
+			pg.shape(shape);
+			pg.popMatrix();
+		}
+
+		public void advance() {
+//			this.sx = 0;
+//			this.sy = 1;
+			this.x += this.sx;
+			this.y += this.sy;
+			if(this.y > 0) this.sy += this.y * 0.00015f;
+			if (this.x < 0 || this.x > pg.width) {
+				this.sx = -this.sx;
+			}     
+//			if (this.y < 0 || this.y > pg.height * 2) {
+			if (this.y > pg.height * 2) {
+//				this.sy = -this.sy;
+				this.y = -pg.height;
+				randomDirection();
+			}
+		}
+	}
+}
