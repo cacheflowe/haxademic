f7529ef515b6b3e3b264b7f1f5bea3979780a2bd
cacheflowe <cacheflowe@cacheflowe.com>
2022-01-15 23:01:45 -0500

    Update Demo_Java8ForEach.java

--- src/com/haxademic/demo/system/Demo_Java8ForEach.java
+++ src/com/haxademic/demo/system/Demo_Java8ForEach.java
@@ -1,5 +1,11 @@
 package com.haxademic.demo.system;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.ArrayList;
 
 import com.haxademic.core.app.P;
@@ -25,6 +31,47 @@ extends PAppletHax {
 		strings.forEach(P::error);
 		// local instance method
 		strings.forEach(name -> testOutput(name));
+		
+		// switch state as assignment (Java 14)
+		String day = "MONDAY";
+		boolean isTodayHoliday = switch (day) {
+		    case "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY" -> false;
+		    case "SATURDAY", "SUNDAY" -> true;
+		    default -> throw new IllegalArgumentException("What's a " + day);
+		};
+		P.out("isTodayHoliday", isTodayHoliday);
+		
+		
+		// multiline String (Java 14)
+		String multiline = """
+		    A quick brown fox jumps over a lazy dog; \n
+		    the lazy dog howls loudly.
+		""";
+		P.out("multiline");
+		P.out(multiline);
+		
+		// String indentation (Java 12)
+		String testIndent = "Test sentence for indentation";
+		testIndent.indent(4);
+		P.out("testIndent");
+		P.out(testIndent);
+		
+		// https://www.baeldung.com/java-11-new-features
+		// httpclient (Java 11)
+		HttpClient httpClient = HttpClient.newBuilder()
+				  .version(HttpClient.Version.HTTP_2)
+				  .connectTimeout(Duration.ofSeconds(20))
+				  .build();
+				HttpRequest httpRequest = HttpRequest.newBuilder()
+				  .GET()
+				  .uri(URI.create("https://cacheflowe.com"))
+				  .build();
+		try {
+			HttpResponse httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+			P.out(httpResponse.body());
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 	
 	protected void testOutput(String input) {
