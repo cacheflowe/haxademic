4e647252984f080b00d658988116fefa1f9bc705
cacheflowe <cacheflowe@cacheflowe.com>
2022-02-23 16:44:36 -0500

    Create polar-split-disk-to-equirectangular.glsl

---
+++ data/haxademic/shaders/filters/polar-split-disk-to-equirectangular.glsl
@@ -0,0 +1,76 @@
+// with help from Nikki & Alex Miller
+
+#ifdef GL_ES
+precision mediump float;
+precision mediump int;
+#endif
+
+#define PROCESSING_TEXTURE_SHADER
+
+uniform sampler2D texture;
+uniform vec2 texOffset;
+varying vec4 vertColor;
+varying vec4 vertTexCoord;
+
+uniform float crossfade = 1.0;
+
+#define PI     3.14159265358
+#define TWO_PI 6.28318530718
+
+void main() {
+  // Quick aliases
+  float x = vertTexCoord.x;
+  float y = 1. - vertTexCoord.y;
+
+	// coords for animated translation
+	vec2 origUV = vec2(x, y);
+
+  // Calculate polar theta, r and center of hemisphere. Fix if other hemisphere.
+  float theta = TWO_PI * x;
+  vec2 center = vec2(0.25, 0.5);
+  float r = y;
+  if (y > 0.5) {
+    center = vec2(0.75, 0.5);
+    r = 1.0 - r;
+    theta = PI - theta;
+  }
+	r *= 0.995;
+
+  // Calculate direction from angle, then lookup r distance in the direction. r
+  // fits 4 times horizontally and 2 times vertically so have to squash.
+  vec2 dir = vec2(cos(theta), sin(theta));
+	vec2 mappedUV = center + r * dir * vec2(0.5, 1.0);
+
+	// animate between UVs
+	vec2 finalUV = mix(origUV, mappedUV, crossfade);
+
+	// grab final color
+  vec4 color = texture2D(texture, finalUV);
+  gl_FragColor = color;
+}
+
+/*
+// pristine version
+void main() {
+  // Quick aliases
+  float x = vertTexCoord.x;
+  float y = 1. - vertTexCoord.y;
+
+  // Calculate polar theta, r and center of hemisphere. Fix if other hemisphere.
+  float theta = TWO_PI * x;
+  vec2 center = vec2(0.25, 0.5);
+  float r = y;
+  if (y > 0.5) {
+    center = vec2(0.75, 0.5);
+    r = 1.0 - r;
+    theta = PI - theta;
+  }
+
+  // Calculate direction from angle, then lookup r distance in the direction. r
+  // fits 4 times horizontally and 2 times vertically so have to squash.
+  vec2 dir = vec2(cos(theta), sin(theta));
+  vec4 color = texture2D(texture, center + r * dir * vec2(0.5, 1.0));
+
+  gl_FragColor = color;
+}
+*/
