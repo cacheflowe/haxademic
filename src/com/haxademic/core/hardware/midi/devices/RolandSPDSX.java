7e08c3f39f3b498ea1caa78a9d8d9feef9e285ed
cacheflowe <cacheflowe@cacheflowe.com>
2022-03-25 12:46:50 -0400

    Create RolandSPDSX.java

---
+++ src/com/haxademic/core/hardware/midi/devices/RolandSPDSX.java
@@ -0,0 +1,18 @@
+package com.haxademic.core.hardware.midi.devices;
+
+public class RolandSPDSX {
+	public static String deviceName = "SPD-SX";
+	
+	public static int PAD_01 = 60;
+	public static int PAD_02 = 61;
+	public static int PAD_03 = 62;
+	public static int PAD_04 = 63;
+	public static int PAD_05 = 64;
+	public static int PAD_06 = 65;
+	public static int PAD_07 = 66;
+	public static int PAD_08 = 67;
+	public static int PAD_09 = 68;
+	
+	public static int KNOB_1 = 70;
+	public static int KNOB_2 = 71;
+}
