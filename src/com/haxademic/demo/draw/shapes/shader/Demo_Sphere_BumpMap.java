9e60b07ca59f0518f6266ab610d21e33a7313dec
cacheflowe <cacheflowe@cacheflowe.com>
2022-04-21 14:31:58 -0400

    Update Demo_Sphere_BumpMap.java

--- src/com/haxademic/demo/draw/shapes/shader/Demo_Sphere_BumpMap.java
+++ src/com/haxademic/demo/draw/shapes/shader/Demo_Sphere_BumpMap.java
@@ -2,13 +2,16 @@ package com.haxademic.demo.draw.shapes.shader;
 
 import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.app.config.AppSettings;
+import com.haxademic.core.app.config.Config;
 import com.haxademic.core.debug.DebugView;
 import com.haxademic.core.draw.context.PG;
 import com.haxademic.core.draw.filters.pshader.BlurProcessingFilter;
+import com.haxademic.core.draw.filters.pshader.BrightnessFilter;
 import com.haxademic.core.draw.image.ImageUtil;
 import com.haxademic.core.file.FileUtil;
 import com.haxademic.core.hardware.mouse.Mouse;
-import com.haxademic.core.media.DemoAssets;
+import com.haxademic.core.render.FrameLoop;
 
 import processing.core.PGraphics;
 import processing.core.PImage;
@@ -28,19 +31,28 @@ extends PAppletHax {
 	protected PShape sphere;
 	protected PShader bumpMapShader;
 
+	protected void config() {
+		Config.setAppSize(1024, 1024);
+		Config.setProperty(AppSettings.LOOP_FRAMES, 900);
+	}
+	
 	protected void firstFrame() {
 		// load textures
-		texture = DemoAssets.textureJupiter();
-		bumpMap = ImageUtil.imageToGraphics(DemoAssets.textureJupiter());
-		specMap = ImageUtil.imageToGraphics(DemoAssets.textureJupiter());
+		texture = P.getImage("images/textures/space/moon-layers/color.jpg"); // DemoAssets.textureJupiter();
+		bumpMap = ImageUtil.imageToGraphics(P.getImage("images/textures/space/moon-layers/displacement.jpg"));
+		ImageUtil.flipV(bumpMap);
+		specMap = ImageUtil.imageToGraphics(bumpMap);
 		
 		// adjust texture, since we're reusing the texture map for alternate purposes
 		BlurProcessingFilter.instance(p).setBlurSize(2);
 		BlurProcessingFilter.instance(p).setSigma(1);
-		for(int i=0; i < 5; i++) BlurProcessingFilter.instance(p).applyTo(bumpMap);
+//		for(int i=0; i < 5; i++) BlurProcessingFilter.instance(p).applyTo(bumpMap);
 		DebugView.setTexture("bumpMap", bumpMap);
 		
 		ImageUtil.copyImage(bumpMap, specMap);
+		BrightnessFilter.instance(p).setBrightness(0.25f);
+		BrightnessFilter.instance(p).applyTo(specMap);
+		for(int i=0; i < 2; i++) BlurProcessingFilter.instance(p).applyTo(specMap);
 		
 		// load shader
 		bumpMapShader = loadShader(
@@ -54,7 +66,7 @@ extends PAppletHax {
 		
 		// create sphere
 		p.sphereDetail(150);
-		sphere = createShape(P.SPHERE, 200);
+		sphere = createShape(P.SPHERE, p.height * 0.35f);
 		sphere.setStroke(false);
 		sphere.setSpecular(color(125));
 		sphere.setShininess(10);
@@ -75,7 +87,8 @@ extends PAppletHax {
 		// draw from center
 		p.shader(bumpMapShader);
 		p.pushMatrix();
-		p.rotateY(P.map(p.mouseX, 0, p.width, 0, P.TWO_PI));
+//		p.rotateY(P.map(p.mouseX, 0, p.width, 0, P.TWO_PI));
+		p.rotateY(FrameLoop.progressRads());
 		p.shape(sphere);
 		p.popMatrix();	  
 	}
