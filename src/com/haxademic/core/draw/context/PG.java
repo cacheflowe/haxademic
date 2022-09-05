adf91d13f591b68cf5c8b6dad544bf7f086753e7
cacheflowe <cacheflowe@cacheflowe.com>
2022-09-05 18:47:32 -0400

    Update PG better lights options

--- src/com/haxademic/core/draw/context/PG.java
+++ src/com/haxademic/core/draw/context/PG.java
@@ -188,14 +188,16 @@ public class PG {
 		setBetterLights(p.g);
 	}
 	
-	public static void setBetterLights( PGraphics p ) {
-		// setup lighting props
-		p.ambient(127);
-		p.lightSpecular(130, 130, 130); 
-		p.directionalLight(200, 200, 200, -0.0f, -0.0f, 1); 
-		p.directionalLight(200, 200, 200, 0.0f, 0.0f, -1); 
-		p.specular(p.color(200)); 
-		p.shininess(5.0f); 
+	public static void setBetterLights(PGraphics pg) {
+		pg.lightSpecular(130, 130, 130); 
+		pg.directionalLight(200, 200, 200, -0.0f, -0.0f, 1); 
+		pg.directionalLight(200, 200, 200, 0.0f, 0.0f, -1); 
+	}
+
+	public static void setBetterLightsAbove(PGraphics pg) {
+		pg.lightSpecular(130, 130, 130); 
+		pg.directionalLight(200, 200, 200, -0.0f, 1.0f, 1); 
+		pg.directionalLight(200, 200, 200, 0.0f, 1.0f, -1); 
 	}
 	
 	//////////////////////////////
