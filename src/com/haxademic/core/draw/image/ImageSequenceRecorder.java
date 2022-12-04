52e734c0c8c759adc05db551596386122b07b5fd
cacheflowe <cacheflowe@cacheflowe.com>
2022-12-04 17:46:32 -0500

    Improve ImageSequenceRecorder

--- src/com/haxademic/core/draw/image/ImageSequenceRecorder.java
+++ src/com/haxademic/core/draw/image/ImageSequenceRecorder.java
@@ -1,5 +1,6 @@
 package com.haxademic.core.draw.image;
 
+import com.haxademic.core.app.P;
 import com.haxademic.core.draw.context.PG;
 
 import processing.core.PGraphics;
@@ -23,7 +24,7 @@ public class ImageSequenceRecorder {
 	protected void buildFrames() {
 		images = new PGraphics[numFrames];
 		for (int i = 0; i < numFrames; i++) {
-			images[i] = PG.newPG2DFast(width, height);
+			images[i] = PG.newPG(width, height);
 		}
 	}
 	
@@ -36,11 +37,15 @@ public class ImageSequenceRecorder {
 	}
 	
 	public int frameIndex() {
-		return frameIndex;
+		return safeIndex();
+	}
+	
+	protected int safeIndex() {
+	    return P.constrain(frameIndex, 0, numFrames - 1);
 	}
 	
 	public void reset() {
-	    frameIndex = 0;
+	    frameIndex = -1;
 	}
 	
 	public int addFrame(PImage img) {
@@ -55,8 +60,16 @@ public class ImageSequenceRecorder {
 	    return (frameIndex < numFrames - 1) ? false : true;
 	}
 	
+	public boolean isRecording() {
+	    return !isComplete();
+	}
+	
+	public float recordProgress() {
+	    return (float) safeIndex() / numFrames;
+	}
+	
 	public PGraphics getCurFrame() {
-		return images[frameIndex];
+		return images[safeIndex()];
 	}
 	
 	public void drawDebug(PGraphics pg) {
@@ -67,7 +80,7 @@ public class ImageSequenceRecorder {
 		if(openContext) pg.beginDraw();
 		float frameW = (float) pg.width / (float) numFrames;
 		float frameH = frameW * ((float) height / (float) width);
-		for (int i = 0; i < frameIndex; i++) {
+		for (int i = 0; i < safeIndex(); i++) {
 			float x = frameW * i;
 			pg.image(images[i], x, 0, frameW, frameH);
 		}
--- src/com/haxademic/demo/draw/image/Demo_ImageSequenceRecorder.java
+++ src/com/haxademic/demo/draw/image/Demo_ImageSequenceRecorder.java
@@ -1,5 +1,6 @@
 package com.haxademic.demo.draw.image;
 
+import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.app.config.AppSettings;
 import com.haxademic.core.app.config.Config;
@@ -22,6 +23,7 @@ implements IWebCamCallback {
 
 	protected ImageSequenceRecorder recorder;
 	protected PGraphics camBuffer;
+	protected boolean shouldRecord = false;
 	
 	protected void config() {
 		Config.setProperty(AppSettings.SHOW_DEBUG, true );
@@ -29,17 +31,32 @@ implements IWebCamCallback {
 		
 	protected void firstFrame () {
 		camBuffer = p.createGraphics(640, 480, PRenderers.P3D);
-		recorder = new ImageSequenceRecorder(camBuffer.width, camBuffer.height, 20);
+		// recorder = new ImageSequenceRecorder(camBuffer.width, camBuffer.height, 20);
+		recorder = new ImageSequenceRecorder(p.width/2, p.height, 60);
 		WebCam.instance().setDelegate(this);
 	}
 
 	protected void drawApp() {
+	    p.background(0);
+	    
 	    // reset recording
-	    if(KeyboardState.keyTriggered(' ')) recorder.reset();
+	    if(KeyboardState.keyTriggered(' ')) {
+	        recorder.reset();
+	        shouldRecord = true;
+	    }
 
 	    // draw camera & recording, side-by-side
 		ImageUtil.cropFillCopyImage(camBuffer, p.g, 0, 0, p.width/2, p.height, true);
-		ImageUtil.cropFillCopyImage(recorder.imageAtFrame(p.frameCount/4), p.g, p.width/2, 0, p.width/2, p.height, true);
+		ImageUtil.cropFillCopyImage(recorder.imageAtFrame(p.frameCount/2), p.g, p.width/2, 0, p.width/2, p.height, true);
+		
+		// draw recording indicator
+		if(recorder.isRecording()) {
+		    p.push();
+		    p.translate(p.width/2 - 100, 40);
+		    p.fill(255, 0, 0);
+		    p.arc(30, 30, 60, 60, -P.HALF_PI, -P.HALF_PI + P.TWO_PI * recorder.recordProgress());
+		    p.pop();
+		}
 		
 		// draw recorder debug
 		recorder.drawDebug(p.g);
@@ -49,12 +66,15 @@ implements IWebCamCallback {
 	public void newFrame(PImage frame) {
 		// set recorder frame - use buffer as intermediary to fix aspect ratio
 		ImageUtil.copyImageFlipH(frame, camBuffer);
-		recorder.addFrame(camBuffer);
-		// do some post-processing
-		SaturationFilter.instance(p).setSaturation(0);
-		SaturationFilter.instance(p).applyTo(recorder.getCurFrame());
-		// set debug staus
 		DebugView.setValue("Last WebCam frame", p.frameCount);
+		
+		// record if we've triggered it
+		if(shouldRecord) { 
+    		recorder.addFrame(camBuffer);
+    		// do some post-processing
+    		SaturationFilter.instance(p).setSaturation(0);
+    		SaturationFilter.instance(p).applyTo(recorder.getCurFrame());
+		}
 	}
 
 }
