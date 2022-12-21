768d153c48ec0c3faebc77a746b73b89c9f83f50
cacheflowe <cacheflowe@cacheflowe.com>
2022-12-21 13:24:50 -0500

    Clean up demo

--- src/com/haxademic/demo/math/Demo_MathUtil_saw.java
+++ src/com/haxademic/demo/math/Demo_MathUtil_saw.java
@@ -4,18 +4,13 @@ import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.app.config.AppSettings;
 import com.haxademic.core.app.config.Config;
-import com.haxademic.core.draw.context.OpenGLUtil;
 import com.haxademic.core.draw.context.PG;
 import com.haxademic.core.math.MathUtil;
-
-import processing.core.PConstants;
-import processing.core.PGraphics;
 
 public class Demo_MathUtil_saw
 extends PAppletHax {
 	public static void main(String args[]) { arguments = args; PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 	
-	protected PGraphics _texture;	
 	
 	protected void config() {
 		Config.setProperty( AppSettings.RENDERING_MOVIE, "false" );
@@ -24,30 +19,24 @@ extends PAppletHax {
 	}
 		
 	protected void firstFrame() {
-		_texture = P.p.createGraphics( 520, 120, P.P3D );
-		_texture.smooth(OpenGLUtil.SMOOTH_HIGH);
-		_texture.beginDraw();
-		_texture.background(0);
-		_texture.endDraw();
 	}
 	
 	protected void drawApp() {
 		p.background(0);
 
 		// texture feedback
-		// _texture.copy(_texture, 0, 0, _texture.width, _texture.height, -1.5f, 0, _texture.width, _texture.height);
-//		_texture.copy(_texture, 0, 0, _texture.width, _texture.height, -2, -2, _texture.width, _texture.height + 4);
+		// pg.copy(pg, 0, 0, pg.width, pg.height, -1.5f, 0, pg.width, pg.height);
+//		pg.copy(pg, 0, 0, pg.width, pg.height, -2, -2, pg.width, pg.height + 4);
 
 		// start texture drawing
-		_texture.beginDraw();
-		_texture.rectMode( PConstants.CENTER );
-		_texture.ellipseMode( PConstants.CENTER );
-		_texture.fill( 255, 255, 255, 255 );
-		_texture.noStroke();
+		pg.beginDraw();
+		PG.setDrawCenter(pg);
+		pg.fill( 255, 255, 255, 255 );
+		pg.noStroke();
 		
 		// fade out
-		_texture.fill( 0, 7f );
-		_texture.rect(p.width/2f, p.height/2f, p.width, p.height);
+		pg.fill( 0, 7f );
+		pg.rect(p.width/2f, p.height/2f, p.width, p.height);
 
 		// increment oscillations
 		float incrementer = p.frameCount / 20f;
@@ -55,24 +44,24 @@ extends PAppletHax {
 		
 		// draw sin
 		float sin = P.sin(incrementer);
-		_texture.fill(255, 255, 0);
-		_texture.ellipse(250, halfH + 20 * sin, 20, 20);
+		pg.fill(255, 255, 0);
+		pg.ellipse(250, halfH + 20 * sin, 20, 20);
 		
 		// draw saw
 		float saw = MathUtil.saw(incrementer);
-		_texture.fill(255, 0, 255);
-		_texture.ellipse(350, halfH + 20 * saw, 20, 20);
+		pg.fill(255, 0, 255);
+		pg.ellipse(350, halfH + 20 * saw, 20, 20);
 		
 		// draw saw tan
 		float sawtan = MathUtil.sawTan(incrementer * 2f);
-		_texture.fill(0, 255, 255);
-		_texture.ellipse(450, halfH + 20 * sawtan, 20, 20);
+		pg.fill(0, 255, 255);
+		pg.ellipse(450, halfH + 20 * sawtan, 20, 20);
 		
 		// finish drawing
-		_texture.endDraw();
+		pg.endDraw();
 		
 		// draw texture to stage
 		PG.setColorForPImage(p);
-		p.image(_texture, 0, 0);
+		p.image(pg, 0, 0);
 	}
 }
