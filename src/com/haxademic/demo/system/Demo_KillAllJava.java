076142bfd5409eb1d43274151360a153548bdc1f
cacheflowe <cacheflowe@cacheflowe.com>
2023-02-08 10:38:46 -0500

    Update Demo_KillAllJava.java

--- src/com/haxademic/demo/system/Demo_KillAllJava.java
+++ src/com/haxademic/demo/system/Demo_KillAllJava.java
@@ -5,17 +5,21 @@ import com.haxademic.core.system.SystemUtil;
 
 public class Demo_KillAllJava
 extends PAppletHax {
-	public static void main(String args[]) { arguments = args; PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
+    public static void main(String args[]) { arguments = args; PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 
-	protected void firstFrame() {
-		
-	}
+    protected void firstFrame() {
 
-	protected void drawApp() {
-		p.background(0);
-		p.text(p.frameCount, 20, 30);
-		if(p.frameCount == 90) {
-		    SystemUtil.killAllJavaWindows();
-		}
-	}
+    }
+
+    protected void drawApp() {
+        p.background(0);
+        p.text(p.frameCount, 20, 30);
+        if(p.frameCount == 90) {
+            // doesn't seem to kill all sub-processes because it gets killed before finishing bigger task kills 
+//            SystemUtil.killAllJavaWindows();
+            
+            // should clean up parent processes?
+            SystemUtil.killAllJavaWindowsScript();
+        }
+    }
 }
