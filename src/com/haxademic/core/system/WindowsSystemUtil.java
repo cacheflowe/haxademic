483a7fe64f7f21152c4b989fda5d6d5af02ac220
cacheflowe <cacheflowe@cacheflowe.com>
2023-02-15 10:56:33 -0500

    Update WindowsSystemUtil.java

--- src/com/haxademic/core/system/WindowsSystemUtil.java
+++ src/com/haxademic/core/system/WindowsSystemUtil.java
@@ -13,51 +13,51 @@ import com.haxademic.core.app.P;
 public class WindowsSystemUtil {
 
 	public static void killOtherJavaApps() {
-        try {
-            // Run "netsh" Windows command
-            Process process = Runtime.getRuntime().exec("tasklist"); // prepend cmd /c ?
-            
-            String javaProcessIDs = "";
-            String line;
-	        BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
-	        while ((line = input.readLine()) != null) {
-	            if(line.indexOf("javaw.exe") != -1 || line.indexOf("cmd.exe") != -1) {
-	            	// System.out.println("## "+line); //<-- Parse data here.
-	            	if(javaProcessIDs.length() > 0) javaProcessIDs += ",";
-	            	javaProcessIDs += line.split("\\s+")[1];
-	            }
-	        }
-	        
-	        String[] javaPIDs = javaProcessIDs.split(",");
-	        if(javaPIDs.length > 1) {
+				try {
+						// Run "netsh" Windows command
+						Process process = Runtime.getRuntime().exec("tasklist"); // prepend cmd /c ?
+						
+						String javaProcessIDs = "";
+						String line;
+					BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+					while ((line = input.readLine()) != null) {
+							if(line.indexOf("javaw.exe") != -1 || line.indexOf("cmd.exe") != -1) {
+								// System.out.println("## "+line); //<-- Parse data here.
+								if(javaProcessIDs.length() > 0) javaProcessIDs += ",";
+								javaProcessIDs += line.split("\\s+")[1];
+							}
+					}
+					
+					String[] javaPIDs = javaProcessIDs.split(",");
+					if(javaPIDs.length > 1) {
 				for (int i = 0; i < javaPIDs.length - 1; i++) {
 					// P.println("javaPIDs[] killing:", i, "=", javaPIDs[i]);
 					// P.println("kill command:", "Taskkill /PID "+ javaPIDs[i] +" /F");
-		        	Process killProcess = Runtime.getRuntime().exec("Taskkill /PID "+ javaPIDs[i] +" /F");
-		        	killProcess.isAlive();
+							Process killProcess = Runtime.getRuntime().exec("Taskkill /PID "+ javaPIDs[i] +" /F /T");
+							killProcess.isAlive();
 				}
 			}
-	        
-            // Get input streams
-            BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+					
+						// Get input streams
+						BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+						BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
 
-            // Read command standard output
-            String s;
+						// Read command standard output
+						String s;
 //            System.out.println("Standard output: ");
-            while ((s = stdInput.readLine()) != null) {
-                System.out.println(s);
-            }
+						while ((s = stdInput.readLine()) != null) {
+								System.out.println(s);
+						}
 
-            // Read command errors
+						// Read command errors
 //            System.out.println("Standard error: ");
-            while ((s = stdError.readLine()) != null) {
-                System.out.println(s);
-            }
+						while ((s = stdError.readLine()) != null) {
+								System.out.println(s);
+						}
 
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
-        }
+				} catch (Exception e) {
+						e.printStackTrace(System.err);
+				}
 	}
 	
 	public static void closeModalWindowAndSetOnTop(int delay) {
@@ -75,15 +75,15 @@ public class WindowsSystemUtil {
 				r.keyPress(KeyEvent.VK_ALT);
 				r.keyPress(KeyEvent.VK_TAB);
 				r.keyRelease(KeyEvent.VK_TAB);
-	            r.delay(200);
-	            
+							r.delay(200);
+							
 				r.keyPress(KeyEvent.VK_TAB);
-	            r.keyRelease(KeyEvent.VK_TAB);
-	            r.delay(200);
-	            
+							r.keyRelease(KeyEvent.VK_TAB);
+							r.delay(200);
+							
 				r.keyRelease(KeyEvent.VK_ALT);
-	            r.delay(500);
-	            
+							r.delay(500);
+							
 				r.keyPress(KeyEvent.VK_ENTER);
 				
 				SystemUtil.setTimeout(requestAppOnTop, 1000);
