10b153ee88df95a6a26e8f111fc475ef15cb23b8
cacheflowe <cacheflowe@cacheflowe.com>
2023-04-16 14:07:33 -0400

    Update Demo_WebServer_HttpInputState.java

--- src/com/haxademic/demo/net/Demo_WebServer_HttpInputState.java
+++ src/com/haxademic/demo/net/Demo_WebServer_HttpInputState.java
@@ -1,8 +1,12 @@
 package com.haxademic.demo.net;
 
+import com.cage.zxing4p3.ZXING4P;
 import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.app.config.AppSettings;
+import com.haxademic.core.app.config.Config;
 import com.haxademic.core.data.store.IAppStoreListener;
+import com.haxademic.core.debug.DebugView;
 import com.haxademic.core.hardware.http.HttpInputState;
 import com.haxademic.core.net.UIControlsHandler;
 import com.haxademic.core.net.WebServer;
@@ -17,18 +21,31 @@ implements IAppStoreListener {
 	public static void main(String args[]) { arguments = args; PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 	
 	protected WebServer server;
+	protected String webServerURL;
 	
+	protected void config() {
+		Config.setProperty(AppSettings.SHOW_DEBUG, true);
+	}
+
 	protected void firstFrame() {
 		// basic web server
-		// server = new WebServer(new UIControlsHandler(), false);
+		server = new WebServer(new UIControlsHandler(), false);
 		
 		// basic web server with SSL
-		server = new WebServer(new UIControlsHandler(), false, true);
+		// server = new WebServer(new UIControlsHandler(), false, true);
+		
+		// store address
+		webServerURL = WebServer.getServerAddress() + "web-server-demo/";
+
+		// create QR code
+		ZXING4P qr = new ZXING4P();
+		PImage qrImage = qr.generateQRCode(webServerURL, 128, 128);
+		DebugView.setTexture("qrImage", qrImage);
 	}
 	
 	protected void drawApp() {
 		background(0);
-		if(p.frameCount == 200) SystemUtil.openWebPage(WebServer.getServerAddress() + "web-server-demo/");
+		if(p.frameCount == 200) SystemUtil.openWebPage(webServerURL);
 		// draw slider val
 		p.fill(255);
 		p.rect(0, 0, P.map(HttpInputState.instance().getValue("slider1"), 0, 1, 0, p.width), p.height);
