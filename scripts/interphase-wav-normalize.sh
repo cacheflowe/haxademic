1bcae97a00d06e21db4c3733654cb53112964c5d
cacheflowe <cacheflowe@cacheflowe.com>
2023-04-24 18:26:12 -0400

    Create interphase-wav-normalize.sh

---
+++ scripts/interphase-wav-normalize.sh
@@ -0,0 +1,9 @@
+
+wavFormatTo16-44Dir.sh ../../interphase/data/audio/samples2/01-kick
+wavFormatTo16-44Dir.sh ../../interphase/data/audio/samples2/02-snare
+wavFormatTo16-44Dir.sh ../../interphase/data/audio/samples2/03-hats
+wavFormatTo16-44Dir.sh ../../interphase/data/audio/samples2/04-perc
+wavFormatTo16-44Dir.sh ../../interphase/data/audio/samples2/05-fx
+wavFormatTo16-44Dir.sh ../../interphase/data/audio/samples2/06-bass
+wavFormatTo16-44Dir.sh ../../interphase/data/audio/samples2/07-keys
+wavFormatTo16-44Dir.sh ../../interphase/data/audio/samples2/08-lead
