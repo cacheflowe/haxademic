ae0788d57c86a59197841f39846f8c4f04cbe24c
cacheflowe <cacheflowe@cacheflowe.com>
2023-06-08 10:41:55 -0400

    Update Demo_ParticleSystem_Custom_AudioRender.java

--- src/com/haxademic/demo/draw/particle/Demo_ParticleSystem_Custom_AudioRender.java
+++ src/com/haxademic/demo/draw/particle/Demo_ParticleSystem_Custom_AudioRender.java
@@ -1,52 +1,68 @@
 package com.haxademic.demo.draw.particle;
 
+import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.app.config.AppSettings;
 import com.haxademic.core.app.config.Config;
 import com.haxademic.core.data.constants.PBlendModes;
 import com.haxademic.core.debug.DebugView;
 import com.haxademic.core.draw.context.PG;
 import com.haxademic.core.draw.filters.pshader.BlurProcessingFilter;
+import com.haxademic.core.draw.filters.pshader.BrightnessStepFilter;
 import com.haxademic.core.draw.particle.Particle;
 import com.haxademic.core.draw.particle.ParticleSystem;
 import com.haxademic.core.hardware.keyboard.KeyboardState;
-import com.haxademic.core.hardware.mouse.Mouse;
+import com.haxademic.core.math.MathUtil;
 import com.haxademic.core.math.easing.Penner;
 import com.haxademic.core.media.DemoAssets;
 import com.haxademic.core.media.audio.AudioUtil;
 import com.haxademic.core.media.audio.analysis.AudioIn;
-import com.haxademic.core.media.audio.analysis.AudioIn.AudioInputLibrary;
+import com.haxademic.core.render.FrameLoop;
+import com.haxademic.core.render.Renderer;
 
 import processing.core.PGraphics;
+import processing.core.PImage;
 
-public class Demo_ParticleSystem_Custom_AudioIn
+public class Demo_ParticleSystem_Custom_AudioRender
 extends PAppletHax {
 	public static void main(String args[]) { arguments = args; PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 
+	// TODO
+	// - Build gradient image w/3 colors, and slide between them while drawing particles
+	// - Tweak particle falloff and pre-effects
+	
 	@SuppressWarnings("rawtypes")
     protected ParticleSystem<ParticleCustom> particles;
 	
 	protected void config() {
-		Config.setAppSize(1024, 1024);
-	}
-
+		Config.setAppSize(1280, 1024);
+        Config.setProperty( AppSettings.RENDERING_MOVIE, true );
+        Config.setProperty( AppSettings.RENDER_AUDIO_SIMULATION, true );
+        Config.setProperty( AppSettings.RENDER_AUDIO_FILE, P.path("haxademic/audio/cacheflowe_bigger_loop.wav") );
+        Config.setProperty( AppSettings.RENDER_AUDIO_FILE, P.path("audio/nike-women.wav") );
+    }
+    
 	@SuppressWarnings("rawtypes")
     protected void firstFrame() {
 		AudioUtil.setPrimaryMixer();
-		AudioIn.instance(AudioInputLibrary.Beads);
+//		AudioIn.instance(AudioInputLibrary.Beads); // for real-time mic input?
+		AudioIn.instance();
 
 		particles = new ParticleSystem<ParticleCustom>(ParticleCustom.class);
 //		particles.enableUI("PARTICLES_", false);
+
+		Renderer.instance().videoRenderer.setPG(pg);
 	}
 	
 	protected void drawApp() {
-		background(255);
+		background(0);
 		
 		// allow a reset
 		if(KeyboardState.keyTriggered(' ')) particles.killAll();
 		
 		// draw image/map base
 		pg.beginDraw();
-		pg.background(255);
+		if(p.frameCount < 10) pg.background(0);
 		PG.setDrawFlat2d(pg, true);
 		PG.setDrawCenter(pg);
 		launchFromMouse();
@@ -63,30 +79,47 @@ extends PAppletHax {
 	}
 	
 	protected void preProcess() {
-	    BlurProcessingFilter.instance().setSigma(10);
-	    BlurProcessingFilter.instance().setBlurSize(10);
+	    BlurProcessingFilter.instance().setSigma(20);
+	    BlurProcessingFilter.instance().setBlurSize(20);
 	    BlurProcessingFilter.instance().applyTo(pg);
+	    BrightnessStepFilter.instance().setBrightnessStep(-30/255f);
+	    BrightnessStepFilter.instance().applyTo(pg);
 	}
 	
 	@SuppressWarnings("rawtypes")
 	protected void launchFromMouse() {
-		int audioAmp = (int) (AudioIn.amplitude() * 100);
-		// if(P.abs(Mouse.xSpeed) > 0 && P.abs(Mouse.ySpeed) > 0) {
-			for(int i=0; i < audioAmp; i++) {
-				ParticleCustom particle = (ParticleCustom) particles.launchParticle(Mouse.x, Mouse.y, 0);
-				particle
-					.setSpeedRange(-2, 2, -2, 2, 0, 0)
-					.setAcceleration(0.97f, 0.97f, 1)
-					.setGravityRange(0, 0, 0, 0, 0, 0)
-					.setRotationRange(0, 0, 0, 0, 0, 0)
-					.setLifespanRange(50, 200)
-	                .setLifespanSustain(0)
-					.setSizeRange(100, 200)
-					.setColor(0xff00ff00)
-					.setImage(DemoAssets.particle())
-				    .randomize();
-			}
-		// }
+		float launchX = pg.width / 2 + P.cos(FrameLoop.count(0.02f)) * 200;
+		float launchY = pg.height / 2 + P.sin(FrameLoop.count(0.02f)) * 200;
+
+		int audioAmp = (int) (AudioIn.amplitude() * 300);
+		if(audioAmp > 0) {
+    		for(int i=0; i < audioAmp * 3; i++) {
+    		    int[] colorss = new int[] {
+    		            0xff00FC00,
+    		            0xff00C457,
+    		            0xff95FF61,
+    		    };
+    		    
+    		    PImage[] particlesTex = new PImage[] {
+//    		            DemoAssets.particleLight(),
+    		            DemoAssets.particleMedium(),
+    		            DemoAssets.particleHeavy(),
+    		    };
+    		    
+    			ParticleCustom particle = (ParticleCustom) particles.launchParticle(launchX, launchY, 0);
+    			particle
+    				.setSpeedRange(-2, 2, -2, 2, 0, 0)
+    				.setAcceleration(0.97f, 0.97f, 1)
+    				.setGravityRange(0, 0, 0, 0, 0, 0)
+    				.setRotationRange(0, 0, 0, 0, 0, 0)
+    				.setLifespanRange(100, 200)
+                    .setLifespanSustain(0)
+    				.setSizeRange(100, 300)
+    				.setColor(colorss[MathUtil.randIndex(colorss.length)])
+    				.setImage(particlesTex[MathUtil.randIndex(particlesTex.length)])
+    			    .randomize();
+    		}
+		}
 	}
 	
 	
