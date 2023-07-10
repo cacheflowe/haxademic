3ace59db79280759856a187f1e6ce7f261308dde
Justin Gitlin <cacheflowe@cacheflowe.com>
2023-07-09 22:58:06 -0400

    Add ColorCorrectionFilter UI helpers

--- src/com/haxademic/core/draw/filters/pshader/ColorCorrectionFilter.java
+++ src/com/haxademic/core/draw/filters/pshader/ColorCorrectionFilter.java
@@ -1,11 +1,16 @@
 package com.haxademic.core.draw.filters.pshader;
 
 import com.haxademic.core.draw.filters.pshader.shared.BaseFragmentShader;
+import com.haxademic.core.ui.UI;
 
 public class ColorCorrectionFilter
 extends BaseFragmentShader {
 
 	public static ColorCorrectionFilter instance;
+
+	public static String BRIGHTNESS = "BRIGHTNESS";
+	public static String CONTRAST = "CONTRAST";
+	public static String GAMMA = "GAMMA";
 	
 	public ColorCorrectionFilter() {
 		super("haxademic/shaders/filters/color-correction.glsl");
@@ -32,4 +37,17 @@ extends BaseFragmentShader {
 		shader.set("gamma", gamma);
 	}
 	
+	public void buildUI(String id, boolean saves) {
+		UI.addTitle("COLOR_CORRECTION | " + id);
+		UI.addSlider(id + BRIGHTNESS, 0, -1, 1, 0.01f, saves);
+		UI.addSlider(id + CONTRAST, 1, 0, 4, 0.01f, saves);
+		UI.addSlider(id + GAMMA, 1, 0, 5, 0.01f, saves);
+	}
+
+	public void setPropsFromUI(String id) {
+		setBrightness(UI.value(id + BRIGHTNESS));
+		setContrast(UI.value(id + CONTRAST));
+		setGamma(UI.value(id + GAMMA));
+	}
+
 }
