3fc04b4949c3b94e81c98424bba171566bfaa340
Cacheflowe <cacheflowe@cacheflowe.com>
2023-07-25 20:55:51 -0500

    Add windows setup scripts

---
+++ scripts/windows-setup/update-exe.ps1
@@ -0,0 +1,4 @@
+# Download files
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://hovercraft.box.com/shared/static/6yfjibgcsp949yypeyoqelp0sxb6cc7u.mp4", "C:\Users\cacheflowe\Documents\workspace\file.mp4")
