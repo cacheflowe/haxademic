6ca369c1353b2acf08cf9c7c182c843722e8fa71
cacheflowe <cacheflowe@cacheflowe.com>
2023-12-05 10:54:57 -0500

    Update Demo_DepthCameraRegion_PageTurnGesture.java

--- src/com/haxademic/demo/hardware/depthcamera/shared/Demo_DepthCameraRegion_PageTurnGesture.java
+++ src/com/haxademic/demo/hardware/depthcamera/shared/Demo_DepthCameraRegion_PageTurnGesture.java
@@ -42,42 +42,42 @@ extends Demo_DepthCameraRegion {
 	}
 	
 	protected void drawDepthImage() {
-	    PG.setPImageAlpha(p.g, 0.4f);
-	    ImageUtil.drawImageCropFill(DepthCamera.instance().camera.getDepthImage(), p.g, false);
-	    PG.resetPImageAlpha(p.g);
+		PG.setPImageAlpha(p.g, 0.4f);
+		ImageUtil.drawImageCropFill(DepthCamera.instance().camera.getDepthImage(), p.g, false);
+		PG.resetPImageAlpha(p.g);
 	}
 	
 	protected void calcDepthGesture() {
-	    xDelta = region.controlX() - lastUserX;
-	    xDelta = (P.abs(xDelta) > 0.1f) ? 0 : xDelta; // limit when starting in a different place
-	    lastUserX = region.controlX(); // userX.value();
-	    gestureHoriz.update(xDelta);
+		xDelta = region.controlX() - lastUserX;
+		xDelta = (P.abs(xDelta) > 0.1f) ? 0 : xDelta; // limit when starting in a different place
+		lastUserX = region.controlX(); // userX.value();
+		gestureHoriz.update(xDelta);
 	}
 	
 	protected void updateTimePlot() {
-	    timePlot.update(gestureHoriz.sum());
+		timePlot.update(gestureHoriz.sum());
 	}
 	
 	protected void drawTimePlot() {
-        p.blendMode(PBlendModes.SCREEN);
-        ImageUtil.drawImageCropFill(timePlot.image(), p.g, false);
-        p.blendMode(PBlendModes.BLEND);
+		p.blendMode(PBlendModes.SCREEN);
+		ImageUtil.drawImageCropFill(timePlot.image(), p.g, false);
+		p.blendMode(PBlendModes.BLEND);
 	}
 	
 	protected void printTextDebug() {
-	    PFont font = FontCacher.getFont(DemoAssets.fontMonospacePath, 30);
-	    FontCacher.setFontOnContext(p.g, font, p.color(255), 1f, PTextAlign.LEFT, PTextAlign.TOP);
-	    p.text("Frame delta: " + StringUtil.roundToPrecision(xDelta, 3, true), 20, p.height - 100);
-	    p.text("Motion sum:  " + StringUtil.roundToPrecision(gestureHoriz.sum(), 3, true), 20, p.height - 70);
-	}
+		PFont font = FontCacher.getFont(DemoAssets.fontMonospacePath, 30);
+		FontCacher.setFontOnContext(p.g, font, p.color(255), 1f, PTextAlign.LEFT, PTextAlign.TOP);
+		p.text("Frame delta: " + StringUtil.roundToPrecision(xDelta, 3, true), 20, p.height - 100);
+		p.text("Motion sum:  " + StringUtil.roundToPrecision(gestureHoriz.sum(), 3, true), 20, p.height - 70);
+}
 	
 	protected void drawApp() { 
 		super.drawApp();
 		drawDepthImage();
-	    calcDepthGesture();
-	    updateTimePlot();
-	    drawTimePlot();
-	    printTextDebug();
+		calcDepthGesture();
+		updateTimePlot();
+		drawTimePlot();
+		printTextDebug();
 	}
 		
 }
