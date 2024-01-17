7f6a8ae23898bec1c86c4d6a6ae677e096ac73ba
cacheflowe <cacheflowe@cacheflowe.com>
2024-01-17 12:20:34 -0500

    Add StringUtil.removeLineBreaks()

--- src/com/haxademic/core/text/StringUtil.java
+++ src/com/haxademic/core/text/StringUtil.java
@@ -14,6 +14,10 @@ public class StringUtil {
 	
 	public static final String EMPTY_STRING = "";
 
+	public static String removeLineBreaks(String str) {
+		return str.replaceAll("[\r\n]+", " ").replaceAll("\\s+", " ");
+	}
+
 	public static String formattedDecimal(String number) {
 		double amount = Double.parseDouble(number);
 		DecimalFormat formatter = new DecimalFormat("#,###.00");
@@ -21,40 +25,40 @@ public class StringUtil {
 	}
 
 	public static String roundToPrecision(float value, int numDecimalPlaces) {
-	    return roundToPrecision(value, numDecimalPlaces, false);
+		return roundToPrecision(value, numDecimalPlaces, false);
 	}
 
-    public static String roundToPrecision(float value, int numDecimalPlaces, boolean padRight) {
-        String decimalPlaces = stringOfLengthWithChar(numDecimalPlaces, '#');
-        DecimalFormat df = new DecimalFormat(" #."+decimalPlaces+";-#"); // https://stackoverflow.com/a/5243412
-        df.setRoundingMode(RoundingMode.FLOOR);
-        String output = df.format(value);
-        // always fill to padded size
-        if(padRight) {
-            if(output.contains(".") == false) {
-                output += "." + stringOfLengthWithChar(numDecimalPlaces, '0');
-            } else {
-                int curDecimalPlaces = output.length() - output.indexOf(".") - 1;
-                int numZerosToFill = numDecimalPlaces - curDecimalPlaces;
-                if(numZerosToFill > 0) {
-                    output += stringOfLengthWithChar(numZerosToFill, '0');
-                }
-            }
-        }
-        return output;
-    }
+	public static String roundToPrecision(float value, int numDecimalPlaces, boolean padRight) {
+		String decimalPlaces = stringOfLengthWithChar(numDecimalPlaces, '#');
+		DecimalFormat df = new DecimalFormat(" #."+decimalPlaces+";-#"); // https://stackoverflow.com/a/5243412
+		df.setRoundingMode(RoundingMode.FLOOR);
+		String output = df.format(value);
+		// always fill to padded size
+		if(padRight) {
+				if(output.contains(".") == false) {
+					output += "." + stringOfLengthWithChar(numDecimalPlaces, '0');
+				} else {
+					int curDecimalPlaces = output.length() - output.indexOf(".") - 1;
+					int numZerosToFill = numDecimalPlaces - curDecimalPlaces;
+					if(numZerosToFill > 0) {
+						output += stringOfLengthWithChar(numZerosToFill, '0');
+					}
+				}
+		}
+		return output;
+	}
 
 	public static String formattedInteger(int number) {
 		return NumberFormat.getInstance().format(number);
 	}
 	
 	public static String stringOfLengthWithChar(int length, char charToFill) {
-	    if (length > 0) {
-	        char[] array = new char[length];
-	        Arrays.fill(array, charToFill);
-	        return new String(array);
-	    }
-	    return "";
+		if (length > 0) {
+			char[] array = new char[length];
+			Arrays.fill(array, charToFill);
+			return new String(array);
+		}
+		return "";
 	}
 	
 	public static String safeString(String str) {
@@ -90,27 +94,27 @@ public class StringUtil {
 		return result;
 	}
 	
-    public static String urlEncode(String value) {
-        try {
-            return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
-        } catch (UnsupportedEncodingException ex) {
-            throw new RuntimeException(ex.getCause());
-        }
-    }
-    
-    public static String subStringByProgress(String str, float progress) {
-    	if(P.abs(progress) == 1) return str;
-    	if(progress == 0) return EMPTY_STRING;
-    	progress = P.constrain(progress, -1, 1);
-    	int index = P.round(progress * str.length());
-    	if(progress > 0) {
-    		return str.substring(0, index);
-    	} else {
-    		int startIndex = str.length() - P.abs(index);
-    		String pad = "";
-    		for (int i = 0; i < startIndex; i++) pad += "  "; 
-    		return pad + str.substring(startIndex, str.length());
-    	}
-    }
+	public static String urlEncode(String value) {
+			try {
+					return URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+			} catch (UnsupportedEncodingException ex) {
+					throw new RuntimeException(ex.getCause());
+			}
+	}
+	
+	public static String subStringByProgress(String str, float progress) {
+		if(P.abs(progress) == 1) return str;
+		if(progress == 0) return EMPTY_STRING;
+		progress = P.constrain(progress, -1, 1);
+		int index = P.round(progress * str.length());
+		if(progress > 0) {
+			return str.substring(0, index);
+		} else {
+			int startIndex = str.length() - P.abs(index);
+			String pad = "";
+			for (int i = 0; i < startIndex; i++) pad += "  "; 
+			return pad + str.substring(startIndex, str.length());
+		}
+	}
 
 }
