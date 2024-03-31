5f21f4316399702c4a23ecfd2d36c90ca90b5a34
Justin Gitlin <cacheflowe@cacheflowe.com>
2024-03-30 22:57:18 -0400

    Create interphase-wav-normalize.cmd

---
+++ scripts/interphase-wav-normalize.cmd
@@ -0,0 +1,8 @@
+start wavFormatTo16-44Dir.cmd ..\..\interphase\data\audio\samples2\01-kick\ ^& exit
+start wavFormatTo16-44Dir.cmd ..\..\interphase\data\audio\samples2\02-snare\ ^& exit
+start wavFormatTo16-44Dir.cmd ..\..\interphase\data\audio\samples2\03-hats\ ^& exit
+start wavFormatTo16-44Dir.cmd ..\..\interphase\data\audio\samples2\04-perc\ ^& exit
+start wavFormatTo16-44Dir.cmd ..\..\interphase\data\audio\samples2\05-fx\ ^& exit
+start wavFormatTo16-44Dir.cmd ..\..\interphase\data\audio\samples2\06-bass\ ^& exit
+start wavFormatTo16-44Dir.cmd ..\..\interphase\data\audio\samples2\07-keys\ ^& exit
+start wavFormatTo16-44Dir.cmd ..\..\interphase\data\audio\samples2\08-lead\ ^& exit
