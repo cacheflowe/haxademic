899886141ba5a1e90843b714ef070f766ae1f99c
Justin Gitlin <cacheflowe@cacheflowe.com>
2024-05-16 21:32:12 -0500

    Add SecondWindow & demo

--- src/com/haxademic/core/system/SecondWindow.java
+++ src/com/haxademic/core/system/SecondWindow.java
@@ -3,7 +3,6 @@ package com.haxademic.core.system;
 import com.haxademic.core.app.P;
 import com.haxademic.core.app.PAppletHax;
 import com.haxademic.core.data.constants.PRegisterableMethods;
-import com.haxademic.core.data.constants.PRenderers;
 import com.haxademic.core.draw.image.ImageUtil;
 
 import processing.core.PGraphics;
@@ -12,60 +11,73 @@ import processing.core.PImage;
 public class SecondWindow {
 
 	public PGraphics srcBuffer;
-	public PImage dstBuffer; // - has to be PImage to be shared between window contexts. PGraphics doesn't work
-	public SecondWindowWindow viewerWindow = null;
+	public PImage displayImage; // - has to be PImage to be shared between window contexts. PGraphics doesn't work
+	public SecondWindowWindow appWindow;
+	protected int defaultX;
+	protected int defaultY;
 	
-	public SecondWindow(PGraphics buffer) {
+	public SecondWindow(PGraphics buffer, int x, int y) {
 		srcBuffer = buffer;
-		dstBuffer = ImageUtil.newImage(srcBuffer.width, srcBuffer.height);
+		defaultX = x;
+		defaultY = y;
+		displayImage = ImageUtil.newImage(srcBuffer.width, srcBuffer.height);
 		P.p.registerMethod(PRegisterableMethods.post, this);	 // update texture to 2nd window after main draw() execution
 	}
 	
 	public void post() {
-		if(viewerWindow == null && P.p.frameCount >= 10) {
-			viewerWindow = new SecondWindowWindow();
+		if(appWindow == null && P.p.frameCount >= 1) {
+			appWindow = new SecondWindowWindow(this);
 		}
-		if(viewerWindow != null) {
-			ImageUtil.copyImage(srcBuffer, dstBuffer);
-			viewerWindow.setImage(dstBuffer);
-		}
+		ImageUtil.copyImage(srcBuffer, displayImage);
 	}
+
+	public void setLocation(int x, int y) {
+		AppUtil.setLocation(appWindow, x, y);
+	}
+	
+	public void setSize(int w, int h) {
+		AppUtil.setSize(appWindow, w, h);
+	}
+
+	public void moveToTop() {
+		appWindow.getSurface().setAlwaysOnTop(true);
+		appWindow.getSurface().setAlwaysOnTop(false);
+	}
+
+	////////////////////////////////////////////////////
+	// PAppletHax subclass for second window
+	////////////////////////////////////////////////////
 	
 	class SecondWindowWindow extends PAppletHax {
 
-		SecondWindow parent;
-		PImage srcImg;
+		protected SecondWindow parentObj;
 
-		public SecondWindowWindow() {
-			runSketch(new String[] {"SecondWindow"}, this);
+		public SecondWindowWindow(SecondWindow parentObj) {
+			this.parentObj = parentObj;
+			runSketch(new String[] {"SecondWindowWindow"}, this);
 		}
 
 		public void settings() {
-			size(dstBuffer.width, dstBuffer.height, PRenderers.P3D);
+			// THIS OVERRIDES PAppletHax normal initialization... So we're cutting off what it can do, however, this breaks super hard if not
+			// size(dstBuffer.width, dstBuffer.height, PRenderers.P3D);
+			fullScreen(P.P3D);
+		}
+
+		public void setup() {
+			P.out("SETUP");
 		}
 		
-		public void setImage(PImage img) {
-			srcImg = img;
+		protected void initWindowProps() {
+			AppUtil.setLocation(this, defaultX, defaultY);
+			AppUtil.setSize(this, displayImage.width, displayImage.height);
+			AppUtil.setResizable(this, true);
 		}
 
 		public void drawApp() {
-			background(0, 0, 255);
-			if(srcImg != null) {
-				image(srcImg, 0, 0);
-			}
+			if(frameCount == 5) initWindowProps();
+			background(0);
+			image(displayImage, 0, 0);
 		}
 
-		public void setLocation(int x, int y) {
-			surface.setLocation(x, y);
-		}
-
-		public void exit() {
-			dispose();
-		}
-
-		public void moveToTop() {
-			surface.setAlwaysOnTop(true);
-			surface.setAlwaysOnTop(false);
-		}
 	}
 }
--- src/com/haxademic/demo/system/Demo_SecondWindow.java
+++ src/com/haxademic/demo/system/Demo_SecondWindow.java
@@ -1,8 +1,12 @@
 package com.haxademic.demo.system;
 
 import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.app.config.Config;
+import com.haxademic.core.debug.DebugView;
 import com.haxademic.core.draw.context.PG;
-import com.haxademic.core.draw.image.ImageUtil;
+import com.haxademic.core.hardware.keyboard.KeyboardState;
+import com.haxademic.core.math.MathUtil;
+import com.haxademic.core.media.DemoAssets;
 import com.haxademic.core.system.SecondWindow;
 
 import processing.core.PGraphics;
@@ -12,14 +16,24 @@ extends PAppletHax {
 	public static void main(String args[]) { arguments = args; PAppletHax.main(Thread.currentThread().getStackTrace()[1].getClassName()); }
 
 	protected SecondWindow secondWindow;
+	protected SecondWindow secondWindow2;
 	protected PGraphics buffer;
 	
+	protected void config() {
+		Config.setAppSize(512, 512);
+	}
+
 	protected void firstFrame() {
-		buffer = PG.newPG(512, 512);
-		secondWindow = new SecondWindow(buffer);
+		buffer = PG.newPG(256, 256);
+		DebugView.setTexture("buffer", buffer);
+		secondWindow = new SecondWindow(buffer, 100, 0);
 	}
 
 	protected void drawApp() {
+		// create another app
+		// if(p.frameCount == 50) secondWindow2 = new SecondWindow(buffer, 400, 300);
+
+		// draw main app
 		PG.setDrawCenter(p);
 		p.background((float)mouseX/width * 255,(float)mouseY/height * 255,0);
 		p.fill(255);
@@ -27,18 +41,27 @@ extends PAppletHax {
 		p.rotate(p.frameCount * 0.01f);
 		p.rect(0, 0, 100, 100);
 
-		// buffer.beginDraw();
-		// buffer.background(0, 255, 0);
-		// buffer.fill(255);
-		// buffer.translate(buffer.width/2, buffer.height/2);
-		// buffer.rotate(p.frameCount * 0.01f);
-		// buffer.rect(0, 0, 100, 100);
-		// buffer.endDraw();
-	}
+		// draw test graphics for second window
+		buffer.beginDraw();
+		PG.setCenterScreen(buffer);
+		PG.setDrawCenter(buffer);
+		buffer.rotate(p.frameCount * 0.01f);
+		buffer.image(DemoAssets.justin(), 0, 0);
+		buffer.endDraw();
 
-	public void keyReleased(){
-		if(key == 'f') {
+		// keystrokes on the main thread - important for doing any window operations
+		if(KeyboardState.keyTriggered('s')) {
+			// setting window size can crash pretty often. should research better ways of doing this
+			secondWindow.setSize(MathUtil.randRange(64, 256), MathUtil.randRange(64, 256));
+			// secondWindow.setSize(buffer.width, buffer.height);
 		}
-	} 
+		if(KeyboardState.keyTriggered('l')) {
+			secondWindow.setLocation(MathUtil.randRange(0, 100), MathUtil.randRange(0, 100));
+			// secondWindow.setLocation(0, 0);
+		}
+		if(KeyboardState.keyTriggered('t')) {
+			secondWindow.moveToTop();
+		}
+	}
 	
 }
