13dbe1aa1a654c1f4a297875ddc11e40c68ec64f
Justin Gitlin <cacheflowe@cacheflowe.com>
2024-06-05 23:53:35 -0400

    Update windows-apps-install.ps1

--- scripts/windows-setup/windows-apps-install.ps1
+++ scripts/windows-setup/windows-apps-install.ps1
@@ -16,7 +16,7 @@ choco install git
 choco install ffmpeg
 choco install teamviewer
 choco install vlc
-choco install googlechrome
+choco install googlechrome --ignore-checksums
 choco install nodejs-lts
 choco install rustdesk
 choco install quicklook
@@ -30,7 +30,7 @@ choco install 7zip.install
 
 $WebClient = New-Object System.Net.WebClient
 # Realsense
-$WebClient.DownloadFile("https://github.com/IntelRealSense/librealsense/releases/download/v2.54.1/Intel.RealSense.SDK-WIN10-2.54.1.5216.exe", "$HOME\Downloads\Intel.RealSense.SDK-WIN10-2.54.1.5216.exe")
+$WebClient.DownloadFile("https://github.com/IntelRealSense/librealsense/releases/download/v2.55.1/Intel.RealSense.SDK-WIN10-2.55.1.6486.exe", "$HOME\Downloads\Intel.RealSense.SDK-WIN10-2.55.1.6486.exe")
 # Wub.exe
 $WebClient.DownloadFile("https://www.sordum.org/files/download/windows-update-blocker/Wub_v1.8.zip", "$HOME\Downloads\Wub_v1.8.zip")
 Expand-Archive -Path "$HOME\Downloads\Wub_v1.8.zip" -Force -DestinationPath "$HOME\Downloads\Wub_v1.8"
